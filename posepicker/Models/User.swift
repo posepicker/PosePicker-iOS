7722dbe9bbaa982587fcbc62f7b018013684ea86
Parkjju <rudwns3927@gmail.com>
2023-10-19 16:40:37 +0900

    add: ignore 리스트 업데이트

---
+++ posepicker/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  posepicker
+//
+//  Created by 박경준 on 2023/10/19.
+//
+
+import Foundation
+
+struct User: Codable {
+    let email: String
+    let id: Int
+    let nickname: String
+    
+}
