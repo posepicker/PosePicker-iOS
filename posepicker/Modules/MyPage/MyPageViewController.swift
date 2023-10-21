adeb275c9204573ec3fed2d47d0de43029c715fc
Parkjju <rudwns3927@gmail.com>
2023-10-21 14:28:32 +0900

    [Feat] 마이페이지 뷰 추가

---
+++ posepicker/Modules/MyPage/MyPageViewController.swift
@@ -0,0 +1,11 @@
+//
+//  MyPageViewController.swift
+//  posepicker
+//
+//  Created by 박경준 on 2023/10/21.
+//
+
+import UIKit
+
+class MyPageViewController: BaseViewController {
+}
---
+++ posepicker/Modules/MyPage/MyPageViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageViewModel.swift
+//  posepicker
+//
+//  Created by 박경준 on 2023/10/21.
+//
+
+import Foundation
+
+class MyPageViewModel: ViewModelType {
+    struct Input { }
+    
+    struct Output { }
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+}
