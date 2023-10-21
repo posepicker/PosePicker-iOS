1f48d18d36ab8cdd7a48c9ba3448374c90d7c489
Parkjju <rudwns3927@gmail.com>
2023-10-21 15:58:09 +0900

    [Feat] UIView Extension - 그래디언트 / 그림자 / 서브뷰 추가 함수

---
+++ posepicker/Extensions/UIView/UIView+.swift
@@ -0,0 +1,49 @@
+//
+//  UIView+.swift
+//  posepicker
+//
+//  Created by 박경준 on 2023/10/21.
+//
+
+import UIKit
+
+extension UIView {
+    func setGradient(colors: [CGColor],
+                     locations: [NSNumber] = [0.0, 1.0],
+                     startPoint: CGPoint = CGPoint(x: 0.0, y: 0.0),
+                     endPoint: CGPoint = CGPoint(x: 1.0, y: 0.0)) -> Self {
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.colors = colors
+        gradient.locations = locations
+        gradient.startPoint = startPoint
+        gradient.endPoint = endPoint
+        gradient.frame = self.bounds
+        layer.addSublayer(gradient)
+        return self
+    }
+    
+    func makeShadow(color: UIColor = .black, alpha: Float, x: CGFloat, y: CGFloat, blur: CGFloat, spread: CGFloat) {
+        layer.masksToBounds = false
+        layer.shadowColor = color.cgColor
+        layer.shadowOpacity = alpha
+        layer.shadowOffset = CGSize(width: x, height: y)
+        layer.shadowRadius = blur / 2.0
+        if spread == 0 {
+            layer.shadowPath = nil
+        } else {
+            let dx = -spread
+            let rect = bounds.insetBy(dx: dx, dy: dx)
+            layer.shadowPath = UIBezierPath(rect: rect).cgPath
+        }
+    }
+    
+    func addSubView<T: UIView>(_ subview: T, completionHandler closure: ((T) -> Void)? = nil) {
+        addSubview(subview)
+        closure?(subview)
+    }
+    
+    func addSubViews<T: UIView>(_ subviews: [T], completionHandler closure: (([T]) -> Void)? = nil) {
+        subviews.forEach { addSubview($0) }
+        closure?(subviews)
+    }
+}
