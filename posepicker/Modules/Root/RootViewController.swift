3e804943f994e613da127ae295a414bc45f15a93
Parkjju <rudwns3927@gmail.com>
2023-10-21 14:20:32 +0900

    [Feat] 루트 뷰 생성

---
+++ posepicker/Modules/Root/RootViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RootViewController.swift
+//  posepicker
+//
+//  Created by 박경준 on 2023/10/21.
+//
+
+import UIKit
+
+class RootViewController: BaseViewController {
+    
+    // MARK: - Subviews
+    
+    // MARK: - Properties
+    
+    // MARK: - Initialization
+    
+    // MARK: - Functions
+    
+    
+}
