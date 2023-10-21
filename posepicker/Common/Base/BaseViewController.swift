95c4c1caae2b3c39161d4438b66469d49467f354
Parkjju <rudwns3927@gmail.com>
2023-10-21 15:53:19 +0900

    [Feat] BaseViewController 내부 정의 - 디스포즈백 및 렌더링 관련 함수

--- posepicker/Common/Base/BaseViewController.swift
+++ posepicker/Common/Base/BaseViewController.swift
@@ -6,24 +6,41 @@
 //
 
 import UIKit
+import RxSwift
+import SnapKit
+import Then
 
 class BaseViewController: UIViewController {
+    var disposeBag = DisposeBag()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    init() {
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
-    */
 
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        render()
+        configUI()
+        bindViewModel()
+    }
+    
+    func render() {
+        // Override Layout
+    }
+    
+    func configUI() {
+        // View Configuration
+    }
+    
+    func bindViewModel() {
+        // view model binding
+    }
 }
