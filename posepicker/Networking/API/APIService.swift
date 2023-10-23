9fd9e83cfb5a0e8598345ba68811431003073750
Parkjju <rudwns3927@gmail.com>
2023-10-24 00:51:10 +0900

    feat: APIService 프로토콜 Single기반 통신 함수 추가

---
+++ posepicker/Networking/API/APIService.swift
@@ -0,0 +1,15 @@
+//
+//  APIService.swift
+//  posepicker
+//
+//  Created by Jun on 2023/10/23.
+//
+
+import Foundation
+
+import Alamofire
+import RxSwift
+
+protocol APIService {
+    func requestSingle<T: Codable> (_ request: APIRouter) -> Single<T>
+}
