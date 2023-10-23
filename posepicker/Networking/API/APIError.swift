4dd5d5542f75a82ba0c55d9f7ee8bea66a471bc4
Parkjju <rudwns3927@gmail.com>
2023-10-24 00:49:49 +0900

    feat: APIError 열거형 / 익스텐션 정의

---
+++ posepicker/Networking/API/APIError.swift
@@ -0,0 +1,28 @@
+//
+//  APIError.swift
+//  posepicker
+//
+//  Created by Jun on 2023/10/24.
+//
+
+import Foundation
+
+enum APIError: Error {
+    case decode
+    case http(status: Int)
+    case unknown
+}
+
+/// API 에러 스트링으로 변환
+extension APIError: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .decode:
+            return "Decode Error"
+        case let .http(status):
+            return "HTTP Error: \(status)"
+        case .unknown:
+            return "Unknown Error"
+        }
+    }
+}
