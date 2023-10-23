42eeac405f8e40204686981ca5e52470bfa3f457
Parkjju <rudwns3927@gmail.com>
2023-10-23 11:07:40 +0900

    feat: UIImage load 메서드 정의 (리터럴 사용)

---
+++ posepicker/Extensions/UIImage/UIImage+.swift
@@ -0,0 +1,25 @@
+//
+//  UIImage+.swift
+//  posepicker
+//
+//  Created by Jun on 2023/10/23.
+//
+
+import UIKit
+
+extension UIImage {
+    static func load(named imageName: String) -> UIImage {
+        guard let image = UIImage(named: imageName, in: nil, compatibleWith: nil) else {
+            return UIImage()
+        }
+        image.accessibilityIdentifier = imageName
+        return image
+    }
+    
+    func resize(to size: CGSize) -> UIImage {
+        let image = UIGraphicsImageRenderer(size: size).image { _ in
+            draw(in: CGRect(origin: .zero, size: size))
+        }
+        return image
+    }
+}
