6e4cb84279d567dedc3cb3461cc0b6fb5f74426d
Parkjju <rudwns3927@gmail.com>
2023-10-24 00:49:08 +0900

    feat: 포즈픽 모델 정의

---
+++ posepicker/Models/PosePick.swift
@@ -0,0 +1,24 @@
+//
+//  PosePick.swift
+//  posepicker
+//
+//  Created by Jun on 2023/10/23.
+//
+
+import Foundation
+
+struct PosePick: Codable {
+    let poseInfo: Pose
+}
+
+struct Pose: Codable {
+    let createdAt: String
+    let frameCount: Int
+    let imageKey: String
+    let peopleCount: Int
+    let poseId: Int
+    let source: String
+    let sourceUrl: String
+    let tagAttributes: String
+    let updatedAt: String
+}
