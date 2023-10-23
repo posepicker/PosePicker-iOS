9bf8a66b28781babd39931ba3dac45d67ac131d8
Parkjju <rudwns3927@gmail.com>
2023-10-24 00:50:41 +0900

    feat: APIRouter 클래스 - 포즈픽 데이터 GET요청 함수 추가

---
+++ posepicker/Networking/API/APIRouter.swift
@@ -0,0 +1,73 @@
+//
+//  APIRouter.swift
+//  posepicker
+//
+//  Created by 박경준 on 2023/10/19.
+//
+
+import Foundation
+
+import Alamofire
+
+enum APIRouter: URLRequestConvertible {
+    
+    case retrievePosePick(peopleCount: Int)
+    
+    // MARK: - HttpMethod
+    
+    /// switch - self 구문으로 각 엔드포인트별 메서드 지정
+    private var method: HTTPMethod {
+        switch self {
+        case .retrievePosePick:
+            return .get
+        }
+    }
+    
+    // MARK: - Path
+    
+    /// switch - self 구문으로 각 엔드포인트별 URL Path 지정
+    private var path: String {
+        switch self {
+        case .retrievePosePick(let peopleCount):
+            return "/api/pose/pick/\(peopleCount)"
+        }
+    }
+    
+    // MARK: - Parameters
+    
+    /// request body 정의
+    /// 빈 body를 보낼때는 nil값 전달
+    private var parameters: Parameters? {
+        switch self {
+        case .retrievePosePick:
+            return nil
+        }
+    }
+    
+    // MARK: - URLRequestConvertible
+    
+    func asURLRequest() throws -> URLRequest {
+        let url = try K.baseUrl.asURL()
+        
+        var urlRequest = URLRequest(url: url.appendingPathComponent(path))
+        
+        /// self의 method 속성을 참조
+        urlRequest.httpMethod = method.rawValue
+        
+        /// 네트워크 통신 일반에 사용되는 헤더 기본추가
+        urlRequest.setValue(K.ContentType.json.rawValue, forHTTPHeaderField: K.HttpHeaderField.acceptType.rawValue)
+        urlRequest.setValue(K.ContentType.json.rawValue, forHTTPHeaderField: K.HttpHeaderField.contentType.rawValue)
+        
+        /// 요청 바디 인코딩
+        let encoding: ParameterEncoding = {
+            switch method {
+            case .get:
+                return URLEncoding.default
+            default:
+                return JSONEncoding.default
+            }
+        }()
+        
+        return try encoding.encode(urlRequest, with: parameters)
+    }
+}
