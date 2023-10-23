26149db72c31a143b4a7f234474027132db28523
Parkjju <rudwns3927@gmail.com>
2023-10-23 11:06:47 +0900

    feat: 애셋 이미지리터럴 연결

--- posepicker/Resources/ImageLiteral.swift
+++ posepicker/Resources/ImageLiteral.swift
@@ -8,4 +8,24 @@
 import UIKit
 
 enum ImageLiteral {
+    static var imgArrowBack24: UIImage {.load(named: "imgArrowBack24")}
+    static var imgBookmarkFill20: UIImage {.load(named: "imgBookmarkFill20")}
+    static var imgBookmarkFill24: UIImage {.load(named: "imgBookmarkFill24")}
+    static var imgBookmarkOff20: UIImage {.load(named: "imgBookmarkOff20")}
+    static var imgBookmarkOff24: UIImage {.load(named: "imgBookmarkOff24")}
+    static var imgCaretDown: UIImage {.load(named: "imgCaretDown")}
+    static var imgCaretUp: UIImage {.load(named: "imgCaretUp")}
+    static var imgClose12: UIImage {.load(named: "imgClose12")}
+    static var imgClose20: UIImage {.load(named: "imgClose20")}
+    static var imgClose24: UIImage {.load(named: "imgClose24")}
+    static var imgGithubLogo: UIImage {.load(named: "imgGithubLogo")}
+    static var imgHelp20: UIImage {.load(named: "imgHelp20")}
+    static var imgHelp24: UIImage {.load(named: "imgHelp24")}
+    static var imgInfo20: UIImage {.load(named: "imgInfo20")}
+    static var imgInstagramLogo: UIImage {.load(named: "imgInstagramLogo")}
+    static var imgLink: UIImage {.load(named: "imgLink")}
+    static var imgMenu24: UIImage {.load(named: "imgMenu24")}
+    static var imgRestart24: UIImage {.load(named: "imgRestart24")}
+    static var imgRestart20: UIImage {.load(named: "imgRestart20")}
+    static var imgShareFill: UIImage {.load(named: "imgShareFill")}
 }
