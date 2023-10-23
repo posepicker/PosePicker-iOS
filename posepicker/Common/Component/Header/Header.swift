90456d50fb224fcdb0398d1460cc0adc19624130
Parkjju <rudwns3927@gmail.com>
2023-10-23 11:07:08 +0900

    feat: 헤더 공통컴포넌트 정의

---
+++ posepicker/Common/Component/Header/Header.swift
@@ -0,0 +1,61 @@
+//
+//  Header.swift
+//  posepicker
+//
+//  Created by Jun on 2023/10/23.
+//
+
+import UIKit
+
+class Header: UIView {
+    
+    // MARK: - Subviews
+    let appTitleLabel = UILabel()
+        .then {
+            $0.font = .h4
+            $0.text = "PosePicker"
+        }
+    
+    let menuButton = UIButton(type: .system)
+        .then {
+            $0.setImage(ImageLiteral.imgMenu24.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+    
+    // MARK: - Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        render()
+        bindUI()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Properties
+    
+    var isNavigating = false
+    
+    // MARK: - Functions
+    
+    func render() {
+        self.addSubViews([appTitleLabel, menuButton])
+        
+        appTitleLabel.snp.makeConstraints { make in
+            make.leading.equalTo(self).offset(20)
+            make.centerY.equalTo(self)
+        }
+        
+        menuButton.snp.makeConstraints { make in
+            make.trailing.equalTo(self).offset(-20)
+            make.centerY.equalTo(self)
+        }
+    }
+    
+    func bindUI() {
+        
+    }
+
+}
