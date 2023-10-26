ba1b44a795510425756ec8d788e8cdab254f4b2e
Parkjju <rudwns3927@gmail.com>
2023-10-26 17:14:25 +0900

    feat: UIScreen isWiderThan375 정의 - 스크린 너비 대응

---
+++ posepicker/Extensions/UIScreen/UIScreen+.swift
@@ -0,0 +1,14 @@
+//
+//  UIScreen+.swift
+//  posepicker
+//
+//  Created by 박경준 on 2023/10/26.
+//
+
+import UIKit
+extension UIScreen {
+  /// - Mini, SE: 375.0
+  /// - pro: 390.0
+  /// - pro max: 428.0
+  var isWiderThan375pt: Bool { self.bounds.size.width > 375 }
+}
