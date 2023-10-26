24a247cc30198e85ac2753a2a60934243252d916
Parkjju <rudwns3927@gmail.com>
2023-10-26 21:20:39 +0900

    feat: 북마크 empty case UI 구현

--- posepicker/Modules/Bookmark/BookMarkViewController.swift
+++ posepicker/Modules/Bookmark/BookMarkViewController.swift
@@ -8,9 +8,39 @@
 import UIKit
 
 class BookMarkViewController: BaseViewController {
+    
+    // MARK: - Subviews
+    
+    let emptyView = EmptyBookmarkView()
+    
+    // MARK: - Properties
+    var viewModel: BookMarkViewModel
+    
+    // MARK: - Initialization
+    
+    init(viewModel: BookMarkViewModel) {
+        self.viewModel = viewModel
+        super.init()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     // MARK: - Functions
+    
+    override func render() {
+        view.addSubViews([emptyView])
+        
+        emptyView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(52)
+            make.height.equalTo(170)
+            make.top.equalToSuperview().offset(80)
+        }
+    }
+    
     override func configUI() {
         self.navigationController?.isNavigationBarHidden = true
-        view.backgroundColor = .mainViolet
+        view.backgroundColor = .bgWhite
     }
 }
