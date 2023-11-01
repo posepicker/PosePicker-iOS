470629ec8dacb3092a620495f0461acaa3d8d38e
Parkjju <rudwns3927@gmail.com>
2023-11-01 16:09:33 +0900

    feat: 뷰모델 이미지 로딩관련 로직 및 상태값 추가

--- posepicker/Modules/PosePick/PosePickViewModel.swift
+++ posepicker/Modules/PosePick/PosePickViewModel.swift
@@ -16,21 +16,68 @@ class PosePickViewModel: ViewModelType {
     
     struct Input {
         let posePickButtonTapped: ControlEvent<Void>
+        let isImageLoading: Observable<Bool>
+        let isAnimating: Observable<Bool>
+        let refetchTrigger: Observable<Void>
     }
     
     struct Output {
         let animate: Driver<Void>
+        let imageUrl: Driver<String>
+        let isLoading: Observable<Bool>
+        let isPosePickerImageHidden: Observable<Bool>
     }
     
     func transform(input: Input) -> Output {
+        let imageUrl = BehaviorRelay<String>(value: "")
+        let animate = BehaviorRelay<Void>(value: ())
+        let isLoading = BehaviorRelay<Bool>(value: false)
+        let isPosePickerImageHidden = BehaviorRelay<Bool>(value: false)
+        
+        /// 포즈픽 데이터 요청
         input.posePickButtonTapped
             .flatMapLatest { [unowned self] _ -> Observable<PosePick> in
                 self.apiSession.requestSingle(.retrievePosePick(peopleCount: 1)).asObservable()
             }
             .subscribe(onNext: {
-                print($0)
+                imageUrl.accept($0.poseInfo.imageKey)
             })
             .disposed(by: disposeBag)
-        return Output(animate: input.posePickButtonTapped.asDriver())
+        
+        /// 이미지 로딩중에 애니메이션 트리거
+        input.isImageLoading
+            .subscribe(onNext: {
+                if $0 {
+                    animate.accept(())
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        /// 탭 이후 애니메이션 트리거
+        input.posePickButtonTapped
+            .subscribe(onNext: {
+                animate.accept(())
+                isPosePickerImageHidden.accept(true) // 썸네일 초기 이미지 숨기기
+            })
+            .disposed(by: disposeBag)
+        
+        /// 이미지 로딩이 끝나지 않아 로티 재요청
+        input.refetchTrigger
+            .subscribe(onNext: {
+                animate.accept(())
+            })
+            .disposed(by: disposeBag)
+        
+        Observable.combineLatest(input.isAnimating, input.isImageLoading)
+            .subscribe(onNext: { isAnimating, isImageLoading in
+                if isAnimating || isImageLoading {
+                    isLoading.accept(true)
+                } else {
+                    isLoading.accept(false)
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        return Output(animate: animate.asDriver(), imageUrl: imageUrl.asDriver(), isLoading: isLoading.asObservable(), isPosePickerImageHidden: isPosePickerImageHidden.asObservable())
     }
 }
