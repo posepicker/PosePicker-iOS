202bd9a6955a31bbdc717083d6ba4373bfbb5eb6
Parkjju <rudwns3927@gmail.com>
2023-11-05 01:11:29 +0900

    feat: 포즈피드 컬렉션뷰 셀 클래스 정의 / 바인딩 함수 추가

---
+++ posepicker/Modules/PoseFeed/Cell/PoseFeedPhotoCell.swift
@@ -0,0 +1,44 @@
+//
+//  PoseFeedPhotoCell.swift
+//  posepicker
+//
+//  Created by Jun on 2023/11/05.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+
+class PoseFeedPhotoCell: BaseCollectionViewCell {
+    
+    // MARK: - Subviews
+    let imageView = UIImageView()
+        .then {
+            $0.contentMode = .scaleAspectFill
+        }
+    
+    // MARK: - Properties
+    static let identifier = "PoseFeedPhotoCell"
+    
+    // MARK: - Life Cycles
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+    }
+    
+    // MARK: - Functions
+    override func render() {
+        self.addSubViews([imageView])
+        
+        imageView.snp.makeConstraints { make in
+            make.top.leading.bottom.trailing.equalToSuperview()
+        }
+    }
+    
+    func bind(to viewModel: PoseFeedPhotoCellViewModel) {
+        viewModel.image.bind(to: imageView.rx.image)
+            .disposed(by: disposeBag)
+    }
+}
