964848478cc4d3c19f29225e08974cfd63dcd5be
Parkjju <rudwns3927@gmail.com>
2023-11-05 01:10:22 +0900

    feat: 포즈피드 모델 정의

---
+++ posepicker/Models/Pose/PoseFeed.swift
@@ -0,0 +1,35 @@
+//
+//  PoseFeed.swift
+//  posepicker
+//
+//  Created by Jun on 2023/11/05.
+//
+
+import Foundation
+
+struct PoseFeed: Codable {
+    let content: Pose
+    let empty: Bool
+    let first: Bool
+    let last: Bool
+    let number: Int
+    let numberOfElements: Int
+    let pageable: Page
+    let size: Int
+    let sort: Sort
+}
+
+struct Page: Codable {
+    let offset: Int
+    let pageNumber: Int
+    let pageSize: Int
+    let paged: Bool
+    let sort: Sort
+    let unpaged: Bool
+}
+
+struct Sort: Codable {
+    let empty: Bool
+    let sorted: Bool
+    let unsorted: Bool
+}
