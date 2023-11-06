e34007786da16649d7abfb5a201f210869fd0998
Parkjju <rudwns3927@gmail.com>
2023-11-06 20:48:05 +0900

    feat: UIImageView 킹피셔 세팅 함수 구현

---
+++ posepicker/Extensions/UIImageView/UIImageView+.swift
@@ -0,0 +1,30 @@
+//
+//  UIImageView+.swift
+//  posepicker
+//
+//  Created by 박경준 on 2023/11/06.
+//
+
+import UIKit
+import Kingfisher
+
+extension UIImageView {
+    func setImage(with urlString: String) {
+        ImageCache.default.retrieveImage(forKey: urlString, options: nil) { result in
+            switch result {
+            case .success(let value):
+                if let image = value.image {
+                    //캐시가 존재하는 경우
+                    self.image = image
+                } else {
+                    //캐시가 존재하지 않는 경우
+                    guard let url = URL(string: urlString) else { return }
+                    let resource = KF.ImageResource(downloadURL: url, cacheKey: urlString)
+                    self.kf.setImage(with: resource)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
