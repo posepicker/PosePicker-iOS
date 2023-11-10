90d6adb5cbf8cacfa48f24ef7f7d3d50b17ea0b1
Parkjju <rudwns3927@gmail.com>
2023-11-11 00:29:54 +0900

    feat: 마이페이지 뷰모델 & 코디네이터 주입

--- posepicker/Modules/MyPage/MyPageViewController.swift
+++ posepicker/Modules/MyPage/MyPageViewController.swift
@@ -8,6 +8,23 @@
 import UIKit
 
 class MyPageViewController: BaseViewController {
+    
+    // MARK: - Properties
+    var viewModel: MyPageViewModel
+    var coordinator: RootCoordinator
+    
+    // MARK: - Life Cycles
+    
+    init(viewModel: MyPageViewModel, coordinator: RootCoordinator) {
+        self.viewModel = viewModel
+        self.coordinator = coordinator
+        super.init()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     // MARK: - Functions
     override func configUI() {
         self.navigationController?.isNavigationBarHidden = true
