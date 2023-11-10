2090e6b09960f99029ef7f7303f51dfe2ff3d375
Parkjju <rudwns3927@gmail.com>
2023-11-11 00:25:06 +0900

    feat: 헤더 뷰 북마크 버튼 추가

--- posepicker/Common/Component/Header/Header.swift
+++ posepicker/Common/Component/Header/Header.swift
@@ -21,6 +21,11 @@ class Header: UIView {
             $0.setImage(ImageLiteral.imgMenu24.withRenderingMode(.alwaysOriginal), for: .normal)
         }
     
+    let bookMarkButton = UIButton(type: .system)
+        .then {
+            $0.setImage(ImageLiteral.imgBookmarkFill24.withTintColor(.iconDefault).withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+    
     // MARK: - Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -41,7 +46,7 @@ class Header: UIView {
     // MARK: - Functions
     
     func render() {
-        self.addSubViews([appTitleLabel, menuButton])
+        self.addSubViews([appTitleLabel, menuButton, bookMarkButton])
         
         appTitleLabel.snp.makeConstraints { make in
             make.leading.equalTo(self).offset(20)
@@ -50,6 +55,11 @@ class Header: UIView {
         
         menuButton.snp.makeConstraints { make in
             make.trailing.equalTo(self).offset(-20)
+            make.centerY.equalTo(self)
+        }
+        
+        bookMarkButton.snp.makeConstraints { make in
+            make.trailing.equalTo(menuButton.snp.leading).offset(-20)
             make.centerY.equalTo(self)
         }
     }
