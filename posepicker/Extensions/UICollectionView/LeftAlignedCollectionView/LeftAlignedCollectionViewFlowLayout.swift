6fc7cf70da2d9abb16a72c0ec2fafd72561d90cc
Parkjju <rudwns3927@gmail.com>
2023-11-14 00:17:30 +0900

    feat: 왼쪽정렬 필터 컬렉션뷰 패딩값 반응형 처리

--- posepicker/Extensions/UICollectionView/LeftAlignedCollectionView/LeftAlignedCollectionViewFlowLayout.swift
+++ posepicker/Extensions/UICollectionView/LeftAlignedCollectionView/LeftAlignedCollectionViewFlowLayout.swift
@@ -14,6 +14,8 @@ class LeftAlignedCollectionViewFlowLayout: UICollectionViewFlowLayout {
 
         var leftMargin = sectionInset.left
         var maxY: CGFloat = -1.0
+        let padding: CGFloat = UIScreen.main.isWiderThan375pt ? 5 : 15
+        
         attributes?.forEach { layoutAttribute in
             // MARK: 한줄 넘어간 시점에 y값을 초기화해주는 코드
             if layoutAttribute.frame.origin.y >= maxY {
@@ -23,7 +25,8 @@ class LeftAlignedCollectionViewFlowLayout: UICollectionViewFlowLayout {
             layoutAttribute.frame.origin.x = leftMargin
 
             // MARK: 컬렉션뷰 배치가 완료된 이후에 minimum space를 더해주고 있음 -> 오토레이아웃에 상관 없이 벗어나게 되는것
-            leftMargin += layoutAttribute.frame.width + 5
+            
+            leftMargin += layoutAttribute.frame.width + padding
             maxY = max(layoutAttribute.frame.maxY , maxY)
         }
 
