339283b9cc7b4e90aa4831de8db13c19558f6add
Parkjju <rudwns3927@gmail.com>
2023-11-14 00:04:47 +0900

    feat: 필터값 수정 (단독 추가 / 소품 삭제 / 유명프레임 단어 수정 / 캐릭터 추가)

--- posepicker/Common/Constants/enum/FilterTags.swift
+++ posepicker/Common/Constants/enum/FilterTags.swift
@@ -8,31 +8,33 @@
 import Foundation
 
 enum FilterTags: String {
+    case solo = "단독"
     case friend = "친구"
     case couple = "커플"
     case family = "가족"
     case coworker = "동료"
+    case celebrity = "유명프레임"
+    case character = "캐릭터"
+    case trend = "유행"
     case fun = "재미"
     case natural = "자연스러움"
-    case trend = "유행"
-    case celebrity = "유명인 프레임"
-    case props = "소품"
     
     static func getAllFilterTags() -> [FilterTags] {
-        return [.friend, .couple, .family, .coworker, .fun, .natural, .trend, .celebrity, .props]
+        return [.solo, .friend, .couple, .family, .coworker, .celebrity, .character, .trend, .fun, .natural]
     }
     
     static func getTagFromTitle(title: String) -> FilterTags? {
         switch title {
+        case "단독": return .solo
         case "친구": return .friend
         case "커플": return .couple
         case "가족": return .family
         case "동료": return .coworker
+        case "유명프레임": return .celebrity
+        case "캐릭터": return .character
+        case "유행": return .trend
         case "재미": return .fun
         case "자연스러움": return .natural
-        case "유행": return .trend
-        case "유명인 프레임": return .celebrity
-        case "소품": return .props
         default: return nil
         }
     }
@@ -77,15 +79,16 @@ enum FilterTags: String {
     
     func getTagNumber() -> Int {
         switch self {
-        case .friend: return 0
-        case .couple: return 1
-        case .family: return 2
-        case .coworker: return 3
-        case .fun: return 4
-        case .natural: return 5
-        case .trend: return 6
-        case .celebrity: return 7
-        case .props: return 8
+        case .solo: return 0
+        case .friend: return 1
+        case .couple: return 2
+        case .family: return 3
+        case .coworker: return 4
+        case .celebrity: return 5
+        case .character: return 6
+        case .trend: return 7
+        case .fun: return 8
+        case .natural: return 9
         }
     }
 }
