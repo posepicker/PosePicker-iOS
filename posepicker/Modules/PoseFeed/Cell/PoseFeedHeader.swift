0a77fce310523cc75b849d851fccf0c8366d8780
Parkjju <rudwns3927@gmail.com>
2023-11-15 07:15:27 +0900

    feat: 포즈피드 헤더 클래스 정의

---
+++ posepicker/Modules/PoseFeed/Cell/PoseFeedHeader.swift
@@ -0,0 +1,47 @@
+//
+//  PoseFeedHeader.swift
+//  posepicker
+//
+//  Created by Jun on 2023/11/14.
+//
+
+import UIKit
+
+class PoseFeedHeader: UICollectionReusableView {
+    
+    let label = UILabel()
+        .then {
+            $0.textAlignment = .left
+            $0.textColor = .textPrimary
+            $0.font = .h4
+            $0.text = "이런 포즈는 어때요?"
+        }
+
+    // MARK: - Properties
+    static let identifier = "PoseFeedHeader"
+    
+    // MARK: - Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        render()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Functions
+    func render() {
+        self.addSubview(label)
+        
+        label.snp.makeConstraints { make in
+            make.leading.centerY.equalToSuperview()
+        }
+    }
+
+    func configureHeader(with title: String) {
+        label.text = title
+    }
+}
