d3a27ebdb1dd81416409d507caf0364f71d9cc8d
Parkjju <rudwns3927@gmail.com>
2023-11-16 13:42:20 +0900

    feat: 포즈 상세화면 뷰모델 구현 === 1. 이미지 출처 버튼 - URL 연결 로직 2. 링크 공유 버튼 - 클립보드 복사 기능 3. 포즈데이터 주입 후 이미지 캐시처리 및 다운로드

--- posepicker/Modules/PoseFeed/PoseDetail/PoseDetailViewModel.swift
+++ posepicker/Modules/PoseFeed/PoseDetail/PoseDetailViewModel.swift
@@ -5,24 +5,85 @@
 //  Created by Jun on 2023/11/15.
 //
 
-import Foundation
+import UIKit
 
 import RxCocoa
 import RxSwift
+import Kingfisher
 
 class PoseDetailViewModel: ViewModelType {
     
     var disposeBag = DisposeBag()
+    var poseDetailData: PosePick
+    
+    init(poseDetailData: PosePick) {
+        self.poseDetailData = poseDetailData
+    }
     
     struct Input {
-        
+        let imageSourceButtonTapped: ControlEvent<Void>
+        let linkShareButtonTapped: ControlEvent<Void>
     }
     
     struct Output {
-        
+        let imageSourceLink: Observable<URL?>
+        let image: Observable<UIImage?>
+        let popupPresent: Driver<Void>
     }
     
     func transform(input: Input) -> Output {
-        return Output()
+        let imageSource = BehaviorRelay<URL?>(value: nil)
+        let cacheImage = BehaviorRelay<UIImage?>(value: nil)
+        let popupPresent = PublishSubject<Void>()
+        
+        /// 이미지 출처 - URL 전달
+        input.imageSourceButtonTapped
+            .subscribe(onNext: { [unowned self] in
+                let url = URL(string: "https://" + self.poseDetailData.poseInfo.sourceUrl)
+                imageSource.accept(url)
+            })
+            .disposed(by: disposeBag)
+        
+        /// 이미지 캐시처리
+        ImageCache.default.retrieveImage(forKey: poseDetailData.poseInfo.imageKey, options: nil) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let value):
+                if let image = value.image {
+                    let newSizeImage = self.newSizeImageWidthDownloadedResource(image: image)
+                    cacheImage.accept(newSizeImage)
+                } else {
+                    guard let url = URL(string: self.poseDetailData.poseInfo.imageKey) else { return }
+                    KingfisherManager.shared.retrieveImage(with: url) { downloadResult in
+                        switch downloadResult {
+                        case .success(let downloadImage):
+                            let newSizeImage = self.newSizeImageWidthDownloadedResource(image: downloadImage.image)
+                            cacheImage.accept(newSizeImage)
+                        case .failure:
+                            return
+                        }
+                    }
+                }
+            case .failure:
+                cacheImage.accept(nil)
+            }
+        }
+        
+        /// 링크 공유 버튼 탭
+        input.linkShareButtonTapped
+            .subscribe(onNext: { [unowned self] in
+                UIPasteboard.general.string = "https://www.posepicker.site/detail/\(self.poseDetailData.poseInfo.poseId)"
+                popupPresent.onNext(())
+            })
+            .disposed(by: disposeBag)
+        
+        return Output(imageSourceLink: imageSource.asObservable(), image: cacheImage.asObservable(), popupPresent: popupPresent.asDriver(onErrorJustReturn: ()))
+    }
+    
+    /// 디자인 수치 기준으로 이미지 리사이징
+    func newSizeImageWidthDownloadedResource(image: UIImage) -> UIImage {
+        let targetWidth = UIScreen.main.bounds.width
+        let newSizeImage = image.resize(newWidth: targetWidth)
+        return newSizeImage
     }
 }
