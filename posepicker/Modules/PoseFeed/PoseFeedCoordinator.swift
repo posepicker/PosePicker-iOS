97456423cf42bae5712a2df0af11b04bba7f6e46
Parkjju <rudwns3927@gmail.com>
2023-11-20 15:25:03 +0900

    feat: 포즈피드 코디네이터 dismiss 이후 선택 태그 전달

--- posepicker/Modules/PoseFeed/PoseFeedCoordinator.swift
+++ posepicker/Modules/PoseFeed/PoseFeedCoordinator.swift
@@ -40,6 +40,20 @@ class PoseFeedCoordinator: NSObject, Coordinator {
     func dismissPoseDetailWithTagSelection(tag: String) {
         guard let posefeedViewController = self.navigationController.viewControllers.first as? PoseFeedViewController else { return }
         
+        if let peopleCountTag = PeopleCountTags.getTagFromTitle(title: tag) {
+            // 1. 인원 수 태그인 경우
+            self.poseFeedFilterViewController.selectedHeadCount.accept(peopleCountTag)
+        } else if let frameCountTag = FrameCountTags.getTagFromTitle(title: tag) {
+            // 2. 프레임 수 태그인 경우
+            self.poseFeedFilterViewController.selectedFrameCount.accept(frameCountTag)
+        } else {
+            // 3. 서브태그인 경우
+            self.poseFeedFilterViewController.registeredSubTag.accept(tag)
+        }
+        
+        // 셀렉션 초기화
+        self.poseFeedFilterViewController.detailViewDismissTrigger.onNext(())
+        
         posefeedViewController.modalDismissWithTag.onNext(tag)
     }
 }
