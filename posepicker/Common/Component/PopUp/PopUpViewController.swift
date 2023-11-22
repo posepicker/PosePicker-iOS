8542b4d874286fe0de432bbe83bdf9a0e9c9ada7
Parkjju <rudwns3927@gmail.com>
2023-11-22 09:45:02 +0900

    feat: 팝업 modalTransitionStyle 재사용을 위한 타입캐스팅 로직 - 팝업뷰 로직 분리

--- posepicker/Common/Component/PopUp/PopUpViewController.swift
+++ posepicker/Common/Component/PopUp/PopUpViewController.swift
@@ -11,28 +11,61 @@ import RxSwift
 class PopUpViewController: BaseViewController {
 
     // MARK: - Subviews
-    let popUpView = PopUpView()
+    lazy var popUpView = self.isLoginPopUp ? LoginPopUpView() : PopUpView()
     
     // MARK: - Properties
+    var isLoginPopUp: Bool
+    
+    // MARK: - Initialization
+    init(isLoginPopUp: Bool) {
+        self.isLoginPopUp = isLoginPopUp
+        super.init()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     // MARK: - Functions
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        self.dismiss(animated: true)
+    }
+    
     override func render() {
         self.view.addSubViews([popUpView])
         
-        popUpView.snp.makeConstraints { make in
-            make.width.equalTo(300)
-            make.height.equalTo(158)
-            make.center.equalToSuperview()
+        if let popUpView = popUpView as? PopUpView {
+            popUpView.snp.makeConstraints { make in
+                make.width.equalTo(300)
+                make.height.equalTo(158)
+                make.center.equalToSuperview()
+            }
+        } else {
+            popUpView.snp.makeConstraints { make in
+                make.width.equalTo(300)
+                make.height.equalTo(220)
+                make.center.equalToSuperview()
+            }
         }
+    
     }
     
     override func configUI() {
         view.backgroundColor = .init(hex: "#000000", alpha: 0.3)
         
-        popUpView.completeButton.rx.tap
-            .subscribe(onNext: { [unowned self] in
-                self.dismiss(animated: true)
-            })
-            .disposed(by: disposeBag)
+        /// 팝업 뷰일때
+        if let popUpView = popUpView as? PopUpView {
+            popUpView.completeButton.rx.tap
+                .subscribe(onNext: { [unowned self] in
+                    self.dismiss(animated: true)
+                })
+                .disposed(by: disposeBag)
+        }
+        
+        /// 로그인 팝업일때
+        if let popUpView = popUpView as? LoginPopUpView {
+            
+        }
     }
 }
