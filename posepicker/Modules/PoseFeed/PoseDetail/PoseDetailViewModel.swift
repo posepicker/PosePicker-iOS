c5fbe8212a427f8077594712120245fdf129c23d
Parkjju <rudwns3927@gmail.com>
2023-11-22 16:19:35 +0900

    feat: 카카오 공유하기 구현 / FeedTemplate 객체 리턴

--- posepicker/Modules/PoseFeed/PoseDetail/PoseDetailViewModel.swift
+++ posepicker/Modules/PoseFeed/PoseDetail/PoseDetailViewModel.swift
@@ -10,6 +10,11 @@ import UIKit
 import RxCocoa
 import RxSwift
 import Kingfisher
+import KakaoSDKShare
+import RxKakaoSDKShare
+import KakaoSDKTemplate
+import RxKakaoSDKCommon
+import KakaoSDKCommon
 
 class PoseDetailViewModel: ViewModelType {
     
@@ -23,6 +28,7 @@ class PoseDetailViewModel: ViewModelType {
     struct Input {
         let imageSourceButtonTapped: ControlEvent<Void>
         let linkShareButtonTapped: ControlEvent<Void>
+        let kakaoShareButtonTapped: ControlEvent<Void>
     }
     
     struct Output {
@@ -83,6 +89,43 @@ class PoseDetailViewModel: ViewModelType {
         let tags = getTagArrayFromData(peopleCount: poseDetailData.poseInfo.peopleCount, frameCount: poseDetailData.poseInfo.frameCount, tagString: poseDetailData.poseInfo.tagAttributes)
         tagItems.accept(tags.map { PoseDetailTagCellViewModel(title: $0) })
         
+        /// 5. 카카오 공유하기 버튼
+        input.kakaoShareButtonTapped
+            .subscribe(onNext: { [unowned self] in
+                let template = self.getFeedTemplateObject()
+                    
+                // 메시지 템플릿 encode
+                if let templateJsonData = (try? SdkJSONEncoder.custom.encode(template)) {
+                    // 생성한 메시지 템플릿 객체를 jsonObject로 변환
+                    if let templateJsonObject = SdkUtils.toJsonObject(templateJsonData) {
+                        // 카카오톡 앱이 있는지 체크합니다.
+                        if ShareApi.isKakaoTalkSharingAvailable() {
+                            ShareApi.shared.shareDefault(templateObject:templateJsonObject) {(linkResult, error) in
+                                if let error = error {
+                                    print("error : \(error)")
+                                }
+                                else {
+                                    print("defaultLink(templateObject:templateJsonObject) success.")
+                                    guard let linkResult = linkResult else { return }
+                                    UIApplication.shared.open(linkResult.url, options: [:], completionHandler: nil)
+                                }
+                            }
+                        } else {
+                            // 없을 경우 카카오톡 앱스토어로 이동합니다. (이거 하려면 URL Scheme에 itms-apps 추가 해야함)
+                            let url = "itms-apps://itunes.apple.com/app/362057947"
+                            if let url = URL(string: url), UIApplication.shared.canOpenURL(url) {
+                                if #available(iOS 10.0, *) {
+                                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                                } else {
+                                    UIApplication.shared.openURL(url)
+                                }
+                            }
+                        }
+                    }
+                }
+            })
+            .disposed(by: disposeBag)
+        
         return Output(imageSourceLink: imageSource.asObservable(), image: cacheImage.asObservable(), popupPresent: popupPresent.asDriver(onErrorJustReturn: ()), tagItems: tagItems.asDriver())
     }
     
@@ -102,5 +145,24 @@ class PoseDetailViewModel: ViewModelType {
         }
         
         return tags
+    }
+    
+    func getFeedTemplateObject() -> FeedTemplate {
+        let link = Link(webUrl: URL(string: "https://www.posepicker.site/detail/\(self.poseDetailData.poseInfo.poseId)"),
+                            mobileWebUrl: URL(string: "https://www.posepicker.site/detail/\(self.poseDetailData.poseInfo.poseId)"))
+
+        // 앱 링크입니다. 파라미터를 함께 전달하여 앱으로 들어왔을 때 특정 페이지로 이동할 수 있는 역할을 합니다.
+//        let appLink = Link(androidExecutionParams: ["key1": "value1", "key2": "value2"],
+//                           iosExecutionParams: ["key1": "value1", "key2": "value2"])
+        
+        // 버튼들 입니다.
+        let webButton = Button(title: "포즈가 고민될 땐? 포즈피커!", link: link)
+//        let appButton = Button(title: "앱으로 보기", link: appLink) // MARK: - 포즈피커 앱으로 연결
+        
+        // 메인이 되는 사진, 이미지 URL, 클릭 시 이동하는 링크를 설정합니다.
+        let content = Content(title: "PosePicker | 포즈피커",
+                              imageUrl: URL(string: "https://www.posepicker.site/meta/og_kakao.png")!,
+                              link: link)
+        return FeedTemplate(content: content, buttons: [webButton])
     }
 }
