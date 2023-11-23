508da76c2bb1c47bedf5e7b3e9cf63979a1ddb5c
Parkjju <rudwns3927@gmail.com>
2023-11-23 14:49:15 +0900

    feat: 마이페이지 뷰 제작 - 로그인 버튼 & 서비스 이용 문의 팝업 제작 및 링크 연결

--- posepicker/Modules/MyPage/MyPageViewController.swift
+++ posepicker/Modules/MyPage/MyPageViewController.swift
@@ -9,6 +9,36 @@ import UIKit
 
 class MyPageViewController: BaseViewController {
     
+    // MARK: - Subviews
+    let loginButton = UIButton(type: .system)
+        .then {
+            $0.backgroundColor = .violet050
+            $0.layer.cornerRadius = 16
+        }
+    
+    let loginLogo = UIImageView(image: ImageLiteral.imgLoginLogo)
+    
+    let loginTitle = UILabel()
+        .then {
+            $0.font = .pretendard(.medium, ofSize: 16)
+            $0.textColor = .textPrimary
+            $0.text = "로그인하기"
+        }
+    
+    let serviceUsageInquiryButton = UIButton(type: .system)
+        .then {
+            $0.setTitleColor(.textPrimary, for: .normal)
+            $0.titleLabel?.font = .pretendard(.medium, ofSize: 16)
+            $0.setTitle("서비스 이용문의", for: .normal)
+        }
+    
+    let serviceInformationButton = UIButton(type: .system)
+        .then {
+            $0.setTitleColor(.textPrimary, for: .normal)
+            $0.setTitle("서비스 정보", for: .normal)
+            $0.titleLabel?.font = .pretendard(.medium, ofSize: 16)
+        }
+    
     // MARK: - Properties
     var viewModel: MyPageViewModel
     var coordinator: RootCoordinator
@@ -27,7 +57,68 @@ class MyPageViewController: BaseViewController {
     
     // MARK: - Functions
     override func configUI() {
-        self.navigationController?.isNavigationBarHidden = true
-        view.backgroundColor = .green
+        self.navigationController?.isNavigationBarHidden = false
+        self.title = "메뉴"
+        view.backgroundColor = .bgWhite
+    }
+    
+    override func render() {
+        view.addSubViews([loginButton, loginLogo, loginTitle, serviceUsageInquiryButton, serviceInformationButton])
+        
+        loginButton.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(12)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(88)
+        }
+        
+        loginLogo.snp.makeConstraints { make in
+            make.centerY.equalTo(loginButton)
+            make.leading.equalTo(loginButton).offset(20)
+        }
+        
+        loginTitle.snp.makeConstraints { make in
+            make.leading.equalTo(loginLogo.snp.trailing).offset(16)
+            make.centerY.equalTo(loginButton)
+        }
+        
+        serviceUsageInquiryButton.snp.makeConstraints { make in
+            make.leading.equalTo(loginButton)
+            make.top.equalTo(loginButton.snp.bottom).offset(24)
+            make.height.equalTo(24)
+        }
+        
+        serviceInformationButton.snp.makeConstraints { make in
+            make.leading.equalTo(loginButton)
+            make.top.equalTo(serviceUsageInquiryButton.snp.bottom).offset(24)
+            make.height.equalTo(24)
+        }
+    }
+    
+    override func bindViewModel() {
+        serviceUsageInquiryButton.rx.tap.asDriver()
+            .drive(onNext: { [unowned self] in
+                let popupViewController = PopUpViewController(isLoginPopUp: false, isChoice: true)
+                popupViewController.modalTransitionStyle = .crossDissolve
+                popupViewController.modalPresentationStyle = .overFullScreen
+                let popupView = popupViewController.popUpView as! PopUpView
+                popupView.alertText.accept("문의사항을 남기시겠습니까?")
+                
+                popupView.confirmButton.rx.tap.asDriver()
+                    .drive(onNext: {
+                        if let url = URL(string: "https://litt.ly/posepicker") {
+                            UIApplication.shared.open(url)
+                        }
+                    })
+                    .disposed(by: self.disposeBag)
+                
+                popupView.cancelButton.rx.tap.asDriver()
+                    .drive(onNext: {
+                        popupViewController.dismiss(animated: true)
+                    })
+                    .disposed(by: self.disposeBag)
+                
+                self.present(popupViewController, animated: true)
+            })
+            .disposed(by: disposeBag)
     }
 }
