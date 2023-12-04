7ad6c1ed744601f21381e722136ad0a8c076fbbf
Parkjju <rudwns3927@gmail.com>
2023-12-04 17:22:55 +0900

    feat: posepicker 예시 테스트코드 작성

--- posepickerTests/posepickerTests.swift
+++ posepickerTests/posepickerTests.swift
@@ -6,31 +6,51 @@
 //
 
 import XCTest
+import Alamofire
+import RxCocoa
+import RxSwift
 @testable import posepicker
 
 final class posepickerTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    var sut: APISession!
+    var disposeBag: DisposeBag!
+    
+    override func setUp() {
+        super.setUp()
+        let session: Session = {
+            let configuration: URLSessionConfiguration = {
+                let configuration = URLSessionConfiguration.default
+                configuration.protocolClasses = [MockURLProtocol.self] // 내가만든 가짜 프로토콜 주입!
+                return configuration
+            }()
+            return Session(configuration: configuration)
+        }()
+        sut = APISession(session: session)
+        disposeBag = DisposeBag()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func test_api_pose_id() {
+        MockURLProtocol.responseWithDTO(type: .posepick)
+        MockURLProtocol.responseWithStatusCode(code: 200)
+        
+        let expectation = XCTestExpectation(description: "포즈픽 /api/pose/{id} 테스트")
+        let single: Single<PosePick> = sut.requestSingle(.retrievePoseDetail(poseId: 460))
+        
+        single.asObservable()
+            .subscribe(onNext: {
+                XCTAssertEqual($0.poseInfo.poseId, 460)
+                print($0)
+            })
+            .disposed(by: disposeBag)
+        
+        wait(for: [expectation], timeout: 5)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    
+    override func tearDown() {
+        super.tearDown()
+        sut = nil
+        disposeBag = nil
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
