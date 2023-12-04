b967273922e4b929ab1c3ae92d9254f43a0c1811
Parkjju <rudwns3927@gmail.com>
2023-12-05 00:36:23 +0900

    test: 마이페이지 뷰 토큰 셋업 테스트코드 작성

--- posepickerTests/MyPage/mypageTests.swift
+++ posepickerTests/MyPage/mypageTests.swift
@@ -31,28 +31,40 @@ final class MyPageTests: XCTestCase {
         sut = APISession(session: session)
         disposeBag = DisposeBag()
         scheduler = TestScheduler(initialClock: 0)
-        viewModel = MyPageViewModel()
+        viewModel = MyPageViewModel(apiSession: sut) // 목업 세션 주입
     }
     
-    /// 1. 애플로그인 버튼 탭
-    /// 2. idToken 전달
-    /// 3. 로그인처리
+    /// Given - 애플 아이디 토큰
+    /// When - apiSession 객체를 통해 토큰 요청
+    /// Then - 토큰 발급 후 액세스토큰 & 리프레시 토큰 추출
     func test_애플로그인_탭_이후_로그인처리() {
-        let user = scheduler.createObserver(User?.self)
+        MockURLProtocol.responseWithDTO(type: .user)
+        MockURLProtocol.responseWithStatusCode(code: 200)
+        
+        let expectation = XCTestExpectation(description: "/api/users/login/apple/ 테스트")
         var input = retrieveDefaultInputObservable()
         
         input.appleIdToken = scheduler.createColdObservable([
-            .next(10, "test_jwt")
+            .next(10, "test_idToken")
         ]).asObservable()
         
         let output = viewModel.transform(input: input)
         
-        output.user
-            .drive(user)
+        // 액세스 토큰 세팅
+        output.user.asObservable()
+            .compactMap { $0 }
+            .map { $0.token }
+            .subscribe(onNext: {
+                print("세팅된 TOKEN: \($0)")
+                XCTAssertEqual($0.accessToken, "string")
+                XCTAssertEqual($0.refreshToken, "string")
+                expectation.fulfill()
+            })
             .disposed(by: disposeBag)
         
         scheduler.start()
         
+        wait(for: [expectation], timeout: 5)
     }
     
     override func tearDown() {
