404acdad4a61970176d0d47e67cd2bf1437203cb
Parkjju <rudwns3927@gmail.com>
2023-12-06 01:42:37 +0900

    feat: 이미지 다운로드 목업 URL 프로토콜 정의 / 로컬 이미지 내보내기

---
+++ posepicker/Networking/Mock/MockImageDownloaderIURLProtocol.swift
@@ -0,0 +1,101 @@
+//
+//  MockImageDownloaderIURLProtocol.swift
+//  posepicker
+//
+//  Created by 박경준 on 12/5/23.
+//
+
+import Foundation
+
+final class MockImageDownloaderIURLProtocol: URLProtocol {
+    
+    private lazy var session: URLSession = {
+        let configuration: URLSessionConfiguration = URLSessionConfiguration.default
+        return URLSession(configuration: configuration)
+    }()
+    
+    enum ResponseType {
+        case error(APIError)
+        case success(HTTPURLResponse)
+    }
+    
+    static var responseType: ResponseType!
+    static var dtoType: MockDTOType!
+}
+
+extension MockImageDownloaderIURLProtocol {
+    
+    static func responseWithFailure() {
+        MockImageDownloaderIURLProtocol.responseType = MockImageDownloaderIURLProtocol.ResponseType.error(APIError.unknown)
+    }
+    
+    static func responseWithStatusCode(code: Int) {
+        MockImageDownloaderIURLProtocol.responseType = MockImageDownloaderIURLProtocol.ResponseType.success(HTTPURLResponse(url: URL(string: K.baseUrl)!, statusCode: code, httpVersion: nil, headerFields: nil)!)
+    }
+    
+    static func responseWithDTO(type: MockDTOType) {
+        MockImageDownloaderIURLProtocol.dtoType = type
+    }
+}
+
+
+extension MockImageDownloaderIURLProtocol {
+    
+    enum MockDTOType {
+        case empty
+        case cacheImage
+        
+        var fileName: String {
+            switch self {
+            case .empty: return ""
+            case .cacheImage: return "image.jpeg"
+            }
+        }
+    }
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        let response = setUpMockResponse()
+        let data = setUpMockData()
+
+        client?.urlProtocol(self, didReceive: response!, cacheStoragePolicy: .notAllowed)
+        
+        client?.urlProtocol(self, didLoad: data!)
+        
+        self.client?.urlProtocolDidFinishLoading(self)
+    }
+    
+    private func setUpMockResponse() -> HTTPURLResponse? {
+        var response: HTTPURLResponse?
+        switch MockImageDownloaderIURLProtocol.responseType {
+        case .error(let error)?:
+            client?.urlProtocol(self, didFailWithError: error)
+        case .success(let newResponse)?:
+            response = newResponse
+        default:
+            fatalError("No fake responses found.")
+        }
+        return response!
+    }
+    
+    private func setUpMockData() -> Data? {
+        let fileName: String = MockImageDownloaderIURLProtocol.dtoType.fileName
+        print("HI~~~ \(fileName)")
+       // 번들에 있는 json 파일로 Data 객체를 뽑아내는 과정.
+        guard let file = Bundle.main.url(forResource: fileName, withExtension: nil) else {
+            return Data()
+        }
+        print("setupmockdata! \(file)")
+        return try? Data(contentsOf: file)
+    }
+    
+    override func stopLoading() {
+    }
+}
