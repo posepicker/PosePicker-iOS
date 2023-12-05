c4e4a39cd224d3ee82fe4f95c1ce4a2ebcdfce68
Parkjju <rudwns3927@gmail.com>
2023-12-06 01:41:20 +0900

    test: 프로토콜 삽입 후 이미지 불러오기 / 이미지 동일성 여부 검증 테스트코드 작성

--- posepickerTests/Bookmark/bookmarkTests.swift
+++ posepickerTests/Bookmark/bookmarkTests.swift
@@ -10,6 +10,7 @@ import Alamofire
 import RxCocoa
 import RxSwift
 import RxTest
+import Kingfisher
 @testable import posepicker
 
 final class bookmarkTests: XCTestCase {
@@ -28,10 +29,59 @@ final class bookmarkTests: XCTestCase {
             }()
             return Session(configuration: configuration)
         }()
+        
+        let imageDownloader: ImageDownloader = {
+            let downloader = ImageDownloader.default
+            let configuration = URLSessionConfiguration.default
+            configuration.protocolClasses = [MockImageDownloaderIURLProtocol.self]
+            downloader.sessionConfiguration = configuration
+//            downloader.sessionConfiguration.protocolClasses = [MockImageDownloaderIURLProtocol.self]
+            return downloader
+        }()
+        
         sut = APISession(session: session)
         disposeBag = DisposeBag()
         scheduler = TestScheduler(initialClock: 0)
-        viewModel = BookMarkViewModel(apiSession: sut) // 목업 세션 주입
+        viewModel = BookMarkViewModel(apiSession: sut, imageDownloader: imageDownloader) // 목업 세션 주입
+        ImageCache.default.clearCache() // 캐시 비우기
+    }
+    
+    /// Given - viewDidLoad
+    /// When - transform
+    /// Then - items
+    func test_데이터가_없을때_empty뷰를_띄워주는지() {
+        
+        MockURLProtocol.responseWithStatusCode(code: 200)
+        MockURLProtocol.responseWithDTO(type: .bookmarkFeed)
+        
+        MockImageDownloaderIURLProtocol.responseWithStatusCode(code: 200)
+        MockImageDownloaderIURLProtocol.responseWithDTO(type: .cacheImage)
+        
+        var input = retrieveDefaultInputObservable()
+        
+        input.viewDidLoadTrigger = scheduler.createColdObservable([
+            .next(1, ())
+        ]).asObservable()
+        
+        let output = viewModel.transform(input: input)
+        let expectation = XCTestExpectation(description: "북마크 API 테스트")
+        
+        scheduler.start()
+        
+        // 네트워크에 의존중..
+        output.bookmarkItems
+            .compactMap { $0 }
+            .drive(onNext: {
+                // 캐시처리가 오래걸리나?
+                $0.forEach { element in
+                    print(element.image.value)
+                    print(element.poseId.value)
+                }
+                expectation.fulfill()
+            })
+            .disposed(by: disposeBag)
+        
+        wait(for: [expectation], timeout: 5)
     }
     
     override func tearDown() {
@@ -40,5 +90,12 @@ final class bookmarkTests: XCTestCase {
         sut = nil
         scheduler = nil
         viewModel = nil
+    }
+    
+    func retrieveDefaultInputObservable() -> BookMarkViewModel.Input {
+        let viewDidLoadTrigger: TestableObservable<Void> = scheduler.createColdObservable([])
+        let input = BookMarkViewModel.Input(viewDidLoadTrigger: viewDidLoadTrigger.asObservable())
+        
+        return input
     }
 }
