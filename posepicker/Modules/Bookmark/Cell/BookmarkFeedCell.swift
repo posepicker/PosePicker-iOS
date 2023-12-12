3aa04a862f9ba3fc50b0802f4fedda4706c34c3f
Parkjju <rudwns3927@gmail.com>
2023-12-12 17:54:56 +0900

    feat: 북마크 셀 UI 추가 / bind 함수 구현

--- posepicker/Modules/Bookmark/Cell/BookmarkFeedCell.swift
+++ posepicker/Modules/Bookmark/Cell/BookmarkFeedCell.swift
@@ -9,7 +9,51 @@ import UIKit
 
 class BookmarkFeedCell: BaseCollectionViewCell {
     
+    // MARK: - Subviews
+    let imageView = UIImageView()
+        .then {
+            $0.contentMode = .scaleAspectFill
+        }
+    
+    let bookmarkButton = UIButton()
+        .then {
+            $0.setImage(ImageLiteral.imgBookmarkOff24.withTintColor(.iconWhite, renderingMode: .alwaysOriginal), for: .normal)
+            $0.layer.cornerRadius = 18
+            $0.clipsToBounds = true
+            $0.backgroundColor = .bgWhite.withAlphaComponent(0.38)
+        }
+    
     // MARK: - Properties
     static let identifier = "BookmarkFeedCell"
     
+    // MARK: - Life Cycles
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+    }
+    
+    // MARK: - Functions
+    
+    override func render() {
+        self.addSubViews([imageView, bookmarkButton])
+        
+        imageView.snp.makeConstraints { make in
+            make.top.leading.bottom.trailing.equalToSuperview()
+        }
+        
+        bookmarkButton.snp.makeConstraints { make in
+            make.bottom.trailing.equalToSuperview().inset(6)
+            make.width.height.equalTo(36)
+        }
+    }
+    
+    override func configUI() {
+        self.clipsToBounds = true
+        self.layer.cornerRadius = 8
+    }
+    
+    func bind(to viewModel: PoseFeedPhotoCellViewModel) {
+        viewModel.image.bind(to: imageView.rx.image).disposed(by: disposeBag)
+    }
 }
