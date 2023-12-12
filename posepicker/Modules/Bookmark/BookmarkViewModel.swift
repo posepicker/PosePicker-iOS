a11f14dbcc2d89a24cfa74fda527b0727e243ec1
Parkjju <rudwns3927@gmail.com>
2023-12-12 22:44:10 +0900

    feat: 북마크 뷰모델 retrieveCache isFilterSection 파라미터 삭제 / lodable 객체 추가

--- posepicker/Modules/Bookmark/BookmarkViewModel.swift
+++ posepicker/Modules/Bookmark/BookmarkViewModel.swift
@@ -17,8 +17,8 @@ class BookMarkViewModel: ViewModelType {
     var disposeBag = DisposeBag()
     var imageDownloader: ImageDownloader
     
-    var filteredContentSizes = BehaviorRelay<[CGSize]>(value: [])
-    var recommendedContentsSizes = BehaviorRelay<[CGSize]>(value: [])
+    var bookmarkContentSizes = BehaviorRelay<[CGSize]>(value: [])
+    let bookmarkToPosefeedButtonTrigger = PublishSubject<Void>()
     
     var currentPage = 0
     var isLast = false
@@ -27,11 +27,30 @@ class BookMarkViewModel: ViewModelType {
     /// 포즈피드 컬렉션뷰 datasource 정의
     lazy var dataSource = RxCollectionViewSectionedReloadDataSource<BookmarkSection>(configureCell: { dataSource, collectionView, indexPath, item in
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BookmarkFeedCell.identifier, for: indexPath) as? BookmarkFeedCell else { return UICollectionViewCell() }
+        cell.bind(to: item)
+        
+//        cell.bookmarkButton.rx.tap
+//            .subscribe(onNext: { [unowned self] in
+//                if let _ = try? KeychainManager.shared.retrieveItem(ofClass: .password, key: K.Parameters.userId) {
+//                    // API요청 보내기
+//                    self.bookmarkButtonTapped.onNext(item.poseId.value)
+//                } else {
+//                    self.presentLoginPopUp.onNext(())
+//                }
+//            })
+//            .disposed(by: self.disposeBag)
         
         return cell
     }, configureSupplementaryView: { dataSource, collectionView, kind, indexPath -> UICollectionReusableView in
         if indexPath.section == 0 {
             let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: BookmarkEmptyView.identifier, for: indexPath) as! BookmarkEmptyView
+            
+            header.transitionButton.rx.tap
+                .subscribe(onNext: { [unowned self] in
+                    self.bookmarkToPosefeedButtonTrigger.onNext(())
+                })
+                .disposed(by: self.disposeBag)
+            
             return header
         } else { return UICollectionReusableView() }
     })
@@ -49,41 +68,34 @@ class BookMarkViewModel: ViewModelType {
     struct Output {
         let sectionItems: Observable<[BookmarkSection]>
         let isEmpty: Driver<Bool?>
+        let isLoading: Observable<Bool>
+        let transitionToPoseFeed: Observable<Void>
     }
     
     func transform(input: Input) -> Output {
         let isEmpty = BehaviorRelay<Bool?>(value: nil)
         let sectionItems = BehaviorRelay<[BookmarkSection]>(value: [BookmarkSection(header: "", items: [])])
+        let loadable = BehaviorRelay<Bool>(value: false)
         
         /// 1. 뷰 로드 이후 컬렉션뷰 셀 아이템 API 요청
         input.viewDidLoadTrigger
             .flatMapLatest { [unowned self] _ -> Observable<PoseFeed> in
-                return apiSession.requestSingle(.retrieveBookmarkFeed(userId: 0, pageNumber: 0, pageSize: 8)).asObservable()
-            }
-            .map { $0.content }
-            .flatMapLatest { [unowned self] posefeed -> Observable<[BookmarkFeedCellViewModel]> in
-                return self.retrieveCacheObservable(posefeed: posefeed)
-            }
-            .subscribe(onNext: {
-                var items = sectionItems.value.first?.items ?? []
-                items.append(contentsOf: $0)
-                let newSection = BookmarkSection(header: "", items: items)
+                self.beginLoading()
+                loadable.accept(true)
+                if let userIdString = try? KeychainManager.shared.retrieveItem(ofClass: .password, key: K.Parameters.userId),
+                   let userId = Int64(userIdString)
+                {
+                    return apiSession.requestSingle(.retrieveBookmarkFeed(userId: userId, pageNumber: 0, pageSize: 8)).asObservable()
+                } else {
+                    return Observable<PoseFeed>.empty()
+                }
                 
-                sectionItems.accept([newSection])
-                isEmpty.accept($0.isEmpty ? true : false)
-            })
-            .disposed(by: disposeBag)
-        
-        /// 2. 무한스크롤 다음 페이지 트리거
-        input.nextPageTrigger
-            .flatMapLatest { [unowned self] _ -> Observable<PoseFeed> in
-                self.isLoading = true
-                return apiSession.requestSingle(.retrieveBookmarkFeed(userId: 0, pageNumber: 0, pageSize: 8)).asObservable()
             }
             .map { [unowned self] in
                 self.currentPage = $0.pageable.pageNumber
                 self.isLast = $0.last
-                self.isLoading = false
+                self.endLoading()
+                loadable.accept(false)
                 return $0.content
             }
             .flatMapLatest { [unowned self] posefeed -> Observable<[BookmarkFeedCellViewModel]> in
@@ -93,13 +105,38 @@ class BookMarkViewModel: ViewModelType {
                 var items = sectionItems.value.first?.items ?? []
                 items.append(contentsOf: $0)
                 let newSection = BookmarkSection(header: "", items: items)
-                
+                sectionItems.accept([newSection])
+                isEmpty.accept($0.isEmpty ? true : false)
+            })
+            .disposed(by: disposeBag)
+        
+        /// 2. 무한스크롤 다음 페이지 트리거
+        input.nextPageTrigger
+            .flatMapLatest { [unowned self] _ -> Observable<PoseFeed> in
+                self.beginLoading()
+                loadable.accept(true)
+                return apiSession.requestSingle(.retrieveBookmarkFeed(userId: 0, pageNumber: 0, pageSize: 8)).asObservable()
+            }
+            .map { [unowned self] in
+                self.currentPage = $0.pageable.pageNumber
+                self.isLast = $0.last
+                self.endLoading()
+                loadable.accept(false)
+                return $0.content
+            }
+            .flatMapLatest { [unowned self] posefeed -> Observable<[BookmarkFeedCellViewModel]> in
+                return self.retrieveCacheObservable(posefeed: posefeed)
+            }
+            .subscribe(onNext: {
+                var items = sectionItems.value.first?.items ?? []
+                items.append(contentsOf: $0)
+                let newSection = BookmarkSection(header: "", items: items)
                 sectionItems.accept([newSection])
                 isEmpty.accept(true)
             })
             .disposed(by: disposeBag)
         
-        return Output(sectionItems: sectionItems.asObservable(), isEmpty: isEmpty.asDriver())
+        return Output(sectionItems: sectionItems.asObservable(), isEmpty: isEmpty.asDriver(), isLoading: loadable.asObservable(), transitionToPoseFeed: self.bookmarkToPosefeedButtonTrigger)
     }
     
     // MARK: - 킹피셔 이미지 캐싱 관련 함수들
@@ -111,7 +148,7 @@ class BookMarkViewModel: ViewModelType {
     }
     
     // 메인 스케줄러에서 처리
-    func retrieveCacheObservable(posefeed: [PosePick], isFilterSection: Bool = true) -> Observable<[BookmarkFeedCellViewModel]> {
+    func retrieveCacheObservable(posefeed: [PosePick]) -> Observable<[BookmarkFeedCellViewModel]> {
         
         let viewModelObservable = BehaviorRelay<[BookmarkFeedCellViewModel]>(value: [])
         
@@ -122,7 +159,7 @@ class BookMarkViewModel: ViewModelType {
                 case .success(let value):
                     if let image = value.image {
                         let newSizeImage = self.newSizeImageWidthDownloadedResource(image: image)
-                        isFilterSection ? self.filteredContentSizes.accept(self.filteredContentSizes.value + [newSizeImage.size]) : self.recommendedContentsSizes.accept(self.recommendedContentsSizes.value + [newSizeImage.size])
+                         self.bookmarkContentSizes.accept(self.bookmarkContentSizes.value + [newSizeImage.size])
                         
                         let viewModel = BookmarkFeedCellViewModel(image: newSizeImage, poseId: posepick.poseInfo.poseId)
                         viewModelObservable.accept(viewModelObservable.value + [viewModel])
@@ -132,7 +169,7 @@ class BookMarkViewModel: ViewModelType {
                             switch downloadResult {
                             case .success(let downloadImage):
                                 let newSizeImage = self.newSizeImageWidthDownloadedResource(image: downloadImage.image)
-                                isFilterSection ? self.filteredContentSizes.accept(self.filteredContentSizes.value + [newSizeImage.size]) : self.recommendedContentsSizes.accept(self.recommendedContentsSizes.value + [newSizeImage.size])
+                                 self.bookmarkContentSizes.accept(self.bookmarkContentSizes.value + [newSizeImage.size])
                                 
                                 let viewModel = BookmarkFeedCellViewModel(image: newSizeImage, poseId: posepick.poseInfo.poseId)
                                 viewModelObservable.accept(viewModelObservable.value + [viewModel])
@@ -147,5 +184,14 @@ class BookMarkViewModel: ViewModelType {
             }
         }
         return viewModelObservable.asObservable().skip(while: { $0.count < posefeed.count })
+    }
+    
+    /// 로딩상태 업데이트
+    func beginLoading() {
+        self.isLoading = true
+    }
+    
+    func endLoading() {
+        self.isLoading = false
     }
 }
