a54f450d18ac671088298c8605022e3079002458
Parkjju <rudwns3927@gmail.com>
2023-12-13 04:05:54 +0900

    feat: 포즈피드 코디네이터 북마크 dismiss 로직 구현

--- posepicker/Modules/PoseFeed/PoseFeedCoordinator.swift
+++ posepicker/Modules/PoseFeed/PoseFeedCoordinator.swift
@@ -41,6 +41,40 @@ class PoseFeedCoordinator: NSObject, Coordinator {
         self.navigationController.present(viewController, animated: true)
     }
     
+    func dismissBookmarkDetailWithTagSelection(tag: String) {
+        guard let root = self.navigationController.viewControllers.first as? RootViewController,
+              let navigationVC = root.viewControllers.last as? UINavigationController,
+              let posefeedViewController = navigationVC.viewControllers.first as? PoseFeedViewController else { return }
+        root.currentPage = 2
+        posefeedViewController.loadViewIfNeeded()
+        
+        // 셀렉션 초기화
+        self.poseFeedFilterViewController.loadViewIfNeeded()
+        self.poseFeedFilterViewController.detailViewDismissTrigger.onNext(())
+        self.poseFeedFilterViewController.registeredSubTag.accept(nil)
+        self.poseFeedFilterViewController.subTagRemoveTrigger.onNext(())
+        
+        if let _ = PeopleCountTags.getTagFromTitle(title: tag) {
+            // 1. 인원 수 태그인 경우
+            guard let peopleCountIndex = PeopleCountTags.getNumberFromPeopleCountString(countString: tag) else { return }
+            self.poseFeedFilterViewController.headCountSelection.buttonTapTrigger
+                .accept(peopleCountIndex)
+        } else if let _ = FrameCountTags.getTagFromTitle(title: tag) {
+            // 2. 프레임 수 태그인 경우
+            guard let frameCountIndex = FrameCountTags.getIndexFromFrameCountString(countString: tag) else { return }
+            self.poseFeedFilterViewController.frameCountSelection.buttonTapTrigger.accept(frameCountIndex)
+        } else if let filteredTag = FilterTags.getTagFromTitle(title: tag) {
+            self.poseFeedFilterViewController.filteredTagAfterDismiss.accept(filteredTag)
+        } else {
+            // 3. 서브태그인 경우
+            self.poseFeedFilterViewController.registeredSubTag.accept(tag)
+        }
+        posefeedViewController.modalDismissWithTag.onNext(tag)
+        posefeedViewController.registerButtonTapped.onNext(())
+        
+        self.navigationController.popViewController(animated: true)
+    }
+    
     func dismissPoseDetailWithTagSelection(tag: String) {
         guard let posefeedViewController = self.navigationController.viewControllers.first as? PoseFeedViewController else { return }
         
