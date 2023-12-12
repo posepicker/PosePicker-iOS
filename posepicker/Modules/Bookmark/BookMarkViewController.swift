2b6e0039e480fec3258a42127aaa3dc4db494de2
Parkjju <rudwns3927@gmail.com>
2023-12-12 17:51:51 +0900

    feat: 북마크 섹션 아이템 데이터소스 바인딩 /  북마크 컬렉션뷰 서브뷰 등록 & 핀터레스트 레이아웃 객체 델리게이트 연결

--- posepicker/Modules/Bookmark/BookMarkViewController.swift
+++ posepicker/Modules/Bookmark/BookMarkViewController.swift
@@ -15,6 +15,21 @@ class BookMarkViewController: BaseViewController {
     
     let emptyView = EmptyBookmarkView()
     
+    lazy var bookmarkCollectionView: UICollectionView = {
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: self.pinterestLayout)
+        cv.register(BookmarkFeedCell.self, forCellWithReuseIdentifier: BookmarkFeedCell.identifier)
+        cv.register(BookmarkEmptyView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: BookmarkEmptyView.identifier)
+        cv.rx.setDelegate(self).disposed(by: disposeBag)
+        return cv
+    }()
+    
+    lazy var pinterestLayout = PinterestLayout()
+        .then {
+            $0.headerReferenceSize = .init(width: UIScreen.main.bounds.width, height: 50)
+            $0.delegate = self
+            $0.scrollDirection = .vertical
+        }
+    
     // MARK: - Properties
     var viewModel: BookMarkViewModel
     var coordinator: RootCoordinator
@@ -65,16 +80,49 @@ class BookMarkViewController: BaseViewController {
                 self.coordinator.moveWithPage(page: .posefeed, direction: .reverse)
             })
             .disposed(by: disposeBag)
+        
+        /// 북마크 무한스크롤
+//        poseFeedCollectionView.rx.contentOffset
+//            .subscribe(onNext: { [unowned self] in
+//                if $0.y > self.poseFeedCollectionView.contentSize.height - self.poseFeedCollectionView.bounds.size.height && !self.viewModel.isLoading && !self.viewModel.isLast {
+//                    self.nextPageRequestTrigger.onNext(())
+//                }
+//            })
+//            .disposed(by: disposeBag)
     }
     
     override func bindViewModel() {
         let input = BookMarkViewModel.Input(viewDidLoadTrigger: viewDidLoadTrigger, nextPageTrigger: nextPageTrigger)
         let output = viewModel.transform(input: input)
+        
+        output.sectionItems
+            .bind(to: bookmarkCollectionView.rx.items(dataSource: viewModel.dataSource))
+            .disposed(by: disposeBag)
     }
     
     // MARK: - Objc Functions
     @objc
     func backButtonTapped() {
         navigationController?.popViewController(animated: true)
+    }
+}
+
+extension BookMarkViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if collectionView == bookmarkCollectionView {
+            return viewModel.filteredContentSizes.value[indexPath.item]
+        }
+        return CGSize(width: 60, height: 30)
+    }
+}
+
+
+extension BookMarkViewController: PinterestLayoutDelegate {
+    func collectionView(_ collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 {
+            return viewModel.filteredContentSizes.value[indexPath.item].height
+        } else {
+            return viewModel.recommendedContentsSizes.value[indexPath.item].height
+        }
     }
 }
