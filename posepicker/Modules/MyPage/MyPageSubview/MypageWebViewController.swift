b9f6be8353dec64392085425995b7971b23356df
Parkjju <rudwns3927@gmail.com>
2024-01-06 15:42:56 +0900

    feat: 웹뷰 컨트롤러 추가 / 뷰 타이틀 & URL 주입

--- posepicker/Modules/MyPage/MyPageSubview/MypageWebViewController.swift
+++ posepicker/Modules/MyPage/MyPageSubview/MypageWebViewController.swift
@@ -6,24 +6,74 @@
 //
 
 import UIKit
+import WebKit
 
-class MypageWebViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class MypageWebViewController: BaseViewController {
+    
+    // MARK: - Subviews
+    private var webView: WKWebView!
+    
+    private let activityView = UIActivityIndicatorView(style: .large)
+        .then {
+            $0.startAnimating()
+            $0.color = .mainViolet
+        }
+    
+    // MARK: - Properties
+    var urlString: String
+    var pageTitle: String
+    
+    // MARK: - Initialization
+    init(urlString: String, pageTitle: String) {
+        self.urlString = urlString
+        self.pageTitle = pageTitle
+        super.init()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
+    
+    // MARK: - Functions
+    
+    override func configUI() {
+        webView.navigationDelegate = self
+        
+        let url = URL(string: urlString)!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
+        view.backgroundColor = .bgWhite
+        
+        let backButton = UIBarButtonItem(image: ImageLiteral.imgArrowBack24.withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(backButtonTapped))
+        self.navigationItem.leftBarButtonItem = backButton
+        self.title = pageTitle
+    }
+    
+    override func render() {
+        webView = WKWebView()
+        
+        self.view.addSubViews([webView, activityView])
+        
+        webView.snp.makeConstraints { make in
+            make.top.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        activityView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Objc Functions
+    @objc
+    func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
 
+}
+
+extension MypageWebViewController: WKNavigationDelegate { 
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        activityView.stopAnimating()
+    }
 }
