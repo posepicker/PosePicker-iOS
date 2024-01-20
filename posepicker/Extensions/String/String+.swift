4cc25205fa61c42077c12fe5fb17e9ec5a4dc2e1
Parkjju <rudwns3927@gmail.com>
2024-01-20 23:09:43 +0900

    feat: v0.1.0-beta String 폰트 전달 후 측정 높이값 너비값 반환 함수 정의

---
+++ posepicker/Extensions/String/String+.swift
@@ -0,0 +1,24 @@
+//
+//  String+.swift
+//  posepicker
+//
+//  Created by 박경준 on 1/20/24.
+//
+
+import UIKit
+
+extension String {
+    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+    
+        return ceil(boundingBox.height)
+    }
+
+    func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+
+        return ceil(boundingBox.width)
+    }
+}
