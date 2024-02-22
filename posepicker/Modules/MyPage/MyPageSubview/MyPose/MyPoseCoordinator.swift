c3e1ad4d30bb159d478cfee3e03dce950b944482
Parkjju <rudwns3927@gmail.com>
2024-02-22 23:50:46 +0900

    feat: 마이포즈 뷰 컨트롤러 UI 정의 (이미지 & 버튼)

---
+++ posepicker/Modules/MyPage/MyPageSubview/MyPose/MyPoseCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  MyPoseCoordinator.swift
+//  posepicker
+//
+//  Created by 박경준 on 2/22/24.
+//
+
+import Foundation
--- posepicker/Modules/MyPage/MyPageSubview/MyPose/MyPoseViewController.swift
+++ posepicker/Modules/MyPage/MyPageSubview/MyPose/MyPoseViewController.swift
@@ -9,4 +9,46 @@ import UIKit
 
 class MyPoseViewController: BaseViewController {
     
+    // MARK: - Subviews
+    lazy var registeredImageView = UIImageView(image: self.registeredImage)
+        .then {
+            $0.contentMode = .scaleAspectFit
+        }
+    
+    let nextButton = PosePickButton(status: .defaultStatus, isFill: true, position: .none, buttonTitle: "다음", image: nil)
+    
+    // MARK: - Properties
+    let registeredImage: UIImage?
+    
+    // MARK: - Initialization
+    init(registeredImage: UIImage?) {
+        self.registeredImage = registeredImage
+        super.init()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Functions
+    override func render() {
+        view.addSubViews([registeredImageView, nextButton])
+        
+        registeredImageView.snp.makeConstraints { make in
+            make.width.equalTo(120)
+            make.height.equalTo(160)
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(nextButton.snp.top).offset(-27)
+        }
+        
+        nextButton.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(18.5)
+            make.height.equalTo(60)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-16)
+        }
+    }
+    
+    override func configUI() {
+        view.backgroundColor = .bgWhite
+    }
 }
