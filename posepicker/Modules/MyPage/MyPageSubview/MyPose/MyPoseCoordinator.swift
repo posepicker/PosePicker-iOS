f6b553ba27319c1e959f3dfab15d83c60ec8628e
Parkjju <rudwns3927@gmail.com>
2024-02-22 23:51:05 +0900

    feat: 마이포즈 코디네이터 정의

--- posepicker/Modules/MyPage/MyPageSubview/MyPose/MyPoseCoordinator.swift
+++ posepicker/Modules/MyPage/MyPageSubview/MyPose/MyPoseCoordinator.swift
@@ -5,4 +5,19 @@
 //  Created by 박경준 on 2/22/24.
 //
 
-import Foundation
+import UIKit
+
+class MyPoseCoordinator: NSObject, Coordinator {
+    
+    var childCoordinators: [Coordinator] = []
+    var navigationController: UINavigationController
+    
+    // MARK: - Initialization
+    
+    required init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    // MARK: - Functions
+    func start() { }
+}
