c4d9efcaceffb8fe3b5184acbc2bbab298b52a62
Parkjju <rudwns3927@gmail.com>
2024-02-23 00:18:18 +0900

    feat: 커스텀 세그먼트 UI 로직 구성

--- posepicker/Modules/MyPage/MyPageSubview/MyPose/MyPoseViewController.swift
+++ posepicker/Modules/MyPage/MyPageSubview/MyPose/MyPoseViewController.swift
@@ -17,6 +17,16 @@ class MyPoseViewController: BaseViewController {
     
     let nextButton = PosePickButton(status: .defaultStatus, isFill: true, position: .none, buttonTitle: "다음", image: nil)
     
+    let buttons = [CircleSegmentButton(title: "1", isCurrent: true), CircleSegmentButton(title: "2"), CircleSegmentButton(title: "3"), CircleSegmentButton(title: "4")]
+    
+    lazy var pageButtons = UIStackView(arrangedSubviews: self.buttons)
+        .then {
+            $0.distribution = .fillEqually
+            $0.alignment = .center
+            $0.axis = .horizontal
+            $0.spacing = 12
+        }
+    
     // MARK: - Properties
     let registeredImage: UIImage?
     
@@ -32,7 +42,14 @@ class MyPoseViewController: BaseViewController {
     
     // MARK: - Functions
     override func render() {
-        view.addSubViews([registeredImageView, nextButton])
+        view.addSubViews([pageButtons, registeredImageView, nextButton])
+        
+        pageButtons.snp.makeConstraints { make in
+            make.height.equalTo(24)
+            make.width.equalTo(132)
+            make.leading.equalToSuperview().offset(20)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(48)
+        }
         
         registeredImageView.snp.makeConstraints { make in
             make.width.equalTo(120)
@@ -50,5 +67,20 @@ class MyPoseViewController: BaseViewController {
     
     override func configUI() {
         view.backgroundColor = .bgWhite
+        
+        buttons.forEach { [weak self] button in
+            guard let self = self else { return }
+            
+            button.rx.tap
+                .subscribe(onNext: {
+                    self.resetButtonUI()
+                    button.isCurrent = true
+                })
+                .disposed(by: self.disposeBag)
+        }
+    }
+    
+    func resetButtonUI() {
+        buttons.forEach { $0.isCurrent = false }
     }
 }
