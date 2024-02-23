efa2ce389a62dde381e20c249b4dd74d78ee16b0
Parkjju <rudwns3927@gmail.com>
2024-02-23 17:36:30 +0900

    feat: 프레임 카운트 버튼 그룹 추가

--- posepicker/Modules/MyPage/MyPageSubview/MyPose/MyPosePages/MyPoseFramecountViewController.swift
+++ posepicker/Modules/MyPage/MyPageSubview/MyPose/MyPosePages/MyPoseFramecountViewController.swift
@@ -20,6 +20,8 @@ class MyPoseFramecountViewController: BaseViewController {
             $0.textAlignment = .left
         }
     
+    let framecountButtons: [MyPoseSelectButton] = [MyPoseSelectButton(title: "1컷", isCurrent: true), MyPoseSelectButton(title: "3컷"), MyPoseSelectButton(title: "4컷"), MyPoseSelectButton(title: "6컷"), MyPoseSelectButton(title: "8컷 이상")]
+    
     lazy var registeredImageView = UIImageView(image: self.registeredImage)
         .then {
             $0.contentMode = .scaleAspectFit
@@ -43,11 +45,41 @@ class MyPoseFramecountViewController: BaseViewController {
     // MARK: - Functions
     
     override func render() {
-        view.addSubViews([mainLabel, registeredImageView, nextButton])
+        let firstLineButtons = UIStackView(arrangedSubviews: [framecountButtons[0], framecountButtons[1], framecountButtons[2]])
+            .then {
+                $0.axis = .horizontal
+                $0.alignment = .fill
+                $0.distribution = .fillEqually
+                $0.spacing = 12
+            }
+        
+        let secondLineButtons = UIStackView(arrangedSubviews: [framecountButtons[3], framecountButtons[4]])
+            .then {
+                $0.axis = .horizontal
+                $0.alignment = .fill
+                $0.distribution = .fillEqually
+                $0.spacing = 12
+            }
+        
+        view.addSubViews([mainLabel, firstLineButtons, secondLineButtons, registeredImageView, nextButton])
         
         mainLabel.snp.makeConstraints { make in
             make.top.equalToSuperview()
             make.leading.equalToSuperview().offset(20)
+        }
+        
+        firstLineButtons.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+            make.top.equalTo(mainLabel.snp.bottom).offset(36)
+            make.height.equalTo(108)
+        }
+        
+        secondLineButtons.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalTo(framecountButtons[1])
+            make.top.equalTo(firstLineButtons.snp.bottom).offset(12)
+            make.height.equalTo(108)
         }
         
         registeredImageView.snp.makeConstraints { make in
@@ -57,6 +89,8 @@ class MyPoseFramecountViewController: BaseViewController {
             make.bottom.equalTo(nextButton.snp.top).offset(-27)
         }
         
+        
+        
         nextButton.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview().inset(18.5)
             make.height.equalTo(60)
@@ -65,6 +99,20 @@ class MyPoseFramecountViewController: BaseViewController {
     }
     
     override func configUI() {
-        
+        framecountButtons.enumerated().forEach { [weak self] index, button in
+            guard let self = self else { return }
+            
+            button.rx.tap
+                .subscribe(onNext: {
+                    self.resetButtonUI()
+                    button.isCurrent = true
+                })
+                .disposed(by: self.disposeBag)
+        }
+        framecountButtons[0].isCurrent = true
+    }
+    
+    func resetButtonUI() {
+        framecountButtons.forEach { $0.isCurrent = false }
     }
 }
