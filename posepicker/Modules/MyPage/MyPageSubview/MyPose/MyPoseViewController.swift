804f4e3c57d1274acc7e95896a3de617a0a39d82
Parkjju <rudwns3927@gmail.com>
2024-02-23 14:24:35 +0900

    feat: 마이포즈 뷰 세그먼트 & 페이지뷰 로직 연결

--- posepicker/Modules/MyPage/MyPageSubview/MyPose/MyPoseViewController.swift
+++ posepicker/Modules/MyPage/MyPageSubview/MyPose/MyPoseViewController.swift
@@ -10,12 +10,6 @@ import UIKit
 class MyPoseViewController: BaseViewController {
     
     // MARK: - Subviews
-    lazy var registeredImageView = UIImageView(image: self.registeredImage)
-        .then {
-            $0.contentMode = .scaleAspectFit
-        }
-    
-    let nextButton = PosePickButton(status: .defaultStatus, isFill: true, position: .none, buttonTitle: "다음", image: nil)
     
     let buttons = [CircleSegmentButton(title: "1", isCurrent: true), CircleSegmentButton(title: "2"), CircleSegmentButton(title: "3"), CircleSegmentButton(title: "4")]
     
@@ -27,8 +21,38 @@ class MyPoseViewController: BaseViewController {
             $0.spacing = 12
         }
     
+    lazy var pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+        .then {
+            $0.setViewControllers([self.viewControllers[0]], direction: .forward, animated: true)
+            $0.dataSource = self
+            $0.delegate = self
+        }
+    
     // MARK: - Properties
     let registeredImage: UIImage?
+    
+    lazy var viewControllers: [UIViewController] = [
+        MyPoseHeadcountViewController(registeredImage: self.registeredImage),
+        MyPoseFramecountViewController(registeredImage: self.registeredImage),
+        MyPoseTagViewController(registeredImage: self.registeredImage),
+        MyPoseImageSourceViewController(registeredImage: self.registeredImage),
+    ]
+    
+    var currentPage: Int = 0 {
+        didSet {
+            // from segmentedControl -> pageViewController 업데이트
+            let direction: UIPageViewController.NavigationDirection = oldValue <= self.currentPage ? .forward : .reverse
+            self.pageViewController.setViewControllers(
+                [self.viewControllers[self.currentPage]],
+                direction: direction,
+                animated: true,
+                completion: nil
+            )
+            resetButtonUI()
+            buttons[currentPage].isCurrent = true
+        }
+    }
+
     
     // MARK: - Initialization
     init(registeredImage: UIImage?) {
@@ -42,7 +66,7 @@ class MyPoseViewController: BaseViewController {
     
     // MARK: - Functions
     override func render() {
-        view.addSubViews([pageButtons, registeredImageView, nextButton])
+        view.addSubViews([pageButtons, pageViewController.view])
         
         pageButtons.snp.makeConstraints { make in
             make.height.equalTo(24)
@@ -51,36 +75,58 @@ class MyPoseViewController: BaseViewController {
             make.top.equalTo(view.safeAreaLayoutGuide).offset(48)
         }
         
-        registeredImageView.snp.makeConstraints { make in
-            make.width.equalTo(120)
-            make.height.equalTo(160)
-            make.centerX.equalToSuperview()
-            make.bottom.equalTo(nextButton.snp.top).offset(-27)
-        }
-        
-        nextButton.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview().inset(18.5)
-            make.height.equalTo(60)
-            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-16)
+        pageViewController.view.snp.makeConstraints { make in
+            make.top.equalTo(pageButtons.snp.bottom).offset(36)
+            make.leading.trailing.bottom.equalToSuperview()
         }
     }
     
     override func configUI() {
         view.backgroundColor = .bgWhite
         
-        buttons.forEach { [weak self] button in
+        buttons.enumerated().forEach { [weak self] (index, button) in
             guard let self = self else { return }
             
             button.rx.tap
                 .subscribe(onNext: {
                     self.resetButtonUI()
                     button.isCurrent = true
+                    self.currentPage = index
                 })
                 .disposed(by: self.disposeBag)
         }
+        buttons[0].isCurrent = true
     }
     
     func resetButtonUI() {
         buttons.forEach { $0.isCurrent = false }
+    }
+}
+
+extension MyPoseViewController: UIPageViewControllerDelegate {
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        didFinishAnimating finished: Bool,
+        previousViewControllers: [UIViewController],
+        transitionCompleted completed: Bool
+    ) {
+        guard let viewController = pageViewController.viewControllers?[0],
+              let index = viewControllers.firstIndex(of: viewController) else { return }
+        currentPage = index
+    }
+}
+
+extension MyPoseViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let index = self.viewControllers.firstIndex(of: viewController),
+              index - 1 >= 0 else { return nil}
+        return self.viewControllers[index - 1]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let index = self.viewControllers.firstIndex(of: viewController),
+              index + 1 < viewControllers.count else { return nil }
+        return self.viewControllers[index + 1]
     }
 }
