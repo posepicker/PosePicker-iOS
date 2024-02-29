9ab60d09edcb52b4a728ae38c107592303450069
Parkjju <rudwns3927@gmail.com>
2024-02-29 20:56:58 +0900

    feat: UITextField 좌측 패딩값 부여 익스텐션 함수 추가

---
+++ posepicker/Extensions/UITextField/UITextField+.swift
@@ -0,0 +1,16 @@
+//
+//  UITextField+.swift
+//  posepicker
+//
+//  Created by 박경준 on 2/29/24.
+//
+
+import UIKit
+
+extension UITextField {
+    func addLeftPadding(width: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = ViewMode.always
+    }
+}
