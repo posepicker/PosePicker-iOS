3744a5c9b8beb08420e0fc7c8f4ec2663a75fd72
Parkjju <rudwns3927@gmail.com>
2024-02-29 17:28:33 +0900

    feat: 주석 삭제 / 알파값 조정 옵저버블 속성 추가 / Pinch & PanGesture UI로직 구현 + 예외처리

--- posepicker/Extensions/UIImageView/UIImageView+.swift
+++ posepicker/Extensions/UIImageView/UIImageView+.swift
@@ -31,162 +31,9 @@ extension UIImageView {
     }
 }
 
-extension UIImageView{
-//    @objc func startZooming(_ sender: UIPinchGestureRecognizer){
-//        
-//        if(sender.view!.transform.a > 0.6){
-//            let scaleResult = sender.view?.transform.scaledBy(x: sender.scale, y: sender.scale)
-//            
-//            guard let scale = scaleResult else { return }
-//            sender.view?.transform = scale
-//            sender.scale = 1
-//        }
-//        
-//        if(sender.state == .ended){
-////            checkImageOriginIsZero(sender)
-//        }
-//        
-//    }
-//    
-//    @objc func startDragging(_ sender: UIPanGestureRecognizer){
-//        // 이동량을 더하는 형태 -> 계속 누적되어 목표값을 넘어서게됨
-//        // 직전의 translate값을 저장
-//        // 변화하는 상태값을 계속 트래킹하며 이미지뷰 origin위치를 변경해줘야함
-//        print(sender.location(in: sender.view))
-//        sender.view!.center.x += sender.translation(in: sender.view).x
-//        sender.view!.center.y += sender.translation(in: sender.view).y
-//        sender.setTranslation(.zero, in: sender.view)
-//        
-//        if(sender.state == .ended){
-//            checkImageOriginIsZero(sender)
-//        }
-//    }
-    
-    /// 팬제스처 종료 이후 제자리로 돌아갈지 여부를 판단하여 동작하는 함수
-    /// 너비 확대 및 축소가 이루어진 경우 origin.x값은 제자리를 벗어날 수 밖에 없게됨
-    /// 너비 확대가 이루어지지 않은 경우부터 체크
-//    func checkImageOriginIsZero(_ sender: UIPanGestureRecognizer){
-//        
-//        /// 너비 변환이 이루어졌을때
-//        if sender.view!.transform.a == 1 {
-//            
-//            /// 왼쪽 끝이 화면 왼쪽 끝에서 안으로 들어온 경우
-//            /// 오른쪽 끝이 화면 오른쪽 끝에서 안으로 들어온 경우
-//            if sender.view!.frame.origin.x > 0 || sender.view!.frame.maxX < sender.view!.superview!.frame.maxX {
-//                UIView.animate(withDuration: 0.3) {
-//                    sender.view!.center.x = sender.view!.frame.width / 2
-//                }
-//            }
-//            
-//            /// 기본 위치에서 상하로 위치 이동한 경우
-//            /// 아래 특정기준 포인트로 넘어갈수록 뒷배경 하얗게 만들고 & dismiss 처리 해야됨
-//            if sender.view!.center.y != sender.view!.superview!.center.y {
-//                UIView.animate(withDuration: 0.3) {
-//                    sender.view!.center = sender.view!.superview!.center
-//                }
-//            }
-//        }
-        
-//        if(sender.view!.transform.a != 1){
-//            // 프레임이 왼쪽경계보다 안쪽으로 당겨질때
-//            if(sender.view!.frame.origin.x > 0){
-//                UIView.animate(withDuration: 0.3) {
-//                    sender.view!.center.x -= sender.view!.frame.origin.x
-//                }
-//            }
-//            
-//            // 프레임이 위쪽경계보다 안쪽으로 당겨질때
-//            if(sender.view!.frame.origin.y > 0  ){
-//                UIView.animate(withDuration: 0.3) {
-//                    sender.view!.center.y -= sender.view!.frame.origin.y
-//                }
-//            }
-//            
-//            // 프레임이 오른쪽 경계보다 안쪽으로 당겨질때 - 이미지 wrapper UIView 너비로 체크
-//            if(sender.view!.frame.maxX < sender.view!.superview!.frame.width){
-//                UIView.animate(withDuration: 0.3) {
-//                    sender.view!.center.x += (sender.view!.superview!.frame.width - sender.view!.frame.maxX)
-//                }
-//            }
-//            
-//            // 프레임이 아래쪽 경계보다 안쪽으로 당겨질때 - 이미지 wrapper UIView 높이로 체크
-//            if(sender.view!.frame.maxY < sender.view!.frame.width){
-//                
-//                UIView.animate(withDuration: 0.3) {
-//                    sender.view!.center.y += (sender.view!.superview!.frame.height - sender.view!.frame.maxY)
-//                }
-//            }
-//            
-//        }
-//        // 아핀변환 없이 스케일링이 잘 맞춰졌을때
-//        else{
-//            if(sender.view!.frame.origin.x < 0){
-//                UIView.animate(withDuration: 0.3) {
-//                    sender.view!.center.x = sender.view!.frame.width / 2
-//                }
-//            }
-//            
-//            if(sender.view!.frame.maxX > sender.view!.frame.width){
-//                UIView.animate(withDuration: 0.3) {
-//                    sender.view!.center.x = sender.view!.frame.width / 2
-//                }
-//            }
-//            
-//            if(sender.view!.frame.maxY < sender.view!.frame.width){
-//                UIView.animate(withDuration: 0.3) {
-//                    sender.view!.center.y += sender.view!.frame.width - sender.view!.frame.maxY
-//                }
-//            }
-//            
-//            if(sender.view!.frame.minY > 0){
-//                UIView.animate(withDuration: 0.3) {
-//                    sender.view!.center.y -=  sender.view!.frame.minY
-//                }
-//            }
-//        }
-        // 스케일링 되어있는 경우와 그렇지 않은 경우를 구분해서 구현해야함
-
-//    }
-    
-//    func checkImageOriginIsZero(_ sender: UIPinchGestureRecognizer){
-//        if(sender.view!.frame.origin.x > 0 || sender.view!.frame.origin.y > 0 || sender.view!.frame.origin.x < 0 || sender.view!.frame.maxY < sender.view!.frame.width){
-//            
-//            let imageView = sender.view as! UIImageView
-//            
-//            UIView.animate(withDuration: 0.3) {
-//                // PinchGesture 종료 후 너비가 높이보다 큰 경우 ratio계산 후 그 이하로는 이미지 스케일링 더 진행되지 않도록 고정
-//                // CustomPickerViewController 컬렉션뷰 didSelect 프로토콜 메서드에서 이미지뷰 아핀변환값 지정하는 부분 체크
-//                if(imageView.frame.height < imageView.frame.width){
-//                    let ratio = imageView.frame.width / imageView.frame.height
-//                    
-//                    imageView.transform.a = ratio
-//                    imageView.transform.d = ratio
-//                }else{
-//                    imageView.transform.a = 1
-//                    imageView.transform.d = 1
-//                }
-//                
-//                sender.view!.center.x = sender.view!.superview!.frame.width / 2
-//                sender.view!.center.y = sender.view!.superview!.frame.height / 2
-//            }
-//        }
-//    }
-    
-//    func enableZoom(){
-//        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(startZooming))
-//        self.isUserInteractionEnabled = true
-//        self.addGestureRecognizer(pinchGesture)
-//    }
-//    
-//    func enableDrag(){
-//        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(startDragging))
-//        self.isUserInteractionEnabled = true
-//        self.addGestureRecognizer(panGesture)
-//    }
-}
-
 class UIImageViewWithDismissNotification: UIImageView {
     var dismissObservable = BehaviorRelay<Bool>(value: false)
+    var backgroundAlphaObservable = PublishSubject<CGFloat>()
     
     @objc func startZooming(_ sender: UIPinchGestureRecognizer){
         
@@ -194,12 +41,23 @@ class UIImageViewWithDismissNotification: UIImageView {
             let scaleResult = sender.view?.transform.scaledBy(x: sender.scale, y: sender.scale)
             
             guard let scale = scaleResult else { return }
+            
+            /// 2배 이상 확대 시도하는 경우 1.5배 확대로 강제 변경
+            if sender.view!.transform.a >= 2 {
+                UIView.animate(withDuration: 0.3) {
+                    sender.view?.transform.a = 1.5
+                    sender.view?.transform.d = 1.5
+                }
+                sender.state = .ended
+                checkImageOriginIsZero(sender)
+                return
+            }
             sender.view?.transform = scale
             sender.scale = 1
         }
         
         if(sender.state == .ended){
-//            checkImageOriginIsZero(sender)
+            checkImageOriginIsZero(sender)
         }
         
     }
@@ -211,6 +69,9 @@ class UIImageViewWithDismissNotification: UIImageView {
         sender.view!.center.x += sender.translation(in: sender.view).x
         sender.view!.center.y += sender.translation(in: sender.view).y
         sender.setTranslation(.zero, in: sender.view)
+        
+        let bgAlphaRatio = (sender.view!.center.y - sender.view!.superview!.center.y) / 100
+        backgroundAlphaObservable.onNext(bgAlphaRatio > 0 ? bgAlphaRatio : 0)
         
         if(sender.state == .ended){
             checkImageOriginIsZero(sender)
@@ -225,13 +86,15 @@ class UIImageViewWithDismissNotification: UIImageView {
         
         /// 너비 변환이 없는 상태일때
         if sender.view!.transform.a == 1 {
-            
-            
             /// 기본 위치에서 이미지가 아래로 이동한 경우
             /// 100포인트 이상 떨어진 경우 화면 dismiss하도록 수퍼뷰에 노티
             if sender.view!.center.y - sender.view!.superview!.center.y > 100 {
                 dismissObservable.accept(true)
                 return
+            } else if sender.view!.center.y - sender.view!.superview!.center.y <= 100 {
+                UIView.animate(withDuration: 0.3) {
+                    sender.view!.center.y = sender.view!.superview!.center.y
+                }
             }
             
             /// 왼쪽 끝이 화면 왼쪽 끝에서 안으로 들어온 경우
@@ -249,88 +112,65 @@ class UIImageViewWithDismissNotification: UIImageView {
                 }
             }
         }
-        
-//        if(sender.view!.transform.a != 1){
-//            // 프레임이 왼쪽경계보다 안쪽으로 당겨질때
-//            if(sender.view!.frame.origin.x > 0){
-//                UIView.animate(withDuration: 0.3) {
-//                    sender.view!.center.x -= sender.view!.frame.origin.x
-//                }
-//            }
-//
-//            // 프레임이 위쪽경계보다 안쪽으로 당겨질때
-//            if(sender.view!.frame.origin.y > 0  ){
-//                UIView.animate(withDuration: 0.3) {
-//                    sender.view!.center.y -= sender.view!.frame.origin.y
-//                }
-//            }
-//
-//            // 프레임이 오른쪽 경계보다 안쪽으로 당겨질때 - 이미지 wrapper UIView 너비로 체크
-//            if(sender.view!.frame.maxX < sender.view!.superview!.frame.width){
-//                UIView.animate(withDuration: 0.3) {
-//                    sender.view!.center.x += (sender.view!.superview!.frame.width - sender.view!.frame.maxX)
-//                }
-//            }
-//
-//            // 프레임이 아래쪽 경계보다 안쪽으로 당겨질때 - 이미지 wrapper UIView 높이로 체크
-//            if(sender.view!.frame.maxY < sender.view!.frame.width){
-//
-//                UIView.animate(withDuration: 0.3) {
-//                    sender.view!.center.y += (sender.view!.superview!.frame.height - sender.view!.frame.maxY)
-//                }
-//            }
-//
-//        }
-//        // 아핀변환 없이 스케일링이 잘 맞춰졌을때
-//        else{
-//            if(sender.view!.frame.origin.x < 0){
-//                UIView.animate(withDuration: 0.3) {
-//                    sender.view!.center.x = sender.view!.frame.width / 2
-//                }
-//            }
-//
-//            if(sender.view!.frame.maxX > sender.view!.frame.width){
-//                UIView.animate(withDuration: 0.3) {
-//                    sender.view!.center.x = sender.view!.frame.width / 2
-//                }
-//            }
-//
-//            if(sender.view!.frame.maxY < sender.view!.frame.width){
-//                UIView.animate(withDuration: 0.3) {
-//                    sender.view!.center.y += sender.view!.frame.width - sender.view!.frame.maxY
-//                }
-//            }
-//
-//            if(sender.view!.frame.minY > 0){
-//                UIView.animate(withDuration: 0.3) {
-//                    sender.view!.center.y -=  sender.view!.frame.minY
-//                }
-//            }
-//        }
-        // 스케일링 되어있는 경우와 그렇지 않은 경우를 구분해서 구현해야함
+        /// 너비변환이 이루어진 상태일때
+        else {
+            
+            /// 기본 위치에서 이미지가 아래로 이동한 경우
+            /// 100포인트 이상 떨어진 경우 화면 dismiss하도록 수퍼뷰에 노티
+            /// 이미지가 확대됐을때는 최상단으로부터 이미지가 떨어져있어야됨
+            if sender.view!.frame.minY > 0 && sender.view!.center.y - sender.view!.superview!.center.y > 100 {
+                dismissObservable.accept(true)
+                return
+            } else if sender.view!.frame.minY > 0 && sender.view!.center.y - sender.view!.superview!.center.y <= 100 {
+                UIView.animate(withDuration: 0.3) {
+                    sender.view!.center.y = sender.view!.superview!.center.y
+                }
+            }
+            
+            /// 확대 이미지가 위쪽 경계 넘어섰을때
+            if sender.view!.frame.minY < 0 && sender.view!.frame.height < sender.view!.superview!.frame.height {
+                UIView.animate(withDuration: 0.3) {
+                    sender.view!.center.y -= sender.view!.frame.origin.y
+                }
+            }
+            
+            /// 프레임이 왼쪽경계보다 안쪽으로 당겨질때
+            if(sender.view!.frame.origin.x > 0){
+                UIView.animate(withDuration: 0.3) {
+                    sender.view!.center.x -= sender.view!.frame.origin.x
+                }
+            }
 
+            /// 프레임이 오른쪽 경계보다 안쪽으로 당겨질때 - 이미지 wrapper UIView 너비로 체크
+            if(sender.view!.frame.maxX < sender.view!.superview!.frame.width){
+                UIView.animate(withDuration: 0.3) {
+                    sender.view!.center.x += (sender.view!.superview!.frame.width - sender.view!.frame.maxX)
+                }
+            }
+            
+            /// 확대 이미지가 하단으로부터 안쪽으로 더 올라왔을때
+            /// 확대 이미지 높이가 상하단 화면 경계를 넘어설때만 아래 조건문 실행해야됨
+            /// 상하단 높이값이 작은상태에서 아래 조건문 실행시 이미지가 아래로 붙어버림
+            if sender.view!.frame.height >= sender.view!.superview!.frame.height && sender.view!.frame.maxY < sender.view!.superview!.frame.maxY {
+                UIView.animate(withDuration: 0.3) {
+                    sender.view!.center.y += (sender.view!.superview!.frame.height - sender.view!.frame.maxY)
+                }
+            }
+        }
+        
+        backgroundAlphaObservable.onNext(0)
     }
     
     func checkImageOriginIsZero(_ sender: UIPinchGestureRecognizer){
-        if(sender.view!.frame.origin.x > 0 || sender.view!.frame.origin.y > 0 || sender.view!.frame.origin.x < 0 || sender.view!.frame.maxY < sender.view!.frame.width){
-            
-            let imageView = sender.view as! UIImageView
-            
+        
+        /// 핀치 좁혀서 화면보다 너비값이 작아졌을때
+        /// 원래 비율로 돌이키고 중심 수퍼뷰에 맞추기
+        if sender.view!.frame.width < sender.view!.superview!.frame.width {
             UIView.animate(withDuration: 0.3) {
-                // PinchGesture 종료 후 너비가 높이보다 큰 경우 ratio계산 후 그 이하로는 이미지 스케일링 더 진행되지 않도록 고정
-                // CustomPickerViewController 컬렉션뷰 didSelect 프로토콜 메서드에서 이미지뷰 아핀변환값 지정하는 부분 체크
-                if(imageView.frame.height < imageView.frame.width){
-                    let ratio = imageView.frame.width / imageView.frame.height
-                    
-                    imageView.transform.a = ratio
-                    imageView.transform.d = ratio
-                }else{
-                    imageView.transform.a = 1
-                    imageView.transform.d = 1
-                }
-                
-                sender.view!.center.x = sender.view!.superview!.frame.width / 2
-                sender.view!.center.y = sender.view!.superview!.frame.height / 2
+                sender.view!.transform.a = 1
+                sender.view!.transform.d = 1
+                sender.view!.center.y = sender.view!.superview!.center.y
+                sender.view!.center.x = sender.view!.superview!.center.x
             }
         }
     }
