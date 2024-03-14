fe83af38b9760cf9e1f92ef605b02ff7b2847aea
Parkjju <rudwns3927@gmail.com>
2024-03-14 18:22:27 +0900

    feat: 이미지 업로드 프로그레스 수치 콘솔 프린팅

--- posepicker/Networking/API/APISession.swift
+++ posepicker/Networking/API/APISession.swift
@@ -9,6 +9,7 @@ import Foundation
 
 import Alamofire
 import RxSwift
+import RxCocoa
 
 // 세션 의존성 주입
 struct APISession: APIService {
@@ -22,7 +23,41 @@ struct APISession: APIService {
     /// Single Trait으로 데이터 요청
     func requestSingle<T: Codable>(_ request: APIRouter) -> Single<T> {
         return Single<T>.create { observer -> Disposable in
-                let request = session.request(request, interceptor: APIInterceptor()).responseDecodable { (response: DataResponse<T, AFError>) in
+            let request = session.request(request, interceptor: APIInterceptor()).responseDecodable { (response: DataResponse<T, AFError>) in
+
+                guard let statusCode = response.response?.statusCode else {
+                    observer(.failure(APIError.unknown))
+                    return
+                }
+
+                guard (200 ... 399).contains(statusCode) else {
+                    observer(.failure(APIError.http(status: statusCode)))
+                    return
+                }
+
+                guard let decoded = response.data?.decode(T.self) else {
+                    observer(.failure(APIError.decode))
+                    return
+                }
+
+                observer(.success(decoded))
+                return
+            }
+            return Disposables.create {
+                request.cancel()
+            }
+        }
+    }
+    
+    /// Single Trait 멀티파트 통신
+    func requestMultipartSingle<T: Codable>(_ request: APIRouter) -> Single<T> {
+        return Single<T>.create { observer -> Disposable in
+            let request = session.upload(multipartFormData: request.multipartFormData, with: request, interceptor: APIInterceptor())
+                .uploadProgress(closure: { (progress) in
+                    print("Upload Progress: \(progress.fractionCompleted)")
+                })
+                .validate(statusCode: 200..<300)
+                .responseDecodable { (response: DataResponse<T, AFError>) in
                     guard let statusCode = response.response?.statusCode else {
                         observer(.failure(APIError.unknown))
                         return
@@ -41,10 +76,10 @@ struct APISession: APIService {
                     observer(.success(decoded))
                     return
                 }
-
-                return Disposables.create {
-                    request.cancel()
-                }
+            
+            return Disposables.create {
+                request.cancel()
             }
+        }
     }
 }
