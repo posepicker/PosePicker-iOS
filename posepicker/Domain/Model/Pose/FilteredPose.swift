77f08fb6b5b897dd95adc2950c9e76329ca01f68
Parkjju <rudwns3927@gmail.com>
2024-03-20 15:04:38 +0900

    fix: 필터링 포즈 모델 - PoseFeed로 수정

--- posepicker/Domain/Model/Pose/FilteredPose.swift
+++ posepicker/Domain/Model/Pose/FilteredPose.swift
@@ -8,19 +8,7 @@
 import Foundation
 
 struct FilteredPose: Codable {
-    let filteredContents: FilteredContents?
+    let filteredContents: PoseFeed?
     let recommendation: Bool
-    let recommendedContents: RecommendedContents?
-}
-
-struct FilteredContents: Codable {
-    let content: [PosePick]
-    let empty: Bool
-    let first: Bool
-    let last: Bool
-    let number: Int
-    let numberOfElements: Int
-    let pageable: Page
-    let size: Int
-    let sort: Sort
+    let recommendedContents: PoseFeed?
 }
