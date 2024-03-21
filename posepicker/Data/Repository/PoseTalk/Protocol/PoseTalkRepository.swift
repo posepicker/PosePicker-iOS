4a0006e0117b17cdd562551b59908080a93452b9
Parkjju <rudwns3927@gmail.com>
2024-03-21 14:32:27 +0900

    feat: 포즈톡 레포지토리 명세 작성

---
+++ posepicker/Data/Repository/PoseTalk/Protocol/PoseTalkRepository.swift
@@ -0,0 +1,13 @@
+//
+//  PoseTalkRepository.swift
+//  posepicker
+//
+//  Created by 박경준 on 3/21/24.
+//
+
+import Foundation
+import RxSwift
+
+protocol PoseTalkRepository {
+    func fetchPoseWord() -> Observable<PoseTalk>
+}
