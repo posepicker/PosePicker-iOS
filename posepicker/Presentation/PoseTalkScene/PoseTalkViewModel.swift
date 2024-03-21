f94df6f2f300ffb20fe59a972fc6febc454b4e94
Parkjju <rudwns3927@gmail.com>
2024-03-21 16:09:36 +0900

    feat: 포즈톡 뷰모델 - 인풋 configure 함수 구현 / 아웃풋 생성 함수 추가 / transform 수정 / 아웃풋 타입에 디폴트값 삽입

--- posepicker/Presentation/PoseTalkScene/PoseTalkViewModel.swift
+++ posepicker/Presentation/PoseTalkScene/PoseTalkViewModel.swift
@@ -10,8 +10,16 @@ import RxCocoa
 import RxSwift
 
 class PoseTalkViewModel: ViewModelType {
+    weak var coordinator: RootCoordinator?
+    private let posetalkUseCase: PoseTalkUseCase
+    
     var apiSession: APIService = APISession()
     var disposeBag = DisposeBag()
+    
+    init(coordinator: RootCoordinator?, posetalkUseCase: PoseTalkUseCase) {
+        self.coordinator = coordinator
+        self.posetalkUseCase = posetalkUseCase
+    }
     
     struct Input {
         let poseTalkButtonTapped: ControlEvent<Void>
@@ -19,23 +27,14 @@ class PoseTalkViewModel: ViewModelType {
     }
     
     struct Output {
-        let animate: Driver<Void>
-        let poseWord: Observable<String>
-        let isLoading: BehaviorRelay<Bool>
+        let animate = Driver<Void>.empty()
+        let poseWord = PublishRelay<String>()
+        let isLoading = BehaviorRelay<Bool>(value: false)
     }
     
     func transform(input: Input) -> Output {
         let poseWord = BehaviorRelay<String>(value: "제시어에 맞춰\n포즈를 취해요!")
         let isLoading = BehaviorRelay<Bool>(value: false)
-        
-        input.poseTalkButtonTapped
-            .flatMapLatest { [unowned self] _ -> Observable<PoseTalk> in
-                self.apiSession.requestSingle(.retrievePoseTalk).asObservable()
-            }
-            .subscribe(onNext: {
-                poseWord.accept($0.poseWord.content)
-            })
-            .disposed(by: disposeBag)
         
         /// 애니메이션 + 네트워크 관련 로직 추가를 고려한 설계
         input.isAnimating
@@ -43,7 +42,32 @@ class PoseTalkViewModel: ViewModelType {
                 isLoading.accept($0)
             })
             .disposed(by: disposeBag)
+        self.configureInput(input, disposeBag: disposeBag)
+        return createOutput(from: input, disposeBag: disposeBag)
+    }
+    
+    /// configureInput: 인풋 이벤트를 기반으로 유스케이스 -> 레파지토리를 거쳐 데이터를 저장해둔다.
+    /// 유스케이스에는 레파지토리의 데이터 요청 내부 동작을 가린채 정제가 완료된 최종 데이터만 저장된다.
+    /// 아웃풋에서는 저장된 유스케이스의 데이터를 바인딩만 해주면 끝
+    private func configureInput(_ input: Input, disposeBag: DisposeBag) {
+        input.poseTalkButtonTapped
+            .subscribe(onNext: { [weak self] in
+                print("TAPPP")
+                self?.posetalkUseCase.fetchPoseTalk()
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    /// createOutput: configureInput 함수 내에서 유스케이스에 모든 데이터 정제가 끝날 수도 있지만
+    /// cocoa 이벤트를 기반으로 새롭게 아웃풋에 정제가 필요한 경우도 존재함
+    private func createOutput(from input: Input, disposeBag: DisposeBag) -> Output {
+        let output = Output()
+
+        self.posetalkUseCase
+            .poseWord
+            .bind(to: output.poseWord)
+            .disposed(by: disposeBag)
         
-        return Output(animate: input.poseTalkButtonTapped.asDriver(), poseWord: poseWord.asObservable(), isLoading: isLoading)
+        return output
     }
 }
