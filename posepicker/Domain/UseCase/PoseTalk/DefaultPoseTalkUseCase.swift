b578fa1f8a4ead5b6ce34d60bfb0dfc5df0f5178
Parkjju <rudwns3927@gmail.com>
2024-03-21 16:10:41 +0900

    feat: 포즈톡 유스케이스 타입 가리기

--- posepicker/Domain/UseCase/PoseTalk/DefaultPoseTalkUseCase.swift
+++ posepicker/Domain/UseCase/PoseTalk/DefaultPoseTalkUseCase.swift
@@ -8,6 +8,8 @@
 import Foundation
 import RxSwift
 
+// 도메인 유스케이스 입장에서는 네트워크 요청이 어떻게 이루어지는지 모름
+// 데이터 타입도 모르는게 좋을 것 같다 -> 레포지토리 쪽에서 데이터 정제 후에 유스케이스에 넘겨주기
 final class DefaultPoseTalkUseCase: PoseTalkUseCase {
     private let posetalkRepository: DefaultPoseTalkRepository
     private let disposeBag = DisposeBag()
@@ -21,7 +23,7 @@ final class DefaultPoseTalkUseCase: PoseTalkUseCase {
     func fetchPoseTalk() {
         posetalkRepository.fetchPoseWord()
             .subscribe(onNext: { [weak self] poseTalk in
-                self?.poseWord.onNext(poseTalk.poseWord.content)
+                self?.poseWord.onNext(poseTalk)
             })
             .disposed(by: disposeBag)
     }
