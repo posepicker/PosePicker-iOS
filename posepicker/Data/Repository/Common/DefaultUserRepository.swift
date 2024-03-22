6c9f7be52edfdce346c1a0ea7fcc223be001cf5b
Parkjju <rudwns3927@gmail.com>
2024-03-22 18:48:52 +0900

    feat: 유저 레파지토리 파일 추가

---
+++ posepicker/Data/Repository/Common/DefaultUserRepository.swift
@@ -0,0 +1,34 @@
+//
+//  DefaultUserRepository.swift
+//  posepicker
+//
+//  Created by 박경준 on 3/20/24.
+//
+
+import Foundation
+
+import RxSwift
+
+//final class DefaultUserRepository: UserRepository {
+//    func loginWithKakao(email: String, accessToken: String, refreshToken: String) -> RxSwift.Observable<PosePickerUser> {
+//        <#code#>
+//    }
+//    
+//    func loginWithApple(idToken: String) -> RxSwift.Observable<PosePickerUser> {
+//        <#code#>
+//    }
+//    
+//    func reissueToken(refreshToken: String) -> RxSwift.Observable<Token> {
+//        <#code#>
+//    }
+//    
+//    func logout(accessToken: String, refreshToken: String) -> RxSwift.Observable<MeaninglessResponse> {
+//        <#code#>
+//    }
+//    
+//    func deleteUserInfo(accessToken: String, refreshToken: String, withdrawalReason: String) -> RxSwift.Observable<MeaninglessResponse> {
+//        <#code#>
+//    }
+//    
+    
+//}
