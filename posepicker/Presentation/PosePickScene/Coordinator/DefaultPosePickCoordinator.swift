e726b1dc0b014e8b50eabe7dfc9e4a7fd318f485
Parkjju <rudwns3927@gmail.com>
2024-03-27 17:55:11 +0900

    feat: 포즈픽 코디네이터 기본 틀 작성 / 코디네이터 프로토콜 채택 기본 구현 작성

---
+++ posepicker/Presentation/PosePickScene/Coordinator/DefaultPosePickCoordinator.swift
@@ -0,0 +1,39 @@
+//
+//  DefaultPosePickCoordinator.swift
+//  posepicker
+//
+//  Created by 박경준 on 3/27/24.
+//
+
+import UIKit
+
+final class DefaultPosePickCoordinator: PosePickCoordinator {
+    weak var finishDelegate: CoordinatorFinishDelegate?
+    var navigationController: UINavigationController
+    var posepickViewController: PosePickViewController
+    var childCoordinators: [Coordinator] = []
+    var type: CoordinatorType = .posepick
+    
+    required init(_ navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        self.posepickViewController = PosePickViewController(viewModel: PosePickViewModel())
+    }
+    
+    func start() {
+        self.posepickViewController.viewModel = PosePickViewModel()
+        
+        self.navigationController.pushViewController(self.posepickViewController, animated: true)
+    }
+    
+    func presentDetailImage() {
+        
+    }
+}
+
+extension DefaultPosePickCoordinator {
+    func coordinatorDidFinish(childCoordinator: Coordinator) {
+        self.childCoordinators = self.childCoordinators
+            .filter({ $0.type != childCoordinator.type })
+        childCoordinator.navigationController.popToRootViewController(animated: true)
+    }
+}
