0cc86631a478693d1d41c563a295f9fb548c7baa
Parkjju <rudwns3927@gmail.com>
2024-03-27 16:36:45 +0900

    feat: Common 그룹 뷰 컨트롤러, 뷰모델 추가

---
+++ posepicker/Presentation/Common/ViewModel/CommonViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  CommonViewModel.swift
+//  posepicker
+//
+//  Created by 박경준 on 3/27/24.
+//
+
+import Foundation
+
+import RxRelay
+import RxSwift
+
+final class CommonViewModel {
+    private weak var coordinator: DefaultPageViewCoordinator?
+    private let disposeBag = DisposeBag()
+}
