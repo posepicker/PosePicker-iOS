34c13215fb4b52aea164b8af6afb5ded600b6fc7
Parkjju <rudwns3927@gmail.com>
2024-03-27 10:55:25 +0900

    feat: Coordinator 프로토콜 익스텐션 기본구현 추가

--- posepicker/Presentation/Common/Coordinator/Protocol/Coordinator.swift
+++ posepicker/Presentation/Common/Coordinator/Protocol/Coordinator.swift
@@ -18,3 +18,25 @@ protocol Coordinator: AnyObject {
     
     init(_ navigationController: UINavigationController)
 }
+
+extension Coordinator {
+    func finish() {
+        childCoordinators.removeAll()
+        finishDelegate?.coordinatorDidFinish(childCoordinator: self)
+    }
+    
+    func findCoordinator(type: CoordinatorType) -> Coordinator? {
+        var stack: [Coordinator] = [self]
+        
+        while !stack.isEmpty {
+            let currentCoordinator = stack.removeLast()
+            if currentCoordinator.type == type {
+                return currentCoordinator
+            }
+            currentCoordinator.childCoordinators.forEach({ child in
+                stack.append(child)
+            })
+        }
+        return nil
+    }
+}
