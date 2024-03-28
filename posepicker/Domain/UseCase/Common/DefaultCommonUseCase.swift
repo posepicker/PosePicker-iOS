9075bd2666a3e968420de74e7472b8e8b87e3e12
Parkjju <rudwns3927@gmail.com>
2024-03-28 19:14:28 +0900

    feat: Common 뷰 유스케이스 작성 - 유저 레파지토리 주입 / dismiss 처리를 위한 옵저버블 추가 / 이메일 저장 및 토큰 저장은 레파지토리 내에서 처리

--- posepicker/Domain/UseCase/Common/DefaultCommonUseCase.swift
+++ posepicker/Domain/UseCase/Common/DefaultCommonUseCase.swift
@@ -7,3 +7,35 @@
 
 import Foundation
 import RxSwift
+
+final class DefaultCommonUseCase: CommonUseCase {
+    private let userRepository: DefaultUserRepository
+    
+    private let disposeBag = DisposeBag()
+    
+    let dismissTrigger = PublishSubject<Void>()
+
+    // 외부에서 목업 키체인 서비스 객체를 주입할 수 있어야됨
+    init(userRepository: DefaultUserRepository) {
+        self.userRepository = userRepository
+    }
+    
+    func loginWithKakao() {
+        // 키체인은 어차피 목업으로 함께 주입되기 때문에 로직이 레파지토리 안에 포함되어 있어도 됨
+        userRepository.loginWithKakao()
+            .subscribe(onNext: { [weak self] _ in
+                self?.dismissTrigger.onNext(())
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    func loginWithApple(idToken: String) {
+        userRepository.loginWithApple(idToken: idToken)
+            .subscribe(onNext: { [weak self] _ in
+                self?.dismissTrigger.onNext(())
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    
+}
