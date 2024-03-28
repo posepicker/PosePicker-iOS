f28469332012355fc51849f15b303e4d21cdb942
Parkjju <rudwns3927@gmail.com>
2024-03-28 15:07:43 +0900

    feat: CommonViewModel - viewControllerBefore & After 데이터소스 함수 코디네이터 통해 호출 / 인풋 마이페이지 버튼 & 델리게이트 트리거 로직 구성

--- posepicker/Presentation/Common/ViewModel/CommonViewModel.swift
+++ posepicker/Presentation/Common/ViewModel/CommonViewModel.swift
@@ -5,12 +5,57 @@
 //  Created by 박경준 on 3/27/24.
 //
 
-import Foundation
+import UIKit
 
 import RxRelay
 import RxSwift
 
 final class CommonViewModel {
-    private weak var coordinator: DefaultPageViewCoordinator?
-    private let disposeBag = DisposeBag()
+    private weak var coordinator: PageViewCoordinator?
+    
+    struct Input {
+        let pageviewTransitionDelegateEvent: Observable<Void>
+        let myPageButtonTapped: Observable<Void>
+    }
+    
+    struct Output {
+        
+    }
+    
+    init(coordinator: PageViewCoordinator?) {
+        self.coordinator = coordinator
+    }
+    
+    func transform(from input: Input, disposeBag: DisposeBag) -> Output {
+        let output = Output()
+        
+        input.pageviewTransitionDelegateEvent
+            .subscribe(onNext: { [weak self] in
+                guard let self = self,
+                      let coordinator = self.coordinator else {return}
+                coordinator.selectPage(coordinator.currentPage() ?? .posepick)
+                
+            })
+            .disposed(by: disposeBag)
+        
+        input.myPageButtonTapped
+            .subscribe(onNext: { [weak self] in
+                guard let self = self,
+                      let coordinator = self.coordinator else { return }
+                coordinator.pushMyPage()
+            })
+            .disposed(by: disposeBag)
+        
+        return output
+    }
+    
+    /// UIPageViewController DataSource - viewControllerBefore
+    func viewControllerBefore() -> UIViewController? {
+        return self.coordinator?.viewControllerBefore()
+    }
+    
+    /// UIPageViewController DataSource -> viewControllerAfter
+    func viewControllerAfter() -> UIViewController? {
+        return self.coordinator?.viewControllerAfter()
+    }
 }
