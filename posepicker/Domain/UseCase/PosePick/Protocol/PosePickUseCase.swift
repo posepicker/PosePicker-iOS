fd9b01747846b77a3e8db53946e0e12c0e5453c5
Parkjju <rudwns3927@gmail.com>
2024-03-31 22:49:40 +0900

    feat: 포즈픽 유스케이스 프로토콜 추가

--- posepicker/Domain/UseCase/PosePick/Protocol/PosePickUseCase.swift
+++ posepicker/Domain/UseCase/PosePick/Protocol/PosePickUseCase.swift
@@ -5,10 +5,10 @@
 //  Created by 박경준 on 3/20/24.
 //
 
-import Foundation
-
+import UIKit
 import RxSwift
 
 protocol PosePickUseCase {
-    
+    var poseImage: PublishSubject<UIImage> { get set }
+    func fetchPosePick(peopleCount: Int)
 }
