9870a0bf5d5b3440663d1bd0dc72e05759547144
Parkjju <rudwns3927@gmail.com>
2024-04-01 09:19:37 +0900

    feat: 포즈픽 레파지토리 이미지 캐시 로직 추가

--- posepicker/Data/Repository/PosePick/DefaultPosePickRepository.swift
+++ posepicker/Data/Repository/PosePick/DefaultPosePickRepository.swift
@@ -5,9 +5,10 @@
 //  Created by 박경준 on 3/29/24.
 //
 
-import Foundation
+import UIKit
 import RxSwift
 import RxCocoa
+import Kingfisher
 
 final class DefaultPosePickRepository: PosePickRepository {
     
@@ -17,11 +18,43 @@ final class DefaultPosePickRepository: PosePickRepository {
         self.networkService = networkService
     }
     
-    func fetchPose(peopleCount: Int) -> Observable<Pose> {
+    func fetchPoseImage(peopleCount: Int) -> Observable<UIImage> {
         return networkService.requestSingle(.retrievePosePick(peopleCount: peopleCount))
             .asObservable()
             .flatMapLatest { (posepick: Pose) -> Observable<Pose> in
                 return BehaviorRelay<Pose>(value: posepick).asObservable()
             }
+            .map { $0.poseInfo.imageKey }
+            .withUnretained(self)
+            .flatMapLatest { (owner, imageURL) -> Observable<UIImage> in
+                return owner.cacheItem(for: imageURL)
+            }
+    }
+    
+    private func cacheItem(for imageURL: String) -> Observable<UIImage> {
+        return Observable.create { observer in
+            ImageCache.default.retrieveImageInDiskCache(forKey: imageURL) { result in
+                switch result {
+                case .success(let value):
+                    if let image = value?.images?.first {
+                        observer.onNext(image)
+                    } else if let url = URL(string: imageURL) {
+                        KingfisherManager.shared.retrieveImage(with: url) { downloadResult in
+                            switch downloadResult {
+                            case .success(let downloaded):
+                                observer.onNext(downloaded.image)
+                            case .failure(let error):
+                                observer.onError(error)
+                            }
+                        }
+                    }
+                case .failure(let error):
+                    observer.onError(error)
+                }
+            }
+            return Disposables.create {
+                
+            }
+        }
     }
 }
