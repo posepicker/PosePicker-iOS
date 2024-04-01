096b5a4ad9a377d30dd63a3f9ba0cb2896e774ae
Parkjju <rudwns3927@gmail.com>
2024-04-01 09:19:56 +0900

    feat: 포즈픽 유스케이스 이미지 캐시 로직 캡슐화

--- posepicker/Domain/UseCase/PosePick/DefaultPosePickUseCase.swift
+++ posepicker/Domain/UseCase/PosePick/DefaultPosePickUseCase.swift
@@ -10,55 +10,22 @@ import RxSwift
 import Kingfisher
 
 final class DefaultPosePickUseCase: PosePickUseCase {
-    private let posepickRepository: DefaultPosePickRepository
+    private let posepickRepository: PosePickRepository
     private let disposeBag = DisposeBag()
     
     var poseImage = PublishSubject<UIImage>()
     
-    init(posepickRepository: DefaultPosePickRepository) {
+    init(posepickRepository: PosePickRepository) {
         self.posepickRepository = posepickRepository
     }
     
+    // 캐시 데이터 정제 로직도 레파지토리에 있어야됨
     func fetchPosePick(peopleCount: Int) {
         posepickRepository
-            .fetchPose(peopleCount: peopleCount)
-            .map { $0.poseInfo.imageKey }
-            .withUnretained(self)
-            .flatMapLatest { (owner, imageURL) -> Observable<UIImage> in
-                return owner.cacheItem(for: imageURL)
-            }
-            .catchAndReturn(ImageLiteral.imgHelp24)
+            .fetchPoseImage(peopleCount: peopleCount)
             .subscribe(onNext: { [weak self] in
                 self?.poseImage.onNext($0)
             })
             .disposed(by: disposeBag)
-    }
-    
-    private func cacheItem(for imageURL: String) -> Observable<UIImage> {
-        return Observable.create { observer in
-            ImageCache.default.retrieveImageInDiskCache(forKey: imageURL) { result in
-                switch result {
-                case .success(let value):
-                    if let image = value?.images?.first {
-                        observer.onNext(image)
-                    } else if let url = URL(string: imageURL) {
-                        KingfisherManager.shared.retrieveImage(with: url) { downloadResult in
-                            switch downloadResult {
-                            case .success(let downloaded):
-                                observer.onNext(downloaded.image)
-                            case .failure(let error):
-                                observer.onError(error)
-                            }
-                        }
-                    }
-                case .failure(let error):
-                    observer.onError(error)
-                }
-            }
-            return Disposables.create {
-                
-            }
-            
-        }
     }
 }
