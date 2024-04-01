012f4c1bd2fbbb41b38267d5f901de932c11302d
Parkjju <rudwns3927@gmail.com>
2024-04-01 09:19:24 +0900

    feat: 포즈픽 레파지토리 - 데이터 정제 로직 유스케이스에서 레파지토리로 이동, 프로토콜 내에 함수 명세 추가

--- posepicker/Data/Repository/PosePick/Protocol/PosePickRepository.swift
+++ posepicker/Data/Repository/PosePick/Protocol/PosePickRepository.swift
@@ -5,9 +5,9 @@
 //  Created by 박경준 on 3/29/24.
 //
 
-import Foundation
+import UIKit
 import RxSwift
 
 protocol PosePickRepository {
-    func fetchPose(peopleCount: Int) -> Observable<Pose>
+    func fetchPoseImage(peopleCount: Int) -> Observable<UIImage>
 }
