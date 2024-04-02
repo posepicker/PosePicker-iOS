75fb039d0130cbda0427575b61b34881a7c9ae70
Parkjju <rudwns3927@gmail.com>
2024-04-02 09:42:31 +0900

    test: 포즈픽 뷰모델 테스트코드 케이스 추가 === 1. flatMapLatest로 결합된 두 버튼 로직 개선 필요 2. 셀렉션 탭은 요청과 관련 없도록 수정

--- PosePickerViewModelTests/PosePickScene/PosePickViewModelTests.swift
+++ PosePickerViewModelTests/PosePickScene/PosePickViewModelTests.swift
@@ -18,6 +18,7 @@ final class PosePickViewModelTests: XCTestCase {
     private var scheduler: TestScheduler!
     private var input: PosePickViewModel.Input!
     private var posepickUseCase: PosePickUseCase!
+    private var output: PosePickViewModel.Output!
     
     override func setUp() {
         super.setUp()
@@ -47,7 +48,8 @@ final class PosePickViewModelTests: XCTestCase {
     /// 4. isLoading으로 아웃풋 생성
     func test_버튼_탭_이후_이미지를_불러오기_로딩_상태값이랑_함께_검증 () {
         let peopleCountButtonTestableObservable = self.scheduler.createHotObservable([
-            .next(5, 1)
+            .next(5, 1),
+            .next(13, 2)
         ])
         let posepickButtonTapTestableObservable = self.scheduler.createHotObservable([
             .next(10, ())
@@ -58,6 +60,7 @@ final class PosePickViewModelTests: XCTestCase {
         ])
         
         let poseImageObserver = self.scheduler.createObserver(Data?.self)
+        let animateTriggerObserver = self.scheduler.createObserver(Bool.self)
         
         self.input = PosePickViewModel.Input(
             selectedPeopleCount: peopleCountButtonTestableObservable.asObservable(),
@@ -65,16 +68,32 @@ final class PosePickViewModelTests: XCTestCase {
             isAnimating: isAnimatingTestableObservable.asObservable()
         )
         
-        self.viewModel.transform(input: input, disposeBag: self.disposeBag)
+        self.output = self.viewModel.transform(input: input, disposeBag: self.disposeBag)
+        
+        self.output
             .poseImage
             .map { $0.pngData()! }
             .subscribe(poseImageObserver)
             .disposed(by: self.disposeBag)
         
+        self.output
+            .animate
+            .map { true }
+            .subscribe(animateTriggerObserver)
+            .disposed(by: self.disposeBag)
+        
         self.scheduler.start()
         
+        /// 실패 케이스 1. flatMapLatest로 셀렉션과 요청 버튼이 결합된 이후
+        /// 셀렉션 버튼만 탭해도 요청이 진행되는 문제
         XCTAssertEqual(poseImageObserver.events, [
             .next(12, ImageLiteral.imgInfo24.pngData()!)
+        ])
+        
+        /// 실패 케이스 2. flatMapLatest로 셀렉션과 요청 버튼이 결합된 이후
+        /// 셀렉션 버튼만 탭해도 애니메이션이 트리거되는 문제
+        XCTAssertEqual(animateTriggerObserver.events, [
+            .next(10, true),
         ])
     }
 
