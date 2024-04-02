8938540e1446ff4a693a142a5eefba423c3a37b0
Parkjju <rudwns3927@gmail.com>
2024-04-02 17:19:29 +0900

    feat: 포즈픽 뷰모델 테스트코드 - 로티 숨기기 관련 & 포즈픽 로딩 전까지 애니메이션 트리거 로직 테스트

--- PosePickerViewModelTests/PosePickScene/PosePickViewModelTests.swift
+++ PosePickerViewModelTests/PosePickScene/PosePickViewModelTests.swift
@@ -72,6 +72,7 @@ final class PosePickViewModelTests: XCTestCase {
         
         self.output
             .poseImage
+            .compactMap { $0 }
             .map { $0.pngData()! }
             .subscribe(poseImageObserver)
             .disposed(by: self.disposeBag)
@@ -94,6 +95,80 @@ final class PosePickViewModelTests: XCTestCase {
         /// 셀렉션 버튼만 탭해도 애니메이션이 트리거되는 문제
         XCTAssertEqual(animateTriggerObserver.events, [
             .next(10, true),
+        ])
+    }
+    
+    func test_포즈픽_이미지_로딩이_아직_안끝났을때_애니메이션_한번_더_트리거() {
+        let peopleCountButtonTestableObservable = self.scheduler.createHotObservable([
+            .next(5, 1)
+        ])
+        let posepickButtonTapTestableObservable = self.scheduler.createHotObservable([
+            .next(10, ())
+        ])
+        let isAnimatingTestableObservable = self.scheduler.createHotObservable([
+            .next(10, true),    // 로티 한사이클 돌리는데 1타임
+            .next(11, false)
+        ])
+        
+        let animateTriggerObserver = self.scheduler.createObserver(Bool.self)
+        
+        self.input = PosePickViewModel.Input(
+            selectedPeopleCount: peopleCountButtonTestableObservable.asObservable(),
+            posepickButtonEvent: posepickButtonTapTestableObservable.asObservable(),
+            isAnimating: isAnimatingTestableObservable.asObservable()
+        )
+        
+        self.output = self.viewModel.transform(input: input, disposeBag: self.disposeBag)
+        
+        self.output
+            .animate
+            .map { true }
+            .subscribe(animateTriggerObserver)
+            .disposed(by: self.disposeBag)
+        
+        /// 12타임에 애니메이션 1사이클 종료가 되었음에도 이미지가 nil인 상황
+        /// 로드가 안되었기때문에 애니메이션 다시 트리거 해야됨
+        /// 요청 진행하면 다시 이미지 nil로 세팅
+        // MARK: - 스케줄러 위치는 테스트 직전에!
+        self.scheduler.start()
+        self.posepickUseCase.poseImage.onNext(nil)
+        
+        XCTAssertEqual(animateTriggerObserver.events, [
+            .next(10, true),
+            .next(11, true)
+        ])
+    }
+    
+    func test_이미지_불러온_뒤_로티_숨겨지는지() {
+        let peopleCountButtonTestableObservable = self.scheduler.createHotObservable([
+            .next(5, 1)
+        ])
+        let posepickButtonTapTestableObservable = self.scheduler.createHotObservable([
+            .next(10, ())
+        ])
+        let isAnimatingTestableObservable = self.scheduler.createHotObservable([
+            .next(10, true),    // 로티 한사이클 돌리는데 1타임
+            .next(12, false)
+        ])
+        
+        self.input = PosePickViewModel.Input(
+            selectedPeopleCount: peopleCountButtonTestableObservable.asObservable(),
+            posepickButtonEvent: posepickButtonTapTestableObservable.asObservable(),
+            isAnimating: isAnimatingTestableObservable.asObservable()
+        )
+        
+        self.output = self.viewModel.transform(input: input, disposeBag: self.disposeBag)
+        
+        let lottieHiddenObserver = self.scheduler.createObserver(Bool.self)
+        
+        self.output.lottieImageHidden
+            .bind(to: lottieHiddenObserver)
+            .disposed(by: self.disposeBag)
+        
+        self.scheduler.start()
+        
+        XCTAssertEqual(lottieHiddenObserver.events, [
+            .next(12, true)
         ])
     }
 
