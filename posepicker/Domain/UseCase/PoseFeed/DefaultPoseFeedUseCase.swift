89de3cfe9138df57f1a46e67f29bb4fbf4287c0a
Parkjju <rudwns3927@gmail.com>
2024-04-03 15:25:19 +0900

    feat: 포즈피드 유스케이스 프로토콜 & 클래스 파일 추가

---
+++ posepicker/Domain/UseCase/PoseFeed/DefaultPoseFeedUseCase.swift
@@ -0,0 +1,8 @@
+//
+//  DefaultPoseFeedUseCase.swift
+//  posepicker
+//
+//  Created by 박경준 on 4/3/24.
+//
+
+import Foundation
---
+++ posepicker/Domain/UseCase/PoseFeed/Protocol/PoseFeedUseCase.swift
@@ -0,0 +1,15 @@
+//
+//  PoseFeedUseCase.swift
+//  posepicker
+//
+//  Created by 박경준 on 4/3/24.
+//
+
+import Foundation
+
+protocol PoseFeedUseCase {
+    // 태그 컬렉션뷰
+    // 필터링 섹션
+    // 추천 섹션
+    // 코디네이터 간 UI 상태값 통신은 어떻게 이루어지나?
+}
