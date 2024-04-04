f8626564d2fa003e2c0fba421b1284b0f918e358
Parkjju <rudwns3927@gmail.com>
2024-04-04 15:42:18 +0900

    feat: 포즈피드 유스케이스 정의 / 데이터 fetch 후 사이즈 리스트도 업데이트

--- posepicker/Domain/UseCase/PoseFeed/DefaultPoseFeedUseCase.swift
+++ posepicker/Domain/UseCase/PoseFeed/DefaultPoseFeedUseCase.swift
@@ -5,4 +5,59 @@
 //  Created by 박경준 on 4/3/24.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxRelay
+
+final class DefaultPoseFeedUseCase: PoseFeedUseCase {
+    
+    private var disposeBag = DisposeBag()
+    private let posefeedRepository: PoseFeedRepository
+    
+    init(posefeedRepository: PoseFeedRepository) {
+        self.posefeedRepository = posefeedRepository
+    }
+    
+    var feedContents = PublishSubject<[Section<PoseFeedPhotoCellViewModel>]>()
+    var filterSectionContentSizes = BehaviorRelay<[CGSize]>(value: [])
+    var recommendSectionContentSizes = BehaviorRelay<[CGSize]>(value: [])
+    
+    func fetchFeedContents(peopleCount: String, frameCount: String, filterTags: [String], pageNumber: Int) {
+        
+        self.posefeedRepository
+            .fetchFeedContents(peopleCount: peopleCount, frameCount: frameCount, filterTags: filterTags, pageNumber: pageNumber)
+            .subscribe(onNext: { [weak self] sectionItems in
+                self?.feedContents.onNext(sectionItems)
+                
+                // 필터링 섹션 이미지 사이즈
+                sectionItems[0].items.forEach { viewModel in
+                    guard let image = viewModel.image.value,
+                          let self = self else { return }
+                    let newSizeImage = self.newSizeImageWidthDownloadedResource(image: image)
+                    viewModel.image.accept(newSizeImage)
+                    self.filterSectionContentSizes.accept(
+                        self.filterSectionContentSizes.value + [newSizeImage.size]
+                    )
+                }
+                
+                // 추천 섹션 이미지 사이즈
+                sectionItems[1].items.forEach { viewModel in
+                    guard let image = viewModel.image.value,
+                          let self = self else { return }
+                    let newSizeImage = self.newSizeImageWidthDownloadedResource(image: image)
+                    viewModel.image.accept(newSizeImage)
+                    self.recommendSectionContentSizes.accept(
+                        self.recommendSectionContentSizes.value + [newSizeImage.size]
+                    )
+                }
+            })
+            .disposed(by: disposeBag)
+        
+    }
+    
+    private func newSizeImageWidthDownloadedResource(image: UIImage) -> UIImage {
+        let targetWidth = (UIScreen.main.bounds.width - 56) / 2
+        let newSizeImage = image.resize(newWidth: targetWidth)
+        return newSizeImage
+    }
+}
