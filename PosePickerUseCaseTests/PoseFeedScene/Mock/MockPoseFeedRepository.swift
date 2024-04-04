ad7fc8874300a1c0c14f594f384b5045edad834a
Parkjju <rudwns3927@gmail.com>
2024-04-04 15:41:23 +0900

    feat: 목업 포즈피드 레파지토리 클래스 추가

--- PosePickerUseCaseTests/PoseFeedScene/Mock/MockPoseFeedRepository.swift
+++ PosePickerUseCaseTests/PoseFeedScene/Mock/MockPoseFeedRepository.swift
@@ -6,6 +6,29 @@
 //
 
 import Foundation
+import RxSwift
+
 @testable import posepicker
 
-
+final class MockPoseFeedRepository: PoseFeedRepository {
+    func fetchFeedContents(peopleCount: String, frameCount: String, filterTags: [String], pageNumber: Int) -> Observable<[Section<PoseFeedPhotoCellViewModel>]> {
+        return Observable.just([
+            Section(header: "", items: [
+                PoseFeedPhotoCellViewModel(
+                    image: ImageLiteral.imgInfo24,
+                    poseId: 1,
+                    bookmarkCheck: true
+                )
+            ]),
+            Section(header: "", items: [
+                PoseFeedPhotoCellViewModel(
+                    image: ImageLiteral.imgInfo24,
+                    poseId: 1,
+                    bookmarkCheck: true
+                )
+            ])
+        ])
+    }
+    
+    
+}
