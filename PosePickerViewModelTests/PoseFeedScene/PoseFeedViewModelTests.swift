b125066c046ac76de58449c2610f01f5163d1359
Parkjju <rudwns3927@gmail.com>
2024-04-04 17:59:18 +0900

    test: 무한스크롤 로직 테스트코드 추가 / page 세팅 후 데이터 누적되는지 체크

--- PosePickerViewModelTests/PoseFeedScene/PoseFeedViewModelTests.swift
+++ PosePickerViewModelTests/PoseFeedScene/PoseFeedViewModelTests.swift
@@ -28,8 +28,49 @@ final class PoseFeedViewModelTests: XCTestCase {
         )
         self.disposeBag = DisposeBag()
     }
-
-    override func tearDown() {
+    
+    func test_무한스크롤() {
+        let currentPageTestObservable = self.scheduler.createHotObservable([
+            .next(0, 0),
+            .next(1, 1)
+        ])
+        let filteredContentsCountObserver = self.scheduler.createObserver(Int.self)
+        let recommendedContentsCountObserver = self.scheduler.createObserver(Int.self)
+        self.input = PoseFeedViewModel.Input(
+            currentPage: currentPageTestObservable.asObservable()
+        )
+        self.output = self.viewModel.transform(input: self.input, disposeBag: self.disposeBag)
         
+        output.contents
+            .map { $0[0].items.count }
+            .subscribe(filteredContentsCountObserver)
+            .disposed(by: disposeBag)
+        
+        output.contents
+            .map { $0[1].items.count }
+            .subscribe(recommendedContentsCountObserver)
+            .disposed(by: disposeBag)
+        
+        self.scheduler.start()
+        
+        XCTAssertEqual(filteredContentsCountObserver.events, [
+            .next(0, 5),
+            .next(1, 10)
+        ])
+        
+        XCTAssertEqual(recommendedContentsCountObserver.events, [
+            .next(0, 5),
+            .next(1, 10)
+        ])
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        self.viewModel = nil
+        self.disposeBag = nil
+        self.scheduler = nil
+        self.input = nil
+        self.posefeedUseCase = nil
+        self.output = nil
     }
 }
