655dff0dcfd021dc77a73a4201547683c5d818c7
Parkjju <rudwns3927@gmail.com>
2024-04-04 17:16:37 +0900

    feat: 포즈피드 유스케이스 lastpage 옵저버블 추가 / 피드 컨텐츠 BehaviorRelay로 타입 수정

--- posepicker/Domain/UseCase/PoseFeed/Protocol/PoseFeedUseCase.swift
+++ posepicker/Domain/UseCase/PoseFeed/Protocol/PoseFeedUseCase.swift
@@ -14,9 +14,10 @@ protocol PoseFeedUseCase {
     // 뷰모델에서 유스케이스 데이터에 직접 접근하는 것은 권장되지 않음 (단일 책임원칙 위반)
     
     /// 피드 컨텐츠 & 사이즈 정보
-    var feedContents: PublishSubject<[Section<PoseFeedPhotoCellViewModel>]> { get set }
+    var feedContents: BehaviorRelay<[Section<PoseFeedPhotoCellViewModel>]> { get set }
     var filterSectionContentSizes: BehaviorRelay<[CGSize]> { get set }
     var recommendSectionContentSizes: BehaviorRelay<[CGSize]> { get set }
+    var isLastPage: PublishSubject<Bool> { get set }
     
     func fetchFeedContents(peopleCount: String, frameCount: String, filterTags: [String], pageNumber: Int)
 }
