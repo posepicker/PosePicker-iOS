11a15c182a4c5bad7281b01d74e94517141ef475
Parkjju <rudwns3927@gmail.com>
2024-04-05 17:36:53 +0900

    feat: 포즈피드 필터 유스케이스 인원 수 카운트 & 프레임 수 카운트 & 각종 태그 셀렉팅 및 초기화, 취소 관련 명세 추가

--- posepicker/Domain/UseCase/PoseFeed/Protocol/PoseFeedFilterUseCase.swift
+++ posepicker/Domain/UseCase/PoseFeed/Protocol/PoseFeedFilterUseCase.swift
@@ -9,7 +9,13 @@ import Foundation
 import RxRelay
 
 protocol PoseFeedFilterUseCase {
+    var peopleCount: BehaviorRelay<Int> { get set }
+    var frameCount: BehaviorRelay<Int> { get set }
     var tagItems: BehaviorRelay<[PoseFeedFilterCellViewModel]> { get set }
     
     func selectItem(title: String)
+    func selectPeopleCount(value: Int)
+    func selectFrameCount(value: Int)
+    func resetAllTags()
+    func cancelTagSetting()
 }
