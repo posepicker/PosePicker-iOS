2f94ff1bababdf208004f383daa6c9c0567bc83a
Parkjju <rudwns3927@gmail.com>
2024-04-05 17:37:23 +0900

    test: 태그 리셋관련 테스트코드 추가

--- PosePickerUseCaseTests/PoseFeedScene/FilterUseCase/FilterUseCaseTests.swift
+++ PosePickerUseCaseTests/PoseFeedScene/FilterUseCase/FilterUseCaseTests.swift
@@ -54,7 +54,100 @@ final class FilterUseCaseTests: XCTestCase {
         ])
     }
     
-    override func tearDown() {
+    func test_태그리셋_후_모든_태그_초기화되는지() {
+        let tagTapObservable = self.scheduler.createHotObservable([
+            .next(0, "친구"),
+            .next(0, "커플")
+        ])
         
+        let peopleCountTapObservabable = self.scheduler.createHotObservable([
+            .next(0, 1)
+        ])
+        
+        let frameCountTapObservable = self.scheduler.createHotObservable([
+            .next(0, 1)
+        ])
+        
+        let resetButtonTapObservable = self.scheduler.createHotObservable([
+            .next(1, ())
+        ])
+        
+        let friendTagSelectedObserver = self.scheduler.createObserver(Bool.self)
+        let coupleTagSelectedObserver = self.scheduler.createObserver(Bool.self)
+        let peopleCountSelectedObserver = self.scheduler.createObserver(Int.self)
+        let frameCountSelectedObserver = self.scheduler.createObserver(Int.self)
+        
+        tagTapObservable
+            .subscribe(onNext: { [weak self] in
+                self?.poseFeedUseCase.selectItem(title: $0)
+            })
+            .disposed(by: disposeBag)
+        
+        resetButtonTapObservable
+            .subscribe(onNext: { [weak self] in
+                self?.poseFeedUseCase.resetAllTags()
+            })
+            .disposed(by: disposeBag)
+        
+        peopleCountTapObservabable
+            .subscribe(onNext: { [weak self] in
+                self?.poseFeedUseCase.selectPeopleCount(value: $0)
+            })
+            .disposed(by: disposeBag)
+        
+        frameCountTapObservable
+            .subscribe(onNext: { [weak self] in
+                self?.poseFeedUseCase.selectFrameCount(value: $0)
+            })
+            .disposed(by: disposeBag)
+        
+        poseFeedUseCase.tagItems.value[0].isSelected
+            .subscribe(friendTagSelectedObserver)
+            .disposed(by: disposeBag)
+        
+        poseFeedUseCase.tagItems.value[1].isSelected
+            .subscribe(coupleTagSelectedObserver)
+            .disposed(by: disposeBag)
+        
+        poseFeedUseCase.peopleCount
+            .subscribe(peopleCountSelectedObserver)
+            .disposed(by: disposeBag)
+        
+        poseFeedUseCase.frameCount
+            .subscribe(frameCountSelectedObserver)
+            .disposed(by: disposeBag)
+        
+        self.scheduler.start()
+        
+        XCTAssertEqual(friendTagSelectedObserver.events, [
+            .next(0, false),
+            .next(0, true),
+            .next(1, false)
+        ])
+        
+        XCTAssertEqual(coupleTagSelectedObserver.events, [
+            .next(0, false),
+            .next(0, true),
+            .next(1, false)
+        ])
+        
+        XCTAssertEqual(peopleCountSelectedObserver.events, [
+            .next(0, 0),
+            .next(0, 1),
+            .next(1, 0)
+        ])
+        
+        XCTAssertEqual(frameCountSelectedObserver.events, [
+            .next(0, 0),
+            .next(0, 1),
+            .next(1, 0)
+        ])
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        self.disposeBag = DisposeBag()
+        self.poseFeedUseCase = nil
+        self.scheduler = nil
     }
 }
