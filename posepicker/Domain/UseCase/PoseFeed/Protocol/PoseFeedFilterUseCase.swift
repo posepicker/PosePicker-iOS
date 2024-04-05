062f3b7d3a6c5e454ec67ab5a60a2bcbdbd2c7c3
Parkjju <rudwns3927@gmail.com>
2024-04-05 16:14:06 +0900

    feat: 포즈피드 필터 유스케이스 - 태그아이템 속성 추가

---
+++ posepicker/Domain/UseCase/PoseFeed/Protocol/PoseFeedFilterUseCase.swift
@@ -0,0 +1,13 @@
+//
+//  FilterUseCase.swift
+//  posepicker
+//
+//  Created by 박경준 on 4/5/24.
+//
+
+import Foundation
+import RxRelay
+
+protocol PoseFeedFilterUseCase {
+    var tagItems: BehaviorRelay<[PoseFeedFilterCellViewModel]> { get set }
+}
