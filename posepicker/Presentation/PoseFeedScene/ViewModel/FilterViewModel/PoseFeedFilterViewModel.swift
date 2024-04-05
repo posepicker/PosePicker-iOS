32a5f67976c1263b1ec5c05a0f6ca8e5feb4a254
Parkjju <rudwns3927@gmail.com>
2024-04-05 17:35:50 +0900

    feat: 포즈피드 필터 뷰모델 - 태그리셋 버튼 탭 + 코디네이터 알럿창 confirmed 로직 추가

--- posepicker/Presentation/PoseFeedScene/ViewModel/FilterViewModel/PoseFeedFilterViewModel.swift
+++ posepicker/Presentation/PoseFeedScene/ViewModel/FilterViewModel/PoseFeedFilterViewModel.swift
@@ -20,10 +20,13 @@ final class PoseFeedFilterViewModel {
     
     struct Input {
         let filterTagSelectedEvent: Observable<PoseFeedFilterCellViewModel>
+        let filterResetButtonTapEvent: Observable<Void>
     }
     
     struct Output {
         let tagItems = BehaviorRelay<[PoseFeedFilterCellViewModel]>(value: [])
+        let selectedPeopleCount = PublishRelay<Int>()
+        let selectedFrameCount = PublishRelay<Int>()
     }
     
     func transform(input: Input, disposeBag: DisposeBag) -> Output {
@@ -35,9 +38,36 @@ final class PoseFeedFilterViewModel {
             })
             .disposed(by: disposeBag)
         
+        self.posefeedFilterUseCase.peopleCount
+            .subscribe(onNext: {
+                output.selectedPeopleCount.accept($0)
+            })
+            .disposed(by: disposeBag)
+        
+        self.posefeedFilterUseCase.frameCount
+            .subscribe(onNext: {
+                output.selectedFrameCount.accept($0)
+            })
+            .disposed(by: disposeBag)
+        
         input.filterTagSelectedEvent
             .subscribe(onNext: { [weak self] in
                 self?.posefeedFilterUseCase.selectItem(title: $0.title.value)
+            })
+            .disposed(by: disposeBag)
+        
+        input.filterResetButtonTapEvent
+            .withUnretained(self)
+            .flatMapLatest { (owner, _) -> Observable<Bool> in
+                guard let coordinator = owner.coordinator else { return Observable<Bool>.empty() }
+                return coordinator.presentTagResetConfirmModal(disposeBag: disposeBag)
+            }
+            .subscribe(onNext: { [weak self] in
+                if $0 {
+                    self?.posefeedFilterUseCase.resetAllTags()
+                } else {
+                    return
+                }
             })
             .disposed(by: disposeBag)
         
