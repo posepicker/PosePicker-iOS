e940aaa244ec824d817e014f27cdf83feba6e21b
Parkjju <rudwns3927@gmail.com>
2024-04-08 17:06:23 +0900

    feat: 루트뷰 로그인 완료 유스케이스 구현

--- posepicker/Domain/UseCase/Common/Protocol/CommonUseCase.swift
+++ posepicker/Domain/UseCase/Common/Protocol/CommonUseCase.swift
@@ -6,8 +6,11 @@
 //
 
 import Foundation
+import RxSwift
 
 protocol CommonUseCase {
+    var loginCompleted: PublishSubject<Void> { get set }
+    
     func loginWithKakao()
     func loginWithApple(idToken: String)
 }
