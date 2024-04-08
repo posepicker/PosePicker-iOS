85e1417d81d9829c969c6afad0708b99a45d0d8e
Parkjju <rudwns3927@gmail.com>
2024-04-08 09:16:59 +0900

    feat: 포즈디테일 유스케이스 구현체 - 생성자 함수에서 데이터 불러오기 & 태그정보 및 URL값 불러오기

--- posepicker/Domain/UseCase/PoseFeed/DetailUseCase/DefaultPoseFeedDetailUseCase.swift
+++ posepicker/Domain/UseCase/PoseFeed/DetailUseCase/DefaultPoseFeedDetailUseCase.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import RxSwift
+import RxRelay
+
+final class DefaultPoseFeedDetailUseCase: PoseFeedDetailUseCase {
+    
+    private var disposeBag = DisposeBag()
+    private let posefeedDetailRepository: PoseFeedDetailRepository
+    private let poseId: Int
+    
+    init(posefeedDetailRepository: PoseFeedDetailRepository, poseId: Int) {
+        self.posefeedDetailRepository = posefeedDetailRepository
+        self.poseId = poseId
+        
+        self.posefeedDetailRepository
+            .fetchPoseInfo(poseId: self.poseId)
+            .subscribe(self.pose)
+            .disposed(by: self.disposeBag)
+    }
+    
+    var tagItems = BehaviorRelay<[String]>(value: [])
+    var sourceUrl = PublishSubject<String>()
+    var pose = PublishSubject<Pose>()
+    
+    func getTagsFromPoseInfo() {
+        pose
+            .compactMap { $0.poseInfo.tagAttributes }
+            .map { $0.split(separator: ",").map { String($0) }}
+            .subscribe(onNext: { [weak self] in
+                self?.tagItems.accept($0)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    func getSourceURLFrom() {
+        pose
+            .compactMap { $0.poseInfo.sourceUrl }
+            .subscribe(onNext: { [weak self] in
+                self?.sourceUrl.onNext($0)
+            })
+            .disposed(by: self.disposeBag)
+    }
+    
+    
+}
