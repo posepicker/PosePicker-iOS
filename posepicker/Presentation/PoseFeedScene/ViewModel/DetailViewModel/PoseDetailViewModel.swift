21771756df36a7faef29d4105dea161c8cc18b42
Parkjju <rudwns3927@gmail.com>
2024-04-08 09:18:15 +0900

    feat: 포즈디테일 뷰모델 유스케이스 주입 / viewDidLoadEvent 인풋 트랜스폼 로직 구현

--- posepicker/Presentation/PoseFeedScene/ViewModel/DetailViewModel/PoseDetailViewModel.swift
+++ posepicker/Presentation/PoseFeedScene/ViewModel/DetailViewModel/PoseDetailViewModel.swift
@@ -11,24 +11,51 @@ import RxRelay
 
 final class PoseDetailViewModel {
     weak var coordinator: PoseFeedCoordinator?
-//    private var posefeedUseCase: PoseFeedUseCase
+    private var posefeedDetailUseCase: PoseFeedDetailUseCase
     private let bindViewModel: PoseFeedPhotoCellViewModel
     
-    init(coordinator: PoseFeedCoordinator?, bindViewModel: PoseFeedPhotoCellViewModel) {
+    init(coordinator: PoseFeedCoordinator?, posefeedDetailUseCase: PoseFeedDetailUseCase, bindViewModel: PoseFeedPhotoCellViewModel) {
         self.coordinator = coordinator
+        self.posefeedDetailUseCase = posefeedDetailUseCase
         self.bindViewModel = bindViewModel
     }
     
     struct Input {
-        
+        let viewDidLoadEvent: Observable<Void>
     }
     
     struct Output {
         let image = BehaviorRelay<UIImage?>(value: nil)
+        let tagItems = BehaviorRelay<[PoseDetailTagCellViewModel]>(value: [])
+        let url = PublishRelay<String>()
     }
     
     func transform(input: Input, disposeBag: DisposeBag) -> Output {
         let output = Output()
+        
+        input.viewDidLoadEvent
+            .subscribe(onNext: { [weak self] in
+                self?.posefeedDetailUseCase.getSourceURLFrom()
+                self?.posefeedDetailUseCase.getTagsFromPoseInfo()
+            })
+            .disposed(by: disposeBag)
+        
+        self.posefeedDetailUseCase
+            .tagItems
+            .map { tags in
+                tags.map { PoseDetailTagCellViewModel(title: $0)}
+            }
+            .subscribe(onNext: {
+                output.tagItems.accept($0)
+            })
+            .disposed(by: disposeBag)
+        
+        self.posefeedDetailUseCase
+            .sourceUrl
+            .subscribe(onNext: {
+                output.url.accept($0)
+            })
+            .disposed(by: disposeBag)
         
         output.image.accept(
             bindViewModel.image.value?.resize(newWidth: UIScreen.main.bounds.width)
