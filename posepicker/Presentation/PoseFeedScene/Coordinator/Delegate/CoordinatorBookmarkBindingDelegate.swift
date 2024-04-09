c6c0b4d71c8191c2f331d126240287f6fe4aef08
Parkjju <rudwns3927@gmail.com>
2024-04-09 18:45:03 +0900

    feat: 코디네이터 북마크값 바인딩 델리게이트 프로토콜 추가

---
+++ posepicker/Presentation/PoseFeedScene/Coordinator/Delegate/CoordinatorBookmarkBindingDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  CoordinatorBookmarkBindingDelegate.swift
+//  posepicker
+//
+//  Created by 박경준 on 4/9/24.
+//
+
+import Foundation
+
+protocol CoordinatorBookmarkBindingDelegate: AnyObject {
+    func coordinatorBookmarkModified(childCoordinator: Coordinator, poseId: Int)
+}
