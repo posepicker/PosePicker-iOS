646c01c104632885d8d7684e9fe667993d60bb3b
Parkjju <rudwns3927@gmail.com>
2024-04-09 19:12:09 +0900

    feat: 북마크 디테일 뷰모델 - UI 바인딩 로직 구현 / tagItems & source & isLoading

--- posepicker/Presentation/BookmarkScene/ViewModel/DetailViewModel/BookmarkDetailViewModel.swift
+++ posepicker/Presentation/BookmarkScene/ViewModel/DetailViewModel/BookmarkDetailViewModel.swift
@@ -27,20 +27,121 @@ final class BookmarkDetailViewModel {
     
     struct Input {
         let viewDidLoadEvent: Observable<Void>
+        let kakaoShareButtonTapEvent: Observable<Void>
+        let linkShareButtonTapEvent: Observable<Void>
+        let imageSourceButtonTapEvent: Observable<Void>
+        let poseTagTapEvent: Observable<BookmarkDetailTagCellViewModel>
     }
     
     struct Output {
         let image = BehaviorRelay<UIImage?>(value: nil)
+        let tagItems = BehaviorRelay<[BookmarkDetailTagCellViewModel]>(value: [])
+        let source = PublishRelay<String>()
+        let isLoading = BehaviorRelay<Bool>(value: false)
     }
     
     func transform(input: Input, disposeBag: DisposeBag) -> Output {
         let output = Output()
+        
+        let sourceURL = BehaviorRelay<String>(value: "")
         
         input.viewDidLoadEvent
             .subscribe(onNext: { [weak self] in
                 self?.poseDetailUseCase.getSourceURLFromPoseInfo()
                 self?.poseDetailUseCase.getTagsFromPoseInfo()
                 self?.poseDetailUseCase.getSourceFromPoseInfo()
+            })
+            .disposed(by: disposeBag)
+        
+        self.poseDetailUseCase
+            .tagItems
+            .map { tags in
+                tags.map { BookmarkDetailTagCellViewModel(title: $0)}
+            }
+            .subscribe(onNext: {
+                output.tagItems.accept($0)
+            })
+            .disposed(by: disposeBag)
+        
+        self.poseDetailUseCase
+            .source
+            .subscribe(onNext: {
+                output.source.accept($0)
+            })
+            .disposed(by: disposeBag)
+        
+        /// 5. 카카오 공유하기 버튼
+        input.kakaoShareButtonTapEvent
+            .withUnretained(self)
+            .subscribe(onNext: { (owner, _) in
+                let template = owner.getFeedTemplateObject()
+                output.isLoading.accept(true)
+                // 메시지 템플릿 encode
+                if let templateJsonData = (try? SdkJSONEncoder.custom.encode(template)) {
+                    // 생성한 메시지 템플릿 객체를 jsonObject로 변환
+                    if let templateJsonObject = SdkUtils.toJsonObject(templateJsonData) {
+                        // 카카오톡 앱이 있는지 체크합니다.
+                        if ShareApi.isKakaoTalkSharingAvailable() {
+                            ShareApi.shared.shareDefault(templateObject:templateJsonObject) {(linkResult, error) in
+                                output.isLoading.accept(false)
+                                if let error = error {
+                                    print("error : \(error)")
+                                }
+                                else {
+                                    print("defaultLink(templateObject:templateJsonObject) success.")
+                                    guard let linkResult = linkResult else { return }
+                                    UIApplication.shared.open(linkResult.url, options: [:], completionHandler: nil)
+                                }
+                            }
+                        } else {
+                            // 없을 경우 카카오톡 앱스토어로 이동합니다. (이거 하려면 URL Scheme에 itms-apps 추가 해야함)
+                            output.isLoading.accept(false)
+                            let url = "itms-apps://itunes.apple.com/app/362057947"
+                            if let url = URL(string: url), UIApplication.shared.canOpenURL(url) {
+                                if #available(iOS 10.0, *) {
+                                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                                } else {
+                                    UIApplication.shared.openURL(url)
+                                }
+                            }
+                        }
+                    }
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        input.linkShareButtonTapEvent
+            .withUnretained(self)
+            .subscribe(onNext: { (owner, _) in
+                UIPasteboard.general.string = "https://www.posepicker.site/detail/\(owner.bindViewModel.poseId.value)"
+                owner.coordinator?.presentClipboardCompleted(poseId: owner.bindViewModel.poseId.value)
+            })
+            .disposed(by: disposeBag)
+        
+        input.imageSourceButtonTapEvent
+            .withUnretained(self)
+            .flatMapLatest { (owner, _) in
+                return sourceURL
+            }
+            .map {
+                return URL(string: "https://" + $0)
+            }
+            .compactMap { $0 }
+            .subscribe(onNext: { [weak self] in
+                self?.coordinator?.moveToExternalApp(url: $0)
+            })
+            .disposed(by: disposeBag)
+        
+        self.poseDetailUseCase
+            .sourceUrl
+            .subscribe(onNext: {
+                sourceURL.accept($0)
+            })
+            .disposed(by: disposeBag)
+        
+        input.poseTagTapEvent
+            .subscribe(onNext: { [weak self] in
+                self?.coordinator?.dismissPoseDetail(tag: $0.title.value)
             })
             .disposed(by: disposeBag)
         
