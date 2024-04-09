1e71a8a086d2b2666bbf192a5fc009657f13f792
Parkjju <rudwns3927@gmail.com>
2024-04-10 02:08:08 +0900

    feat: 마이페이지 기존 코드 삭제 / 뷰모델 연동

--- posepicker/Presentation/MyPageScene/ViewController/MyPageViewController.swift
+++ posepicker/Presentation/MyPageScene/ViewController/MyPageViewController.swift
@@ -86,7 +86,7 @@ class MyPageViewController: BaseViewController, UIGestureRecognizerDelegate {
             $0.titleLabel?.font = .pretendard(.medium, ofSize: 16)
         }
     
-    let privacyInforationButton = UIButton(type: .system)
+    let privacyInformationButton = UIButton(type: .system)
         .then {
             $0.setTitleColor(.textPrimary, for: .normal)
             $0.setTitle("개인정보 처리방침", for: .normal)
@@ -183,114 +183,114 @@ class MyPageViewController: BaseViewController, UIGestureRecognizerDelegate {
             })
             .disposed(by: disposeBag)
         
-        serviceUsageInquiryButton.rx.tap.asDriver()
-            .drive(onNext: { [unowned self] in
-                let popupViewController = PopUpViewController(isLoginPopUp: false, isChoice: true)
-                popupViewController.modalTransitionStyle = .crossDissolve
-                popupViewController.modalPresentationStyle = .overFullScreen
-                let popupView = popupViewController.popUpView as! PopUpView
-                popupView.alertText.accept("문의사항을 남기시겠습니까?")
-                
-                popupView.confirmButton.rx.tap.asDriver()
-                    .drive(onNext: { [weak self] in
-//                        self?.coordinator.pushWebView(urlString: "https://litt.ly/posepicker", pageTitle: "문의하기")
-                        popupViewController.dismiss(animated: true)
-                    })
-                    .disposed(by: self.disposeBag)
-                
-                popupView.cancelButton.rx.tap.asDriver()
-                    .drive(onNext: {
-                        popupViewController.dismiss(animated: true)
-                    })
-                    .disposed(by: self.disposeBag)
-                
-                self.present(popupViewController, animated: true)
-            })
-            .disposed(by: disposeBag)
+//        serviceUsageInquiryButton.rx.tap.asDriver()
+//            .drive(onNext: { [unowned self] in
+//                let popupViewController = PopUpViewController(isLoginPopUp: false, isChoice: true)
+//                popupViewController.modalTransitionStyle = .crossDissolve
+//                popupViewController.modalPresentationStyle = .overFullScreen
+//                let popupView = popupViewController.popUpView as! PopUpView
+//                popupView.alertText.accept("문의사항을 남기시겠습니까?")
+//                
+//                popupView.confirmButton.rx.tap.asDriver()
+//                    .drive(onNext: { [weak self] in
+////                        self?.coordinator.pushWebView(urlString: "https://litt.ly/posepicker", pageTitle: "문의하기")
+//                        popupViewController.dismiss(animated: true)
+//                    })
+//                    .disposed(by: self.disposeBag)
+//                
+//                popupView.cancelButton.rx.tap.asDriver()
+//                    .drive(onNext: {
+//                        popupViewController.dismiss(animated: true)
+//                    })
+//                    .disposed(by: self.disposeBag)
+//                
+//                self.present(popupViewController, animated: true)
+//            })
+//            .disposed(by: disposeBag)
         
-        noticeButton.rx.tap.asDriver()
-            .drive(onNext: { [weak self] in
-//                self?.coordinator.pushWebView(urlString: "https://shineshine.notion.site/fde248040bed45f68fbfa3004e2c4856", pageTitle: "공지사항")
-            })
-            .disposed(by: disposeBag)
+//        noticeButton.rx.tap.asDriver()
+//            .drive(onNext: { [weak self] in
+////                self?.coordinator.pushWebView(urlString: "https://shineshine.notion.site/fde248040bed45f68fbfa3004e2c4856", pageTitle: "공지사항")
+//            })
+//            .disposed(by: disposeBag)
         
-        faqButton.rx.tap.asDriver()
-            .drive(onNext: { [weak self] in
-//                self?.coordinator.pushWebView(urlString: "https://shineshine.notion.site/cc71decc2e534ae6abb195bb10a501c0", pageTitle: "자주 묻는 질문")
-            })
-            .disposed(by: disposeBag)
+//        faqButton.rx.tap.asDriver()
+//            .drive(onNext: { [weak self] in
+////                self?.coordinator.pushWebView(urlString: "https://shineshine.notion.site/cc71decc2e534ae6abb195bb10a501c0", pageTitle: "자주 묻는 질문")
+//            })
+//            .disposed(by: disposeBag)
         
-        snsButton.rx.tap.asDriver()
-            .drive(onNext: { [weak self] in
-//                self?.coordinator.pushWebView(urlString: "https://www.instagram.com/posepicker?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==", pageTitle: "포즈피커 공식 SNS")
-            })
-            .disposed(by: disposeBag)
+//        snsButton.rx.tap.asDriver()
+//            .drive(onNext: { [weak self] in
+////                self?.coordinator.pushWebView(urlString: "https://www.instagram.com/posepicker?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==", pageTitle: "포즈피커 공식 SNS")
+//            })
+//            .disposed(by: disposeBag)
         
-        serviceInformationButton.rx.tap.asDriver()
-            .drive(onNext: { [weak self] in
-//                self?.coordinator.pushWebView(urlString: "https://shineshine.notion.site/3113eb146abb4b5c809070c3f01380da", pageTitle: "이용약관")
-            })
-            .disposed(by: disposeBag)
+//        serviceInformationButton.rx.tap.asDriver()
+//            .drive(onNext: { [weak self] in
+////                self?.coordinator.pushWebView(urlString: "https://shineshine.notion.site/3113eb146abb4b5c809070c3f01380da", pageTitle: "이용약관")
+//            })
+//            .disposed(by: disposeBag)
         
-        privacyInforationButton.rx.tap.asDriver()
-            .drive(onNext: { [weak self] in
-//                self?.coordinator.pushWebView(urlString: "https://shineshine.notion.site/75e98a2462824b839a9c37473a6afbd5", pageTitle: "개인정보 처리방침")
-            })
-            .disposed(by: disposeBag)
+//        privacyInforationButton.rx.tap.asDriver()
+//            .drive(onNext: { [weak self] in
+////                self?.coordinator.pushWebView(urlString: "https://shineshine.notion.site/75e98a2462824b839a9c37473a6afbd5", pageTitle: "개인정보 처리방침")
+//            })
+//            .disposed(by: disposeBag)
         
-        logoutButton.rx.tap.asDriver()
-            .drive(onNext: { [unowned self] in
-                let popupViewController = PopUpViewController(isLoginPopUp: false, isChoice: true, isLabelNeeded: true)
-                popupViewController.modalTransitionStyle = .crossDissolve
-                popupViewController.modalPresentationStyle = .overFullScreen
-                let popupView = popupViewController.popUpView as! PopUpView
-                popupView.alertMainLabel.text = "로그아웃"
-                popupView.alertText.accept("북마크는 로그인 시에만 유지되어요.\n정말 로그아웃하시겠어요?")
-                popupView.confirmButton.setTitle("로그인 유지", for: .normal)
-                popupView.cancelButton.setTitle("로그아웃", for: .normal)
-                
-                popupView.confirmButton.rx.tap.asDriver()
-                    .drive(onNext: { [weak self] in
-                        self?.dismiss(animated: true)
-                    })
-                    .disposed(by: disposeBag)
-                
-                // 현재 로그인된 계정이 카카오인지 모름
-                // 토큰 삭제는 뷰모델에서 이루어짐
-                popupView.cancelButton.rx.tap.asDriver()
-                    .drive(onNext: { [weak self] in
-                        guard let self = self else { return }
-                        if let socialLogin = UserDefaults.standard.string(forKey: K.SocialLogin.socialLogin),
-                           socialLogin == K.SocialLogin.kakao {
-                            UserApi.shared.rx.logout()
-                                .subscribe(onCompleted: {
-                                    print("kakao logout completed")
-                                })
-                                .disposed(by: self.disposeBag)
-                        }
-
-                        self.logoutTrigger.onNext(())
-                        self.loginStateTrigger.onNext(())
-                        self.dismiss(animated: true)
-                    })
-                    .disposed(by: disposeBag)
-                
-                self.present(popupViewController, animated: true)
-            })
-            .disposed(by: disposeBag)
+//        logoutButton.rx.tap.asDriver()
+//            .drive(onNext: { [unowned self] in
+//                let popupViewController = PopUpViewController(isLoginPopUp: false, isChoice: true, isLabelNeeded: true)
+//                popupViewController.modalTransitionStyle = .crossDissolve
+//                popupViewController.modalPresentationStyle = .overFullScreen
+//                let popupView = popupViewController.popUpView as! PopUpView
+//                popupView.alertMainLabel.text = "로그아웃"
+//                popupView.alertText.accept("북마크는 로그인 시에만 유지되어요.\n정말 로그아웃하시겠어요?")
+//                popupView.confirmButton.setTitle("로그인 유지", for: .normal)
+//                popupView.cancelButton.setTitle("로그아웃", for: .normal)
+//                
+//                popupView.confirmButton.rx.tap.asDriver()
+//                    .drive(onNext: { [weak self] in
+//                        self?.dismiss(animated: true)
+//                    })
+//                    .disposed(by: disposeBag)
+//                
+//                // 현재 로그인된 계정이 카카오인지 모름
+//                // 토큰 삭제는 뷰모델에서 이루어짐
+//                popupView.cancelButton.rx.tap.asDriver()
+//                    .drive(onNext: { [weak self] in
+//                        guard let self = self else { return }
+//                        if let socialLogin = UserDefaults.standard.string(forKey: K.SocialLogin.socialLogin),
+//                           socialLogin == K.SocialLogin.kakao {
+//                            UserApi.shared.rx.logout()
+//                                .subscribe(onCompleted: {
+//                                    print("kakao logout completed")
+//                                })
+//                                .disposed(by: self.disposeBag)
+//                        }
+//
+//                        self.logoutTrigger.onNext(())
+//                        self.loginStateTrigger.onNext(())
+//                        self.dismiss(animated: true)
+//                    })
+//                    .disposed(by: disposeBag)
+//                
+//                self.present(popupViewController, animated: true)
+//            })
+//            .disposed(by: disposeBag)
         
-        signoutButton.rx.tap.asDriver()
-            .drive(onNext: { [unowned self] in
-                let revokeVC = UserRevokeViewController()
-                self.navigationController?.pushViewController(revokeVC, animated: true)
-            })
-            .disposed(by: disposeBag)
+//        signoutButton.rx.tap.asDriver()
+//            .drive(onNext: { [unowned self] in
+//                let revokeVC = UserRevokeViewController()
+//                self.navigationController?.pushViewController(revokeVC, animated: true)
+//            })
+//            .disposed(by: disposeBag)
         
         setBottomBorder()
     }
     
     override func render() {
-        view.addSubViews([loginButton, loginLogo, loginLogoStar, loginTitle, loginSubTitle, emailLabel, noticeButton, faqButton, snsButton, serviceUsageInquiryButton, serviceInformationButton, privacyInforationButton, logoutButton, signoutButton, loginToast, logoutToast, revokeToast])
+        view.addSubViews([loginButton, loginLogo, loginLogoStar, loginTitle, loginSubTitle, emailLabel, noticeButton, faqButton, snsButton, serviceUsageInquiryButton, serviceInformationButton, privacyInformationButton, logoutButton, signoutButton, loginToast, logoutToast, revokeToast])
         
         loginButton.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(12)
@@ -355,15 +355,15 @@ class MyPageViewController: BaseViewController, UIGestureRecognizerDelegate {
             make.height.equalTo(24)
         }
         
-        privacyInforationButton.snp.makeConstraints { make in
+        privacyInformationButton.snp.makeConstraints { make in
             make.leading.equalTo(serviceInformationButton)
             make.top.equalTo(serviceInformationButton.snp.bottom).offset(24)
             make.height.equalTo(24)
         }
         
         logoutButton.snp.makeConstraints { make in
-            make.leading.equalTo(privacyInforationButton)
-            make.top.equalTo(privacyInforationButton.snp.bottom).offset(24)
+            make.leading.equalTo(privacyInformationButton)
+            make.top.equalTo(privacyInformationButton.snp.bottom).offset(24)
             make.height.equalTo(24)
         }
         
@@ -396,6 +396,18 @@ class MyPageViewController: BaseViewController, UIGestureRecognizerDelegate {
     }
     
     override func bindViewModel() {
+        let input = MyPageViewModel.Input(
+            noticeButtonTapEvent: noticeButton.rx.tap.asObservable(),
+            faqButtonTapEvent: faqButton.rx.tap.asObservable(),
+            snsButtonTapEvent: snsButton.rx.tap.asObservable(),
+            serviceInquiryButtonTapEvent: serviceUsageInquiryButton.rx.tap.asObservable(),
+            serviceInformationButtonTapEvent: serviceInformationButton.rx.tap.asObservable(),
+            privacyInformationButtonTapEvent: privacyInformationButton.rx.tap.asObservable(),
+            logoutButtonTapEventTapEvent: logoutButton.rx.tap.asObservable(),
+            signoutButtonTapEventTapEvent: signoutButton.rx.tap.asObservable()
+        )
+        
+        let output = viewModel?.transform(input: input, disposeBag: disposeBag)
 //        let input = MyPageViewModel.Input(appleIdentityTokenTrigger: appleIdentityTokenTrigger, kakaoLoginTrigger: Observable.combineLatest(kakaoEmailTrigger, kakaoIdTrigger), logoutButtonTapped: logoutTrigger, revokeButtonTapped: revokeTrigger)
 //        let output = viewModel.transform(input: input)
 //        
