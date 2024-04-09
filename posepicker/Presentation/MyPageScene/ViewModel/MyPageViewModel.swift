bdb73a8dcdc8ce72ea7768556685905014db967a
Parkjju <rudwns3927@gmail.com>
2024-04-10 02:05:40 +0900

    feat: 마이페이지 뷰모델 - 웹뷰 푸시 인풋 연동

--- posepicker/Presentation/MyPageScene/ViewModel/MyPageViewModel.swift
+++ posepicker/Presentation/MyPageScene/ViewModel/MyPageViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import RxSwift
 
 final class MyPageViewModel {
     weak var coordinator: MyPageCoordinator?
@@ -14,5 +15,62 @@ final class MyPageViewModel {
     init(coordinator: MyPageCoordinator?, myPageUseCase: MyPageUseCase) {
         self.coordinator = coordinator
         self.myPageUseCase = myPageUseCase
+    }
+    
+    struct Input {
+        let noticeButtonTapEvent: Observable<Void>
+        let faqButtonTapEvent: Observable<Void>
+        let snsButtonTapEvent: Observable<Void>
+        let serviceInquiryButtonTapEvent: Observable<Void>
+        let serviceInformationButtonTapEvent: Observable<Void>
+        let privacyInformationButtonTapEvent: Observable<Void>
+        let logoutButtonTapEventTapEvent: Observable<Void>
+        let signoutButtonTapEventTapEvent: Observable<Void>
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input, disposeBag: DisposeBag) -> Output {
+        let output = Output()
+        
+        input.noticeButtonTapEvent
+            .subscribe(onNext: { [weak self] in
+                self?.coordinator?.pushWebView(webView: .notice)
+            })
+            .disposed(by: disposeBag)
+        
+        input.faqButtonTapEvent
+            .subscribe(onNext: { [weak self] in
+                self?.coordinator?.pushWebView(webView: .faq)
+            })
+            .disposed(by: disposeBag)
+        
+        input.snsButtonTapEvent
+            .subscribe(onNext: { [weak self] in
+                self?.coordinator?.pushWebView(webView: .sns)
+            })
+            .disposed(by: disposeBag)
+        
+        input.serviceInquiryButtonTapEvent
+            .subscribe(onNext: { [weak self] in
+                self?.coordinator?.pushWebView(webView: .serviceInquiry)
+            })
+            .disposed(by: disposeBag)
+        
+        input.serviceInformationButtonTapEvent
+            .subscribe(onNext: { [weak self] in
+                self?.coordinator?.pushWebView(webView: .serviceInformation)
+            })
+            .disposed(by: disposeBag)
+        
+        input.privacyInformationButtonTapEvent
+            .subscribe(onNext: { [weak self] in
+                self?.coordinator?.pushWebView(webView: .privacyInformation)
+            })
+            .disposed(by: disposeBag)
+        
+        return output
     }
 }
