e29c8d6fcf357de7597e57fb8f150fd43193b50b
Parkjju <rudwns3927@gmail.com>
2024-04-11 00:38:34 +0900

    feat: 포즈 가이드라인 뷰모델 - 가이드라인 확인하기 버튼 인풋 구현

--- posepicker/Presentation/MyPoseScene/ViewModel/MyPoseGuidelineViewModel.swift
+++ posepicker/Presentation/MyPoseScene/ViewModel/MyPoseGuidelineViewModel.swift
@@ -5,8 +5,35 @@
 //  Created by 박경준 on 4/10/24.
 //
 
-import Foundation
+import UIKit
+import RxSwift
 
 final class MyPoseGuidelineViewModel {
+    weak var coordinator: MyPoseCoordinator?
     
+    init(coordinator: MyPoseCoordinator?) {
+        self.coordinator = coordinator
+    }
+    
+    struct Input {
+        let guidelineCheckButtonTapEvent: Observable<Void>
+        let imageLoadCompletedEvent: Observable<UIImage?>
+        let imageLoadFailedEvent: Observable<Void>
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input, disposeBag: DisposeBag) -> Output {
+        let output = Output()
+        
+        input.guidelineCheckButtonTapEvent
+            .subscribe(onNext: { [weak self] in
+                self?.coordinator?.pushGuideline()
+            })
+            .disposed(by: disposeBag)
+        
+        return output
+    }
 }
