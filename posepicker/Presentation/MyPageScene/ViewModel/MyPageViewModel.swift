e3f960647f68eb827b1db6da88ac1d959f494b45
Parkjju <rudwns3927@gmail.com>
2024-04-10 10:28:02 +0900

    feat: 마이페이지 뷰모델 - 로그아웃 버튼 탭 이벤트 / 로그인 버튼 탭 이벤트 구현 / 코디네이터 호출 및 인증로직 호출

--- posepicker/Presentation/MyPageScene/ViewModel/MyPageViewModel.swift
+++ posepicker/Presentation/MyPageScene/ViewModel/MyPageViewModel.swift
@@ -7,14 +7,17 @@
 
 import Foundation
 import RxSwift
+import RxRelay
 
 final class MyPageViewModel {
     weak var coordinator: MyPageCoordinator?
     private let myPageUseCase: MyPageUseCase
+    private let commonUseCase: CommonUseCase
     
-    init(coordinator: MyPageCoordinator?, myPageUseCase: MyPageUseCase) {
+    init(coordinator: MyPageCoordinator?, myPageUseCase: MyPageUseCase, commonUseCase: CommonUseCase) {
         self.coordinator = coordinator
         self.myPageUseCase = myPageUseCase
+        self.commonUseCase = commonUseCase
     }
     
     struct Input {
@@ -24,12 +27,13 @@ final class MyPageViewModel {
         let serviceInquiryButtonTapEvent: Observable<Void>
         let serviceInformationButtonTapEvent: Observable<Void>
         let privacyInformationButtonTapEvent: Observable<Void>
-        let logoutButtonTapEventTapEvent: Observable<Void>
-        let signoutButtonTapEventTapEvent: Observable<Void>
+        let logoutButtonTapEvent: Observable<Void>
+        let signoutButtonTapEvent: Observable<Void>
+        let loginButtonTapEvent: Observable<Void>
     }
     
     struct Output {
-        
+        let refreshLoginState = PublishSubject<Bool>()
     }
     
     func transform(input: Input, disposeBag: DisposeBag) -> Output {
@@ -68,6 +72,44 @@ final class MyPageViewModel {
         input.privacyInformationButtonTapEvent
             .subscribe(onNext: { [weak self] in
                 self?.coordinator?.pushWebView(webView: .privacyInformation)
+            })
+            .disposed(by: disposeBag)
+        
+        input.loginButtonTapEvent
+            .withUnretained(self)
+            .flatMapLatest { (owner, _) -> Observable<LoginPopUpView.SocialLogin> in
+                guard let coordinator = owner.coordinator else { return .empty() }
+                return coordinator.loginDelegate?.coordinatorLoginRequested(childCoordinator: coordinator) ?? .empty()
+            }
+            .subscribe(onNext: { [weak self] in
+                switch $0 {
+                case .apple:
+                    guard let idToken = try? KeychainManager.shared.retrieveItem(ofClass: .password, key: K.Parameters.idToken) else { return }
+                    self?.commonUseCase.loginWithApple(idToken: idToken)
+                case .kakao:
+                    self?.commonUseCase.loginWithKakao()
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        self.commonUseCase
+            .loginCompleted
+            .subscribe(onNext: { [weak self] in
+                guard let coordinator = self?.coordinator else { return }
+                self?.coordinator?.loginDelegate?.coordinatorLoginCompleted(childCoordinator: coordinator)
+                output.refreshLoginState.onNext(UserDefaults.standard.bool(forKey: K.SocialLogin.isLoggedIn))
+            })
+            .disposed(by: disposeBag)
+        
+        input.logoutButtonTapEvent
+            .withUnretained(self)
+            .flatMapLatest { (owner, _ ) -> Observable<LoginPopUpView.SocialLogin?> in
+                guard let coordinator = owner.coordinator else { return .empty() }
+                return coordinator.presentLogoutPopup(disposeBag: disposeBag)
+            }
+            .compactMap { $0 }
+            .subscribe(onNext: { [weak self] in
+                self?.commonUseCase.logout(with: $0)
             })
             .disposed(by: disposeBag)
         
