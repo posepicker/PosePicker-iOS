5cd564d149473fba3eec33db79a7709ea91ed488
Parkjju <rudwns3927@gmail.com>
2024-04-10 13:21:57 +0900

    feat: RevokeViewModel 정의 - 버튼 인풋에 따라 코디네이터 화면 전환 로직 구성

---
+++ posepicker/Presentation/MyPageScene/ViewModel/Revoke/UserRevokeViewModel.swift
@@ -0,0 +1,59 @@
+//
+//  UserRevokeViewModel.swift
+//  posepicker
+//
+//  Created by 박경준 on 4/10/24.
+//
+
+import Foundation
+import RxSwift
+import RxRelay
+
+final class UserRevokeViewModel {
+    weak var coordinator: MyPageCoordinator?
+    private let commonUseCase: CommonUseCase
+    
+    init(coordinator: MyPageCoordinator?, commonUseCase: CommonUseCase) {
+        self.coordinator = coordinator
+        self.commonUseCase = commonUseCase
+    }
+    
+    struct Input {
+        let revokeButtonTapEvent: Observable<Void>
+        let revokeCancelButtonTapEvent: Observable<Void>
+        let revokeReason: BehaviorRelay<String>
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input, disposeBag: DisposeBag) -> Output {
+        let output = Output()
+        
+        input.revokeButtonTapEvent
+            .withUnretained(self)
+            .flatMapLatest { (owner, _) -> Observable<LoginPopUpView.SocialLogin?> in
+                guard let coordinator = owner.coordinator else { return .empty() }
+                return coordinator.presentRevokeConfirmPopup(disposeBag: disposeBag)
+            }
+            .compactMap { $0 }
+            .subscribe(onNext: { [weak self] in
+                switch $0 {
+                case .apple:
+                    self?.commonUseCase.revoke(with: .apple, reason: input.revokeReason.value)
+                case .kakao:
+                    self?.commonUseCase.revoke(with: .kakao, reason: input.revokeReason.value)
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        input.revokeCancelButtonTapEvent
+            .subscribe(onNext: { [weak self] in
+                self?.coordinator?.popRevokeView()
+            })
+            .disposed(by: disposeBag)
+        
+        return output
+    }
+}
