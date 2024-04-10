2e1ca393f537e9e92c0ff0eb08521f52075acd5b
Parkjju <rudwns3927@gmail.com>
2024-04-10 10:29:06 +0900

    feat: 마이페이지 코디네이터 - 로그인 델리게이트 & 로그아웃 팝업 속성 추가

--- posepicker/Presentation/MyPageScene/Coordinator/Protocol/MyPageCoordinator.swift
+++ posepicker/Presentation/MyPageScene/Coordinator/Protocol/MyPageCoordinator.swift
@@ -6,7 +6,10 @@
 //
 
 import Foundation
+import RxSwift
 
 protocol MyPageCoordinator: Coordinator {
+    var loginDelegate: CoordinatorLoginDelegate? { get set }
     func pushWebView(webView: WebViewList)
+    func presentLogoutPopup(disposeBag: DisposeBag) -> Observable<LoginPopUpView.SocialLogin?>
 }
