e2ae9890b7acb4c487f35f32361d996d0a25432d
Parkjju <rudwns3927@gmail.com>
2024-04-11 13:49:20 +0900

    feat: 포즈 업로드 뷰모델 - 페이지뷰 코디네이터 호출 로직 구현

--- posepicker/Presentation/PoseUploadScene/ViewModel/PoseUploadViewModel.swift
+++ posepicker/Presentation/PoseUploadScene/ViewModel/PoseUploadViewModel.swift
@@ -5,8 +5,9 @@
 //  Created by 박경준 on 4/11/24.
 //
 
-import Foundation
+import UIKit
 import RxSwift
+import RxRelay
 
 final class PoseUploadViewModel {
     weak var coordinator: PoseUploadCoordinator?
@@ -17,15 +18,44 @@ final class PoseUploadViewModel {
     }
     
     struct Input {
-        
+        let pageviewTransitionDelegateEvent: Observable<Void>
+        let currentPage: Observable<Int>
     }
     
     struct Output {
-        
+        let pageTransitionEvent = PublishRelay<Int>()
     }
     
     func transform(input: Input, disposeBag: DisposeBag) -> Output {
         let output = Output()
+        
+        input.pageviewTransitionDelegateEvent
+            .subscribe(onNext: { [weak self] in
+                guard let self = self,
+                      let coordinator = self.coordinator else {return}
+                coordinator.selectPage(coordinator.currentPage() ?? .headcount)
+                output.pageTransitionEvent.accept(coordinator.currentPage()?.pageOrderNumber() ?? 0)
+            })
+            .disposed(by: disposeBag)
+        
+        input.currentPage
+            .subscribe(onNext: { [weak self] in
+                guard let self = self,
+                      let coordinator = self.coordinator else { return }
+                coordinator.setSelectedIndex($0)
+            })
+            .disposed(by: disposeBag)
+        
         return output
+    }
+    
+    /// UIPageViewController DataSource - viewControllerBefore
+    func viewControllerBefore() -> UIViewController? {
+        return self.coordinator?.viewControllerBefore()
+    }
+    
+    /// UIPageViewController DataSource -> viewControllerAfter
+    func viewControllerAfter() -> UIViewController? {
+        return self.coordinator?.viewControllerAfter()
     }
 }
