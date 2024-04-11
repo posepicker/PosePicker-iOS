6ae8b94f3a99233abcdec42720ea084c600f3ca7
Parkjju <rudwns3927@gmail.com>
2024-04-11 10:24:30 +0900

    feat: 포즈업로드 뷰모델 클래스 정의

--- posepicker/Presentation/PoseUploadScene/ViewModel/PoseUploadViewModel.swift
+++ posepicker/Presentation/PoseUploadScene/ViewModel/PoseUploadViewModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import RxSwift
+
+final class PoseUploadViewModel {
+    weak var coordinator: PoseUploadCoordinator?
+//    private let bookmarkUseCase: BookmarkUseCase
+    
+    init(coordinator: PoseUploadCoordinator?) {
+        self.coordinator = coordinator
+    }
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input, disposeBag: DisposeBag) -> Output {
+        let output = Output()
+        return output
+    }
+}
