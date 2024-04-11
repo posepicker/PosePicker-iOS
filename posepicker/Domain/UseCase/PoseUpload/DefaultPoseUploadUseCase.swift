ed4f1f83c3231491ea832f25b4b6af84969cdba2
Parkjju <rudwns3927@gmail.com>
2024-04-11 17:40:18 +0900

    feat: 포즈 업로드 레파지토리 & 유스케이스 구현 / 멀티파트 통신

---
+++ posepicker/Domain/UseCase/PoseUpload/DefaultPoseUploadUseCase.swift
@@ -0,0 +1,44 @@
+//
+//  DefaultPoseUploadUseCase.swift
+//  posepicker
+//
+//  Created by 박경준 on 4/11/24.
+//
+
+import UIKit
+import RxSwift
+
+final class DefaultPoseUploadUseCase: PoseUploadUseCase {
+    
+    var disposeBag = DisposeBag()
+    let poseUploadRepository: PoseUploadRepository
+    
+    init(poseUploadRepository: PoseUploadRepository) {
+        self.poseUploadRepository = poseUploadRepository
+    }
+    
+    var uploadCompletedEvent = PublishSubject<PoseInfo>()
+    
+    func savePose(
+        image: UIImage?,
+        frameCount: String,
+        peopleCount: String,
+        source: String,
+        sourceUrl: String,
+        tag: String
+    ) {
+        poseUploadRepository
+            .savePose(
+                image: image,
+                frameCount: frameCount,
+                peopleCount: peopleCount,
+                source: source,
+                sourceUrl: sourceUrl,
+                tag: tag
+            )
+            .subscribe(onNext: { [weak self] in
+                self?.uploadCompletedEvent.onNext($0)
+            })
+            .disposed(by: disposeBag)
+    }
+}
