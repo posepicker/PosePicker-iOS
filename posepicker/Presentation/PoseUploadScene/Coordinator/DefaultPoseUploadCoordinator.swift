e35e652b4d08ce01bfddd8ac9ab798b3707ffe2f
Parkjju <rudwns3927@gmail.com>
2024-04-11 12:42:07 +0900

    feat: 포즈 업로드 코디네이터 페이지뷰 관련 동작 정의

--- posepicker/Presentation/PoseUploadScene/Coordinator/DefaultPoseUploadCoordinator.swift
+++ posepicker/Presentation/PoseUploadScene/Coordinator/DefaultPoseUploadCoordinator.swift
@@ -11,8 +11,11 @@ final class DefaultPoseUploadCoordinator: PoseUploadCoordinator {
     weak var finishDelegate: CoordinatorFinishDelegate?
     var navigationController: UINavigationController
     var myPoseGuidelineViewController: MyPoseGuidelineViewController
+    var poseUploadViewController: PoseUploadViewController?
+    var pageViewController: UIPageViewController?
     var childCoordinators: [Coordinator] = []
     var type: CoordinatorType = .mypose
+    var controllers: [UINavigationController] = []
     
     required init(_ navigationController: UINavigationController) {
         self.navigationController = navigationController
@@ -52,8 +55,69 @@ final class DefaultPoseUploadCoordinator: PoseUploadCoordinator {
         let poseUploadViewController = PoseUploadViewController(registeredImage: image)
         poseUploadViewController.viewModel = PoseUploadViewModel(coordinator: self)
         
+        self.pageViewController = poseUploadViewController.pageViewController
+        
         let navigationController = UINavigationController(rootViewController: poseUploadViewController)
         navigationController.modalPresentationStyle = .overFullScreen
         self.navigationController.presentedViewController?.present(navigationController, animated: true)
+    }
+    
+    func selectPage(_ page: PageViewType) {
+        <#code#>
+    }
+    
+    func setSelectedIndex(_ index: Int) {
+        <#code#>
+    }
+    
+    func currentPage() -> PageViewType? {
+        <#code#>
+    }
+    
+    func viewControllerBefore() -> UIViewController? {
+        <#code#>
+    }
+    
+    func viewControllerAfter() -> UIViewController? {
+        <#code#>
+    }
+    
+    private func createViewControllers() {
+        let pages: [PageViewType] = [.posepick, .posetalk, .posefeed]
+        controllers = pages.map({
+            self.createPageViewNavigationController(of: $0)
+        })
+        self.configurePageViewController(with: controllers)
+    }
+    
+    private func configurePageViewController(with pageViewControllers: [UIViewController]) {
+        guard let poseUploadViewController = self.poseUploadViewController else { return }
+        self.pageViewController?.delegate = poseUploadViewController
+        self.pageViewController?.dataSource = poseUploadViewController
+        self.pageViewController?.setViewControllers([pageViewControllers[0]], direction: .forward, animated: true)
+        self.navigationController.pushViewController(poseUploadViewController, animated: true)
+    }
+    
+    private func startPageViewCoordinator(of page: PageViewType, to pageviewNavigationController: UINavigationController) {
+        switch page {
+        case .posepick:
+            self.navigationController.push
+            let posepickCoordinator = DefaultPosePickCoordinator(pageviewNavigationController)
+            posepickCoordinator.finishDelegate = self
+            self.childCoordinators.append(posepickCoordinator)
+            posepickCoordinator.start()
+        case .posetalk:
+            let posetalkCoordinator = DefaultPoseTalkCoordinator(pageviewNavigationController)
+            posetalkCoordinator.finishDelegate = self
+            self.childCoordinators.append(posetalkCoordinator)
+            posetalkCoordinator.start()
+        case .posefeed:
+            let posefeedCoordinator = DefaultPoseFeedCoordinator(pageviewNavigationController)
+            posefeedCoordinator.loginDelegate = self
+            self.childCoordinators.append(posefeedCoordinator)
+            posefeedCoordinator.start()
+        default:
+            break
+        }
     }
 }
--- posepicker/Presentation/PoseUploadScene/Coordinator/Protocol/PoseUploadCoordinator.swift
+++ posepicker/Presentation/PoseUploadScene/Coordinator/Protocol/PoseUploadCoordinator.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 protocol PoseUploadCoordinator: Coordinator {
+    var pageViewController: UIPageViewController? { get set }
     func pushGuideline()
     func presentImageLoadFailedPopup()
     func pushPoseUploadView(image: UIImage?)
+
+    func selectPage(_ page: PageViewType)
+    func setSelectedIndex(_ index: Int)
+    func currentPage() -> PageViewType?
+    func viewControllerBefore() -> UIViewController?
+    func viewControllerAfter() -> UIViewController?
 }
