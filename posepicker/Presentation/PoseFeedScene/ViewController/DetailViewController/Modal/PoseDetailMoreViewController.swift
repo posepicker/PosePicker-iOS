307b370d890dc64403d7a4e17660eac5cd27da34
Parkjju <rudwns3927@gmail.com>
2024-04-12 14:32:43 +0900

    feat: 포즈 디테일 더보기 인풋 & 아웃풋 정의

--- posepicker/Presentation/PoseFeedScene/ViewController/DetailViewController/Modal/PoseDetailMoreViewController.swift
+++ posepicker/Presentation/PoseFeedScene/ViewController/DetailViewController/Modal/PoseDetailMoreViewController.swift
@@ -12,13 +12,12 @@ import RxSwift
 class PoseDetailMoreViewController: BaseViewController {
 
     // MARK: - Subviews
+    let closeButton = UIBarButtonItem(image: ImageLiteral.imgClose24.withRenderingMode(.alwaysOriginal).withTintColor(.iconDefault), style: .plain, target: self, action: #selector(closeButtonTapped))
     
     lazy var navigationBar = UINavigationBar()
         .then {
-            let closeButton = UIBarButtonItem(image: ImageLiteral.imgClose24.withRenderingMode(.alwaysOriginal).withTintColor(.iconDefault), style: .plain, target: self, action: #selector(closeButtonTapped))
-
             let navigationItem = UINavigationItem(title: "")
-            navigationItem.rightBarButtonItem = closeButton
+            navigationItem.rightBarButtonItem = self.closeButton
             $0.items = [navigationItem]
             $0.barTintColor = .bgWhite
         }
@@ -31,16 +30,17 @@ class PoseDetailMoreViewController: BaseViewController {
             $0.titleLabel?.font = .pretendard(.medium, ofSize: 16)
         }
     
-    let blockButton = UIButton(type: .system)
-        .then {
-            $0.setTitle("차단하기", for: .normal)
-            $0.setTitleColor(.textPrimary, for: .normal)
-            $0.contentHorizontalAlignment = .leading
-            $0.titleLabel?.font = .pretendard(.medium, ofSize: 16)
-        }
+//    let blockButton = UIButton(type: .system)
+//        .then {
+//            $0.setTitle("차단하기", for: .normal)
+//            $0.setTitleColor(.textPrimary, for: .normal)
+//            $0.contentHorizontalAlignment = .leading
+//            $0.titleLabel?.font = .pretendard(.medium, ofSize: 16)
+//        }
     
     // MARK: - Properties
     let poseId: Int
+    var viewModel: PoseDetailMoreViewModel?
     
     // MARK: - Initialization
     
@@ -61,7 +61,7 @@ class PoseDetailMoreViewController: BaseViewController {
     
     // MARK: - Functions
     override func render() {
-        self.view.addSubViews([navigationBar, reportButton, blockButton])
+        self.view.addSubViews([navigationBar, reportButton/*, blockButton*/])
         
         navigationBar.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview()
@@ -75,11 +75,11 @@ class PoseDetailMoreViewController: BaseViewController {
             make.top.equalTo(navigationBar.snp.bottom)
         }
         
-        blockButton.snp.makeConstraints { make in
-            make.height.equalTo(48)
-            make.leading.trailing.equalTo(view).inset(20)
-            make.top.equalTo(reportButton.snp.bottom)
-        }
+//        blockButton.snp.makeConstraints { make in
+//            make.height.equalTo(48)
+//            make.leading.trailing.equalTo(view).inset(20)
+//            make.top.equalTo(reportButton.snp.bottom)
+//        }
     }
     
     override func configUI() {
@@ -91,22 +91,38 @@ class PoseDetailMoreViewController: BaseViewController {
         self.navigationBar.layoutIfNeeded()
         
         // 1. 신고하기 버튼 탭
-        reportButton.rx.tap
-            .asDriver()
-            .drive(onNext: { [weak self] in
-                guard let self = self else { return }
-                let reportVC = ReportViewController(poseId: self.poseId)
-                let navigationVC = UINavigationController(rootViewController: reportVC)
-                navigationVC.modalPresentationStyle = .overFullScreen
-                navigationVC.modalTransitionStyle = .coverVertical
-                self.present(navigationVC, animated: true)
-            })
-            .disposed(by: disposeBag)
+//        reportButton.rx.tap
+//            .asDriver()
+//            .drive(onNext: { [weak self] in
+//                guard let self = self else { return }
+//                let reportVC = ReportViewController(poseId: self.poseId)
+//                let navigationVC = UINavigationController(rootViewController: reportVC)
+//                navigationVC.modalPresentationStyle = .overFullScreen
+//                navigationVC.modalTransitionStyle = .coverVertical
+//                self.present(navigationVC, animated: true)
+//            })
+//            .disposed(by: disposeBag)
+    }
+    
+    override func bindViewModel() {
+        let input = PoseDetailMoreViewModel.Input(
+            closeButtonTapEvent: closeButton.rx.tap.asObservable(),
+            reportButtonTapEvent: reportButton.rx.tap.flatMapLatest { Observable.just(self.poseId) }
+        )
+        
+        let output = viewModel?.transform(input: input, disposeBag: disposeBag)
+        configureOutput(output)
     }
     
     // MARK: - Objc Functions
     @objc
     func closeButtonTapped() {
         self.dismiss(animated: true)
+    }
+}
+
+private extension PoseDetailMoreViewController {
+    func configureOutput(_ output: PoseDetailMoreViewModel.Output?) {
+        
     }
 }
