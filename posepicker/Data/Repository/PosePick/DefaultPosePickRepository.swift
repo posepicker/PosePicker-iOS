1a185f00a131c492a2f8b31798fc5e6f2df2d6cd
Parkjju <rudwns3927@gmail.com>
2024-04-15 13:44:47 +0900

    feat: 포즈픽 레파지토리 UIImage 옵셔널 타입 수정 후 로직 개선

--- posepicker/Data/Repository/PosePick/DefaultPosePickRepository.swift
+++ posepicker/Data/Repository/PosePick/DefaultPosePickRepository.swift
@@ -18,40 +18,46 @@ final class DefaultPosePickRepository: PosePickRepository {
         self.networkService = networkService
     }
     
-    func fetchPoseImage(peopleCount: Int) -> Observable<UIImage> {
+    func fetchPoseImage(peopleCount: Int) -> Observable<UIImage?> {
         return networkService.requestSingle(.retrievePosePick(peopleCount: peopleCount))
             .asObservable()
-            .flatMapLatest { (posepick: Pose) -> Observable<Pose> in
-                return BehaviorRelay<Pose>(value: posepick).asObservable()
+            .flatMapLatest { (posepick: Pose) -> Observable<Pose?> in
+                if posepick.poseInfo.poseId == nil { return BehaviorRelay<Pose?>(value: nil).asObservable() }
+                return BehaviorRelay<Pose?>(value: posepick).asObservable()
             }
-            .map { $0.poseInfo.imageKey }
+            .map { $0?.poseInfo.imageKey }
             .withUnretained(self)
-            .flatMapLatest { (owner, imageURL) -> Observable<UIImage> in
+            .flatMapLatest { (owner, imageURL) -> Observable<UIImage?> in
                 return owner.cacheItem(for: imageURL)
             }
     }
     
-    private func cacheItem(for imageURL: String) -> Observable<UIImage> {
+    private func cacheItem(for imageURL: String?) -> Observable<UIImage?> {
         return Observable.create { observer in
-            ImageCache.default.retrieveImageInDiskCache(forKey: imageURL) { result in
-                switch result {
-                case .success(let value):
-                    if let image = value?.images?.first {
-                        observer.onNext(image)
-                    } else if let url = URL(string: imageURL) {
-                        KingfisherManager.shared.retrieveImage(with: url) { downloadResult in
-                            switch downloadResult {
-                            case .success(let downloaded):
-                                observer.onNext(downloaded.image)
-                            case .failure(let error):
-                                observer.onError(error)
+            if let imageURL = imageURL {
+                ImageCache.default.retrieveImageInDiskCache(forKey: imageURL) { result in
+                    switch result {
+                    case .success(let value):
+                        if let image = value?.images?.first {
+                            observer.onNext(image)
+                        } else if let url = URL(string: imageURL) {
+                            KingfisherManager.shared.retrieveImage(with: url) { downloadResult in
+                                switch downloadResult {
+                                case .success(let downloaded):
+                                    observer.onNext(downloaded.image)
+                                case .failure(let error):
+                                    observer.onError(error)
+                                }
                             }
                         }
+                    case .failure(let error):
+                        observer.onError(error)
                     }
-                case .failure(let error):
-                    observer.onError(error)
                 }
+            } else {
+                observer.onNext(nil)
             }
+            
             return Disposables.create {
                 
             }
