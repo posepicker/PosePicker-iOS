784504c8da410686219c82898b37ccc75de2f5bc
Parkjju <rudwns3927@gmail.com>
2024-04-17 09:16:17 +0900

    fix: 포즈톡 유스케이스 타입 프로토콜로 변경

--- posepicker/Domain/UseCase/PoseTalk/DefaultPoseTalkUseCase.swift
+++ posepicker/Domain/UseCase/PoseTalk/DefaultPoseTalkUseCase.swift
@@ -11,12 +11,12 @@ import RxSwift
 // 도메인 유스케이스 입장에서는 네트워크 요청이 어떻게 이루어지는지 모름
 // 데이터 타입도 모르는게 좋을 것 같다 -> 레포지토리 쪽에서 데이터 정제 후에 유스케이스에 넘겨주기
 final class DefaultPoseTalkUseCase: PoseTalkUseCase {
-    private let posetalkRepository: DefaultPoseTalkRepository
+    private let posetalkRepository: PoseTalkRepository
     private var disposeBag = DisposeBag()
     
     var poseWord = PublishSubject<String>()
     
-    init(posetalkRepository: DefaultPoseTalkRepository) {
+    init(posetalkRepository: PoseTalkRepository) {
         self.posetalkRepository = posetalkRepository
     }
     
