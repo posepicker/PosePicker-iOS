534e9a2f3681423dae783282a6508d7e1097ffa0
Parkjju <rudwns3927@gmail.com>
2024-04-17 10:15:51 +0900

    test: 포즈피드 유스케이스 테스트코드 작성 === 1. 페이지넘버 0 & 1일때 데이터 쌓이는지 여부 2. lastPage값 검증 3. 북마크 로직대로 동작하는지 테스트

--- PosePickerUseCaseTests/PoseFeedScene/PoseFeedUseCaseTests.swift
+++ PosePickerUseCaseTests/PoseFeedScene/PoseFeedUseCaseTests.swift
@@ -21,34 +21,47 @@ final class PoseFeedUseCaseTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        let imageDownloader: ImageDownloader = {
-            let downloader = ImageDownloader.default
-            let configuration = URLSessionConfiguration.default
-            configuration.protocolClasses = [MockURLProtocol.self]
-            downloader.sessionConfiguration = configuration
-            return downloader
-        }()
         
         self.posefeedRespository = MockPoseFeedRepository()
         self.posefeedUseCase = DefaultPoseFeedUseCase(
             posefeedRepository: MockPoseFeedRepository()
         )
         self.scheduler = .init(initialClock: 0)
-        
-        ImageCache.default.clearCache()
     }
     
     func test_컨텐츠_세팅_후_사이즈_잘_추출되는지() {
         let filteredContentSizesObserver = self.scheduler.createObserver(Int.self)
         let recommendedContentSizesObserver = self.scheduler.createObserver(Int.self)
         
-        self.posefeedUseCase
-            .fetchFeedContents(
-                peopleCount: "1인",
-                frameCount: "4컷",
-                filterTags: [],
-                pageNumber: 0
-            )
+        self.scheduler
+            .createColdObservable([
+                .next(0, ())
+            ])
+            .subscribe(onNext: { [weak self] in
+                self?.posefeedUseCase
+                    .fetchFeedContents(
+                        peopleCount: "1인",
+                        frameCount: "4컷",
+                        filterTags: [],
+                        pageNumber: 0
+                    )
+            })
+            .disposed(by: self.disposeBag)
+        
+        self.scheduler
+            .createColdObservable([
+                .next(1, ())
+            ])
+            .subscribe(onNext: { [weak self] in
+                self?.posefeedUseCase
+                    .fetchFeedContents(
+                        peopleCount: "1인",
+                        frameCount: "4컷",
+                        filterTags: [],
+                        pageNumber: 1
+                    )
+            })
+            .disposed(by: disposeBag)
         
         self.posefeedUseCase
             .filterSectionContentSizes
@@ -64,12 +77,64 @@ final class PoseFeedUseCaseTests: XCTestCase {
         
         scheduler.start()
         
+        // BehaviorRelay로 인해 초기값 빈 배열 한번 방출
+        // pageNumber 0일때 사이즈 전체 초기화를 위해 빈 배열 방출
         XCTAssertEqual(filteredContentSizesObserver.events, [
-            .next(0, 1)
+            .next(0, 0),
+            .next(0, 0),
+            .next(0, 1),
+            .next(1, 2)
         ])
         
         XCTAssertEqual(recommendedContentSizesObserver.events, [
-            .next(0, 1)
+            .next(0, 0),
+            .next(0, 0),
+            .next(0, 1),
+            .next(1, 2)
+        ])
+    }
+    
+    func test_라스트페이지_여부_체크() {
+        let isLastPageTestObserver = self.scheduler.createObserver(Bool.self)
+        
+        self.posefeedUseCase
+            .isLastPage
+            .subscribe(isLastPageTestObserver)
+            .disposed(by: disposeBag)
+        
+        self.scheduler.start()
+        
+        // 하드코딩 옵저버블 - 필터 데이터는 마지막 페이지 아닌데, 추천 데이터는 마지막 페이지일때 최종적으로 false 리턴
+        XCTAssertEqual(isLastPageTestObserver.events, [
+            .next(0, false)
+        ])
+    }
+    
+    func test_북마크_동작_테스트() {
+        let bookmarkTaskTestObserver = self.scheduler.createObserver(Bool.self)
+        
+        self.scheduler.createColdObservable([
+            .next(0, ())
+        ])
+        .subscribe(onNext: { [weak self] in
+            self?.posefeedUseCase
+                .bookmarkContent(poseId: 1, currentChecked: true)
+            
+            self?.posefeedUseCase
+                .bookmarkContent(poseId: 0, currentChecked: true)
+        })
+        .disposed(by: self.disposeBag)
+        
+        self.posefeedUseCase
+            .bookmarkTaskCompleted
+            .subscribe(bookmarkTaskTestObserver)
+            .disposed(by: disposeBag)
+        
+        self.scheduler.start()
+        
+        XCTAssertEqual(bookmarkTaskTestObserver.events, [
+            .next(0, true),
+            .next(0, false)
         ])
     }
     
@@ -79,6 +144,5 @@ final class PoseFeedUseCaseTests: XCTestCase {
         self.posefeedRespository = nil
         self.posefeedUseCase = nil
         self.scheduler = nil
-        ImageDownloader.default.sessionConfiguration = .default
     }
 }
