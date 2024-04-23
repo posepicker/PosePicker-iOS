1db5f2f19aed0d8378a0184645599c3e181eb9d9
Parkjju <rudwns3927@gmail.com>
2024-04-23 18:22:58 +0900

    feat: 북마크 컨텐츠 로딩 값 아웃풋 이벤트 방출

--- posepicker/Presentation/BookmarkScene/ViewModel/BookmarkViewModel.swift
+++ posepicker/Presentation/BookmarkScene/ViewModel/BookmarkViewModel.swift
@@ -27,6 +27,7 @@ final class BookmarkViewModel {
     struct Output {
         let bookmarkContents = BehaviorRelay<[BookmarkFeedCellViewModel]>(value: [])
         let bookmarkContentSizes = BehaviorRelay<[CGSize]>(value: [])
+        let isLoading = BehaviorRelay<Bool>(value: false)
     }
     
     func transform(input: Input, disposeBag: DisposeBag) -> Output{
@@ -34,6 +35,7 @@ final class BookmarkViewModel {
         
         input.viewDidLoadEvent
             .subscribe(onNext: { [weak self] in
+                output.isLoading.accept(true)
                 self?.bookmarkUseCase.fetchFeedContents(pageNumber: 0, pageSize: 8)
             })
             .disposed(by: disposeBag)
@@ -64,6 +66,13 @@ final class BookmarkViewModel {
                 self?.bookmarkUseCase.bookmarkContent(poseId: $0.0, currentChecked: $0.1)
                 guard let coordinator = self?.coordinator else { return }
                 coordinator.bookmarkBindingDelegate?.coordinatorBookmarkModified(childCoordinator: coordinator, poseId: $0.0)
+            })
+            .disposed(by: disposeBag)
+        
+        self.bookmarkUseCase
+            .contentLoaded
+            .subscribe(onNext: {
+                output.isLoading.accept(false)
             })
             .disposed(by: disposeBag)
         
