e126f061c354dc9f1b50f9a6d8cf3119a917e5ae
Parkjju <rudwns3927@gmail.com>
2024-05-08 16:30:16 +0900

    feat: 마이포즈 유스케이스 - 레파지토리 함수 호출 후 데이터 바인딩

--- posepicker/Domain/UseCase/MyPose/DefaultMyPoseUseCase.swift
+++ posepicker/Domain/UseCase/MyPose/DefaultMyPoseUseCase.swift
@@ -16,4 +16,17 @@ final class DefaultMyPoseUseCase: MyPoseUseCase {
     init(myPoseRepository: MyPoseRepository) {
         self.myPoseRepository = myPoseRepository
     }
+    
+    var uploadedPoseCount = PublishSubject<String>()
+    var savedPoseCount = PublishSubject<String>()
+    
+    func fetchPoseCount() {
+        myPoseRepository
+            .fetchPoseCount()
+            .subscribe(onNext: { [weak self] in
+                self?.uploadedPoseCount.onNext("등록 \($0.uploadCount)")
+                self?.savedPoseCount.onNext("저장 \($0.bookmarkCount)")
+            })
+            .disposed(by: disposeBag)
+    }
 }
