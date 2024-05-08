e3b604f8d6726a5e0b15ccafe8ee64609d6ed1b4
Parkjju <rudwns3927@gmail.com>
2024-05-08 13:42:28 +0900

    feat: 마이포즈 코디네이터 프로토콜 & 클래스 추가

---
+++ posepicker/Presentation/MyPoseScene/Coordinator/DefaultMyPoseCoordinator.swift
@@ -0,0 +1,12 @@
+//
+//  DefaultMyPoseCoordinator.swift
+//  posepicker
+//
+//  Created by 박경준 on 4/30/24.
+//
+
+import Foundation
+
+final class DefaultMyPoseCoordinator: MyPoseCoordinator {
+    
+}
---
+++ posepicker/Presentation/MyPoseScene/Coordinator/Protocol/MyPoseCoordinator.swift
@@ -0,0 +1,12 @@
+//
+//  MyPoseProtocol.swift
+//  posepicker
+//
+//  Created by 박경준 on 4/30/24.
+//
+
+import Foundation
+
+protocol MyPoseCoordinator {
+    
+}
