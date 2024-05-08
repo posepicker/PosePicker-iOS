3ed44513e74ee0f10940e2584dcd94dfe13c5d8a
Parkjju <rudwns3927@gmail.com>
2024-05-08 16:41:54 +0900

    feat: 마이포즈 뷰모델 카운트 값 요청 후 아웃풋 옵저버블 데이터 방출

--- posepicker/Presentation/MyPoseScene/ViewModel/MyPoseViewModel.swift
+++ posepicker/Presentation/MyPoseScene/ViewModel/MyPoseViewModel.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import RxSwift
+import RxRelay
 
 final class MyPoseViewModel {
     weak var coordinator: MyPoseCoordinator?
@@ -14,5 +16,41 @@ final class MyPoseViewModel {
     init(coordinator: MyPoseCoordinator?, myPoseUseCase: MyPoseUseCase) {
         self.coordinator = coordinator
         self.myPoseUseCase = myPoseUseCase
+    }
+    
+    struct Input {
+        let viewDidLoadEvent: Observable<Void>
+    }
+    
+    struct Output {
+        let uploadedCount = BehaviorRelay<String>(value: "등록 0")
+        let savedCount = BehaviorRelay<String>(value: "저장 0")
+    }
+    
+    func transform(input: Input, disposeBag: DisposeBag) -> Output {
+        let output = Output()
+        
+        input
+            .viewDidLoadEvent
+            .subscribe(onNext: { [weak self] in
+                self?.myPoseUseCase.fetchPoseCount()
+            })
+            .disposed(by: disposeBag)
+        
+        myPoseUseCase
+            .uploadedPoseCount
+            .subscribe(onNext: {
+                output.uploadedCount.accept($0)
+            })
+            .disposed(by: disposeBag)
+        
+        myPoseUseCase
+            .savedPoseCount
+            .subscribe(onNext: {
+                output.savedCount.accept($0)
+            })
+            .disposed(by: disposeBag)
+        
+        return output
     }
 }
