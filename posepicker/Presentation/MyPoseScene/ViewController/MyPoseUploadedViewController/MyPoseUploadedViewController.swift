99a389b59f326398c5f95e9da79be4f386201235
Parkjju <rudwns3927@gmail.com>
2024-05-09 16:00:43 +0900

    feat: 마이포즈 업로드 뷰 컨트롤러 클래스 추가

---
+++ posepicker/Presentation/MyPoseScene/ViewController/MyPoseUploadedViewController/MyPoseUploadedViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MyPoseUploadedViewController.swift
+//  posepicker
+//
+//  Created by 박경준 on 5/8/24.
+//
+
+import UIKit
+
+class MyPoseUploadedViewController: BaseViewController {
+
+    override func configUI() {
+        view.backgroundColor = .red
+    }
+}
