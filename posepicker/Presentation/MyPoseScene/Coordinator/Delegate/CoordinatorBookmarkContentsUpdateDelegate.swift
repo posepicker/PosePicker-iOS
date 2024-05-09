d500c8209fb33c985482f9a0e5d4eaa8c93f63a0
Parkjju <rudwns3927@gmail.com>
2024-05-09 15:36:21 +0900

    feat: 포즈피드 북마크 탭 이후 마이포즈 새로고침

---
+++ posepicker/Presentation/MyPoseScene/Coordinator/Delegate/CoordinatorBookmarkContentsUpdateDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  CoordinatorBookmarkContentsUpdateDelegate.swift
+//  posepicker
+//
+//  Created by 박경준 on 5/9/24.
+//
+
+import Foundation
+
+protocol CoordinatorBookmarkContentsUpdateDelegate: AnyObject {
+    func coordinatorBookmarkContentsUpdated(childCoordinator: Coordinator)
+}
