f050fe99e2b3c7ed4c4ecf59abbbd615918851f3
Parkjju <rudwns3927@gmail.com>
2024-05-09 16:01:27 +0900

    feat: 마이포즈 코디네이터 프로토콜 함수 명세 작성

--- posepicker/Presentation/MyPoseScene/Coordinator/Protocol/MyPoseCoordinator.swift
+++ posepicker/Presentation/MyPoseScene/Coordinator/Protocol/MyPoseCoordinator.swift
@@ -5,8 +5,15 @@
 //  Created by 박경준 on 4/30/24.
 //
 
-import Foundation
+import UIKit
 
 protocol MyPoseCoordinator: Coordinator {
+    var bookmarkBindingDelegate: CoordinatorBookmarkBindingDelegate? { get set }
+    func presentBookmarkDetail(viewModel: BookmarkFeedCellViewModel)
+    func presentClipboardCompleted(poseId: Int)
+    func moveToExternalApp(url: URL)
+    func dismissPoseDetail(tag: String)
     
+    func viewControllerBefore() -> UIViewController?
+    func viewControllerAfter() -> UIViewController?
 }
