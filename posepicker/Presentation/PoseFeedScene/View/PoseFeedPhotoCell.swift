98653a3d733cfa55e1e47875633bb4456ced95d3
Parkjju <rudwns3927@gmail.com>
2024-05-21 13:53:02 +0900

    style: 포즈피드 이미지 셀 dimmed30으로 수정

--- posepicker/Presentation/PoseFeedScene/View/PoseFeedPhotoCell.swift
+++ posepicker/Presentation/PoseFeedScene/View/PoseFeedPhotoCell.swift
@@ -25,7 +25,7 @@ class PoseFeedPhotoCell: BaseCollectionViewCell {
             $0.setImage(ImageLiteral.imgBookmarkOff24.withTintColor(.iconWhite, renderingMode: .alwaysTemplate), for: .normal)
             $0.layer.cornerRadius = 18
             $0.clipsToBounds = true
-            $0.backgroundColor = .dimmed70
+            $0.backgroundColor = .dimmed30
         }
     
     // MARK: - Properties
