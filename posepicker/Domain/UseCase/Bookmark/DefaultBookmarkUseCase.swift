c70aec116ea73e514cfe8c45b65d53e26e51f5bb
Parkjju <rudwns3927@gmail.com>
2024-05-21 15:36:08 +0900

    feat: 북마크 유스케이스 컨텐츠 전체 삭제 코드 추가

--- posepicker/Domain/UseCase/Bookmark/DefaultBookmarkUseCase.swift
+++ posepicker/Domain/UseCase/Bookmark/DefaultBookmarkUseCase.swift
@@ -63,6 +63,12 @@ final class DefaultBookmarkUseCase: BookmarkUseCase {
             .disposed(by: disposeBag)
     }
     
+    func removeAllContents() {
+        self.isLastPage.accept(false)
+        self.bookmarkContents.accept([])
+        self.contentSizes.accept([])
+    }
+    
     /// 필터링 & 추천 둘다 마지막 페이지면 무한스크롤 더 이상 호출할 필요 없음
     private func checkIsLastPage() {
         self.bookmarkRepository
