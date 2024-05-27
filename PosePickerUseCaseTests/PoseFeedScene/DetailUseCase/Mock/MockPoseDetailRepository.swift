7e1cda67748a703e9a69f3c17369978533288d27
Parkjju <rudwns3927@gmail.com>
2024-05-27 17:58:20 +0900

    test: 목업 포즈 상세보기 레파지토리 HTTPS prefix 케이스 추가

--- PosePickerUseCaseTests/PoseFeedScene/DetailUseCase/Mock/MockPoseDetailRepository.swift
+++ PosePickerUseCaseTests/PoseFeedScene/DetailUseCase/Mock/MockPoseDetailRepository.swift
@@ -5,7 +5,7 @@
 //  Created by 박경준 on 4/18/24.
 //
 
-import Foundation
+import UIKit
 import RxSwift
 
 @testable import posepicker
@@ -19,7 +19,7 @@ final class MockPoseDetailRepository: PoseDetailRepository {
             peopleCount: 4,
             poseId: 10,
             source: "@gangjuninggg",
-            sourceUrl: "www.instagram.URL",
+            sourceUrl: "www.instagram.com",
             tagAttributes: "친구,자연스러움,가족,재미",
             updatedAt: "2024-04-18T05:37:55.974Z",
             bookmarkCheck: true,
@@ -49,14 +49,45 @@ final class MockPoseDetailRepository: PoseDetailRepository {
         )
     )
     
-    var isNil: Bool
+    private var hasPrefixPose: Pose = .init(
+        poseInfo: .init(
+            createdAt: "2024-04-18T05:37:55.974Z",
+            frameCount: 8,
+            imageKey: "www.S3.url",
+            peopleCount: 4,
+            poseId: 10,
+            source: "@gangjuninggg",
+            sourceUrl: "https://www.instagram.com",
+            tagAttributes: "친구,자연스러움,가족,재미",
+            updatedAt: "2024-04-18T05:37:55.974Z",
+            bookmarkCheck: true,
+            poseUploadUser: .init(
+                uid: 0,
+                nickname: "gangjuninggg",
+                email: "rudwns3927@gmail.com",
+                loginType: "kakao",
+                iosId: "10"
+            )
+        )
+    )
     
-    init(isNil: Bool) {
+    var isNil: Bool
+    var hasPrefix: Bool
+    
+    init(isNil: Bool, hasPrefix: Bool = false) {
         self.isNil = isNil
+        self.hasPrefix = hasPrefix
     }
     
     func fetchPoseInfo(poseId: Int) -> Observable<Pose> {
+        if self.hasPrefix {
+            return Observable.just(self.hasPrefixPose)
+        }
         return Observable.just(self.isNil ? self.nilPoseInfo : self.poseInfo)
+    }
+    
+    func cacheItem(for imageURL: String?) -> Observable<UIImage?> {
+        return Observable.just(nil)
     }
     
     /// 알 수 없는 이유로  북마크 대상 포즈 아이디가 요청 포즈 아이디와 매칭되지 않는 오류 -> 체크 실패 false 리턴
