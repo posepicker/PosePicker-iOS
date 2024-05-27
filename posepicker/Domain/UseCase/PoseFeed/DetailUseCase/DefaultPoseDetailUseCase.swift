43fc0894102e0a735ef1d899e4034043a3c2b27a
Parkjju <rudwns3927@gmail.com>
2024-05-27 17:55:19 +0900

    feat: 포즈 상세보기 유스케이스 cacheItem 함수 삭제

--- posepicker/Domain/UseCase/PoseFeed/DetailUseCase/DefaultPoseDetailUseCase.swift
+++ posepicker/Domain/UseCase/PoseFeed/DetailUseCase/DefaultPoseDetailUseCase.swift
@@ -34,7 +34,8 @@ final class DefaultPoseDetailUseCase: PoseDetailUseCase {
         self.poseDetailRepository
             .fetchPoseInfo(poseId: self.poseId)
             .subscribe(onNext: { [weak self] pose in
-                var tags = self?.tagItems.value ?? []
+                guard let self = self else { return }
+                var tags = self.tagItems.value
                 if let peopleCountTag = PeopleCountTags.getTagTitleFromIndex(index: pose.poseInfo.peopleCount ?? 0),
                    peopleCountTag != "전체" {
                     tags += [peopleCountTag]
@@ -48,27 +49,27 @@ final class DefaultPoseDetailUseCase: PoseDetailUseCase {
                 let tagAttributes = pose.poseInfo.tagAttributes ?? ""
                 tags += tagAttributes.split(separator: ",").map { String($0) }
                 
-                self?.tagItems.accept(tags)
+                self.tagItems.accept(tags)
                 
-                self?.imageURL.accept(pose.poseInfo.imageKey)
+                self.imageURL.accept(pose.poseInfo.imageKey)
                 
                 let sourceURL = pose.poseInfo.sourceUrl ?? ""
                 
                 if String(sourceURL.prefix(5)) == "https" {
-                    self?.sourceUrl.accept(sourceURL)
+                    self.sourceUrl.accept(sourceURL)
                 } else {
-                    self?.sourceUrl.accept("https://" + sourceURL)
+                    self.sourceUrl.accept("https://" + sourceURL)
                 }
                 
                 let source = pose.poseInfo.source ?? "링크바로가기"
-                self?.source.accept(source)
+                self.source.accept(source)
             })
             .disposed(by: self.disposeBag)
         
         imageURL
             .withUnretained(self)
             .flatMapLatest { (owner, url) in
-                owner.cacheItem(for: url)
+                owner.poseDetailRepository.cacheItem(for: url)
             }
             .map { $0?.resize(newWidth: UIScreen.main.bounds.width )}
             .subscribe(onNext: { [weak self] in
@@ -84,37 +85,5 @@ final class DefaultPoseDetailUseCase: PoseDetailUseCase {
                 self?.bookmarkTaskCompleted.onNext($0)
             })
             .disposed(by: disposeBag)
-    }
-    
-    private func cacheItem(for imageURL: String?) -> Observable<UIImage?> {
-        return Observable.create { observer in
-            if let imageURL = imageURL {
-                ImageCache.default.retrieveImageInDiskCache(forKey: imageURL) { result in
-                    switch result {
-                    case .success(let value):
-                        if let image = value?.images?.first {
-                            observer.onNext(image)
-                        } else if let url = URL(string: imageURL) {
-                            KingfisherManager.shared.retrieveImage(with: url) { downloadResult in
-                                switch downloadResult {
-                                case .success(let downloaded):
-                                    observer.onNext(downloaded.image)
-                                case .failure(let error):
-                                    observer.onError(error)
-                                }
-                            }
-                        }
-                    case .failure(let error):
-                        observer.onError(error)
-                    }
-                }
-            } else {
-                observer.onNext(nil)
-            }
-            
-            return Disposables.create {
-                
-            }
-        }
     }
 }
