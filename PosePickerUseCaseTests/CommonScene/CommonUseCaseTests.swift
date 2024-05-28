4d127f4e9e276aa3624ceb1ee0222135f0562c25
Parkjju <rudwns3927@gmail.com>
2024-05-28 13:40:22 +0900

    test: 카카오 로그인 / 애플 로그인 / 로그아웃 테스트 코드 작성

--- PosePickerUseCaseTests/CommonScene/CommonUseCaseTests.swift
+++ PosePickerUseCaseTests/CommonScene/CommonUseCaseTests.swift
@@ -26,6 +26,75 @@ final class CommonSceneUseCaseTests: XCTestCase {
         self.scheduler = TestScheduler(initialClock: 0)
     }
     
+//    func logout(with: LoginPopUpView.SocialLogin)
+//    func revoke(with: LoginPopUpView.SocialLogin, reason: String)
+    
+    func test_카카오_로그인_완료_테스트() {
+        let expectation = XCTestExpectation(description: "login completed test")
+        
+        self.scheduler.createHotObservable([
+            .next(0, ())
+        ])
+        .subscribe(onNext: { [weak self] in
+            self?.commonUseCase.loginWithKakao()
+        })
+        .disposed(by: self.disposeBag)
+        
+        self.commonUseCase.loginCompleted
+            .subscribe(onNext: {
+                expectation.fulfill()
+            })
+            .disposed(by: self.disposeBag)
+        
+        self.scheduler.start()
+        
+        wait(for: [expectation], timeout: 5)
+    }
+    
+    func test_애플_로그인_완료_테스트() {
+        let expectation = XCTestExpectation(description: "login completed test")
+        
+        self.scheduler.createHotObservable([
+            .next(0, ())
+        ])
+        .subscribe(onNext: { [weak self] in
+            self?.commonUseCase.loginWithApple(idToken: "apple id token")
+        })
+        .disposed(by: self.disposeBag)
+        
+        self.commonUseCase.loginCompleted
+            .subscribe(onNext: {
+                expectation.fulfill()
+            })
+            .disposed(by: self.disposeBag)
+        
+        self.scheduler.start()
+        
+        wait(for: [expectation], timeout: 5)
+    }
+    
+    func test_로그아웃_테스트() {
+        let expectation = XCTestExpectation(description: "logout completed test")
+        
+        self.scheduler.createHotObservable([
+            .next(0, ())
+        ])
+        .subscribe(onNext: { [weak self] in
+            self?.commonUseCase.logout(with: .kakao)
+        })
+        .disposed(by: self.disposeBag)
+        
+        self.commonUseCase.logoutCompleted
+            .subscribe(onNext: {
+                expectation.fulfill()
+            })
+            .disposed(by: self.disposeBag)
+        
+        self.scheduler.start()
+        
+        wait(for: [expectation], timeout: 5)
+    }
+    
     override func tearDown() {
         super.tearDown()
         self.userRepository = nil
