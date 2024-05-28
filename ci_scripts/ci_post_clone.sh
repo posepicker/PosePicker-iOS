36153748cd029af0bf485bb60225758ce56b199f
Parkjju <rudwns3927@gmail.com>
2024-05-28 16:33:44 +0900

    fix: filename 수정

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -9,10 +9,9 @@
 FOLDER_PATH="/Volumes/workspace/repository/posepicker/Configs"
 
 # PARTS 배열의 두 번째 요소가 "Scheme Name"에 해당
-IFS='-' read -ra PARTS <<< "$CI_XCODE_SCHEME"
 
 # *.xconfig 파일 이름
-CONFIG_FILENAME="${PARTS[1]}.xcconfig"
+CONFIG_FILENAME="posepicker.xcconfig"
 
 # *.xconfig 파일의 전체 경로 계산
 CONFIG_FILE_PATH="$FOLDER_PATH/$CONFIG_FILENAME"
@@ -24,4 +23,4 @@ echo "SMARTLOOK_PROJECT_KEY = $SMARTLOOK_PROJECT_KEY" >> "$CONFIG_FILE_PATH"
 # 생성된 *.xconfig 파일 내용 출력
 cat "$CONFIG_FILE_PATH"
 
-echo "${PARTS[1]}.xcconfig 파일이 성공적으로 생성되었고, 환경변수 값이 확인되었습니다.
+echo "posepicker.xcconfig 파일이 성공적으로 생성되었고, 환경변수 값이 확인되었습니다.
