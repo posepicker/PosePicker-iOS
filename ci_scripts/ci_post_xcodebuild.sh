b98f83ba8aa2d3ad7bcba2ac77039afed2dbc360
Parkjju <rudwns3927@gmail.com>
2024-05-29 14:19:04 +0900

    feat: 빌드 단계에서만 메시지 전송하도록 스크립트 수정

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -10,30 +10,36 @@
 WEBHOOK_URL="https://discord.com/api/webhooks/1244900222774280232/K6VVRQ36QG6KzkXhCvGMvPYsXW-2rJ06hKuAG-aa_H9wEyKwJLmmXiDZ9dJ4FTumMzxd"
 
 # 환경 변수 설정
-PROJECT_NAME=${CI_PROJECT:-"Unknown Project"}
+PROJECT_NAME=${CI_XCODE_PROJECT:-"Unknown Project"}
 BRANCH_NAME=${CI_BRANCH:-"Unknown Branch"}
 COMMIT_HASH=${CI_COMMIT:-"Unknown Commit"}
 BUILD_URL=${CI_BUILD_URL:-"https://ci.example.com"}
 BUILD_NUMBER=${CI_BUILD_NUMBER:-"Unknown Build Number"}
 WORKFLOW_NAME=${CI_WORKFLOW:-"Unknown Workflow"}
 
-# 빌드 종료 코드 확인
-EXIT_CODE=${CI_XCODEBUILD_EXIT_CODE}
+# 빌드 단계인지 확인
+if [ "$CI_XCODEBUILD_ACTION" = "build" ]; then
 
-# 빌드 상태 확인 및 메시지 설정
-if [ "$EXIT_CODE" -eq 0 ]; then
-    MESSAGE="**Build Succeeded!** 🚀\n\n"
+    # 빌드 종료 코드 확인
+    EXIT_CODE=${CI_XCODEBUILD_EXIT_CODE}
+    
+    # 빌드 상태 확인 및 메시지 설정
+    if [ "$EXIT_CODE" -eq 0 ]; then
+        MESSAGE="**Build Succeeded!** 🚀\n\n"
+    else
+        MESSAGE="**Build Failed!** ❌\n\n"
+    fi
+
+    # 메시지 내용 설정
+    MESSAGE+="**Project**: \`$PROJECT_NAME\`\n"
+    MESSAGE+="**Branch**: \`$BRANCH_NAME\`\n"
+    MESSAGE+="**Commit**: [\`$COMMIT_HASH\`](https://github.com/posepicker/PosePicker-iOS/commit/$COMMIT_HASH)\n"
+    MESSAGE+="**Build Number**: \`$BUILD_NUMBER\`\n"
+    MESSAGE+="**Workflow**: \`$WORKFLOW_NAME\`\n"
+    MESSAGE+="\n[View Build]($BUILD_URL)\n"
+
+    # Discord로 메시지 전송
+    curl -H "Content-Type: application/json" -X POST -d "{\"content\": \"$MESSAGE\"}" $WEBHOOK_URL
 else
-    MESSAGE="**Build Failed!** ❌\n\n"
+    echo "Current action is not a build action. No message will be sent."
 fi
-
-# 메시지 내용 설정
-MESSAGE+="**Project**: \`$PROJECT_NAME\`\n"
-MESSAGE+="**Branch**: \`$BRANCH_NAME\`\n"
-MESSAGE+="**Commit**: [\`$COMMIT_HASH\`](https://github.com/posepicker/PosePicker-iOS/commit/$COMMIT_HASH)\n"
-MESSAGE+="**Build Number**: \`$BUILD_NUMBER\`\n"
-MESSAGE+="**Workflow**: \`$WORKFLOW_NAME\`\n"
-MESSAGE+="\n[View Build]($BUILD_URL)\n"
-
-# Discord로 메시지 전송
-curl -H "Content-Type: application/json" -X POST -d "{\"content\": \"$MESSAGE\"}" $WEBHOOK_URL
