9eb5d15c6c14580e30e8195dee56a8205001e671
Parkjju <rudwns3927@gmail.com>
2024-05-29 17:51:49 +0900

    feat: 테스트 디바이스 & OS 버전 메시지 삽입

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -4,8 +4,7 @@
 #  posepicker
 #
 #  Created by 박경준 on 5/28/24.
-#  
-#!/bin/sh
+#
 # Discord Webhook URL
 WEBHOOK_URL="https://discord.com/api/webhooks/1244900222774280232/K6VVRQ36QG6KzkXhCvGMvPYsXW-2rJ06hKuAG-aa_H9wEyKwJLmmXiDZ9dJ4FTumMzxd"
 
@@ -16,6 +15,8 @@ COMMIT_HASH=${CI_COMMIT:-"Unknown Commit"}
 BUILD_URL=${CI_BUILD_URL:-"https://ci.example.com"}
 BUILD_NUMBER=${CI_BUILD_NUMBER:-"Unknown Build Number"}
 WORKFLOW_NAME=${CI_WORKFLOW:-"Unknown Workflow"}
+DEVICE_NAME=${CI_TEST_DESTINATION_DEVICE_TYPE:-"Unknown Device"}
+DEVICE_OS_VERSION=${CI_TEST_DESTINATION_RUNTIME:-"Unknown Version"}
 
 # 빌드 단계인지 확인
 if [ "$CI_XCODEBUILD_ACTION" = "build" ]; then
@@ -51,6 +52,8 @@ elif [ "$CI_XCODEBUILD_ACTION" = "test-without-building" ]; then
     # 메시지 내용 설정
     MESSAGE+="**Project**: \`$PROJECT_NAME\`\n"
     MESSAGE+="**Branch**: \`$BRANCH_NAME\`\n"
+    MESSAGE+="**Device**: \`$DEVICE_NAME\`\n"
+    MESSAGE+="**OS Version**: \`$DEVICE_OS_VERSION\`\n"
     MESSAGE+="**Commit**: [\`$COMMIT_HASH\`](https://github.com/posepicker/PosePicker-iOS/commit/$COMMIT_HASH)\n"
     MESSAGE+="**Build Number**: \`$BUILD_NUMBER\`\n"
     MESSAGE+="**Workflow**: \`$WORKFLOW_NAME\`\n"
