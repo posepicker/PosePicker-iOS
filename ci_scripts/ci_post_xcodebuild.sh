d4cc2b3ebe8e29427d4a1d9acf156ec8b73ea5cd
Parkjju <rudwns3927@gmail.com>
2024-05-29 14:57:27 +0900

    feat: 테스트 phase 웹훅 스크립트 메시지 작성

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -10,7 +10,7 @@
 WEBHOOK_URL="https://discord.com/api/webhooks/1244900222774280232/K6VVRQ36QG6KzkXhCvGMvPYsXW-2rJ06hKuAG-aa_H9wEyKwJLmmXiDZ9dJ4FTumMzxd"
 
 # 환경 변수 설정
-PROJECT_NAME=${CI_XCODE_PROJECT:-"Unknown Project"}
+PROJECT_NAME="PosePicker"
 BRANCH_NAME=${CI_BRANCH:-"Unknown Branch"}
 COMMIT_HASH=${CI_COMMIT:-"Unknown Commit"}
 BUILD_URL=${CI_BUILD_URL:-"https://ci.example.com"}
@@ -39,6 +39,23 @@ if [ "$CI_XCODEBUILD_ACTION" = "build" ]; then
     MESSAGE+="\n[View Build]($BUILD_URL)\n"
 
     # Discord로 메시지 전송
+    curl -H "Content-Type: application/json" -X POST -d "{\"content\": \"$MESSAGE\"}" $WEBHOOK_URL
+elif [ "$CI_XCODEBUILD_ACTION" = "test" ];
+    # 테스트 상태 확인 및 메시지 설정
+    if [ "$EXIT_CODE" -eq 0 ]; then
+        MESSAGE="**Tests Passed!** ✅\n\n"
+    else
+        MESSAGE="**Tests Failed!** ❌\n\n"
+    fi
+
+    # 메시지 내용 설정
+    MESSAGE+="**Project**: \`$PROJECT_NAME\`\n"
+    MESSAGE+="**Branch**: \`$BRANCH_NAME\`\n"
+    MESSAGE+="**Commit**: [\`$COMMIT_HASH\`](https://github.com/posepicker/PosePicker-iOS/commit/$COMMIT_HASH)\n"
+    MESSAGE+="**Build Number**: \`$BUILD_NUMBER\`\n"
+    MESSAGE+="**Workflow**: \`$WORKFLOW_NAME\`\n"
+    MESSAGE+="\n[View Build]($BUILD_URL)\n"
+    
     curl -H "Content-Type: application/json" -X POST -d "{\"content\": \"$MESSAGE\"}" $WEBHOOK_URL
 else
     echo "Current action is not a build action. No message will be sent."
