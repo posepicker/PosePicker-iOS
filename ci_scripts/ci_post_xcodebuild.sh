35a7c76e02bd361c62d3469812820ee0cfa9ece4
Parkjju <rudwns3927@gmail.com>
2024-05-29 14:02:19 +0900

    feat: 웹훅 메시지 정의

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -5,19 +5,35 @@
 #
 #  Created by 박경준 on 5/28/24.
 #  
-
+#!/bin/sh
 # Discord Webhook URL
 WEBHOOK_URL="https://discord.com/api/webhooks/1244900222774280232/K6VVRQ36QG6KzkXhCvGMvPYsXW-2rJ06hKuAG-aa_H9wEyKwJLmmXiDZ9dJ4FTumMzxd"
 
-# 메시지 내용 설정
+# 환경 변수 설정
+PROJECT_NAME=${CI_PROJECT:-"Unknown Project"}
+BRANCH_NAME=${CI_BRANCH:-"Unknown Branch"}
+COMMIT_HASH=${CI_COMMIT:-"Unknown Commit"}
+BUILD_URL=${CI_BUILD_URL:-"https://ci.example.com"}
+BUILD_NUMBER=${CI_BUILD_NUMBER:-"Unknown Build Number"}
+WORKFLOW_NAME=${CI_WORKFLOW:-"Unknown Workflow"}
+
+# 빌드 종료 코드 확인
+EXIT_CODE=${CI_XCODEBUILD_EXIT_CODE}
+
 # 빌드 상태 확인 및 메시지 설정
-if [ "$CI_BUILD_STATUS" = "success" ]; then
-    MESSAGE="**Build succeeded!**\n- Project: \`PosePicker\`\n- Branch: \`$CI_BRANCH\`\n- Commit: \`$CI_COMMIT\`\n\n[View Build]($CI_BUILD_URL)"
-elif [ "$CI_BUILD_STATUS" = "failure" ]; then
-    MESSAGE="**Build failed!**\n- Project: \`PosePicker\`\n- Branch: \`$CI_BRANCH\`\n- Commit: \`$CI_COMMIT\`\n\nPlease check the [build logs]($CI_BUILD_URL) for more details."
+if [ "$EXIT_CODE" -eq 0 ]; then
+    MESSAGE="**Build Succeeded!** 🚀\n\n"
 else
-    MESSAGE="**Build status unknown.**"
+    MESSAGE="**Build Failed!** ❌\n\n"
 fi
+
+# 메시지 내용 설정
+MESSAGE+="**Project**: \`$PROJECT_NAME\`\n"
+MESSAGE+="**Branch**: \`$BRANCH_NAME\`\n"
+MESSAGE+="**Commit**: [\`$COMMIT_HASH\`](https://github.com/posepicker/PosePicker-iOS/commit/$COMMIT_HASH)\n"
+MESSAGE+="**Build Number**: \`$BUILD_NUMBER\`\n"
+MESSAGE+="**Workflow**: \`$WORKFLOW_NAME\`\n"
+MESSAGE+="\n[View Build]($BUILD_URL)\n"
 
 # Discord로 메시지 전송
 curl -H "Content-Type: application/json" -X POST -d "{\"content\": \"$MESSAGE\"}" $WEBHOOK_URL
