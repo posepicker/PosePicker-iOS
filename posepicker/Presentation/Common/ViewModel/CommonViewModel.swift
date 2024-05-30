9804fa04f688ae80f01f070d5abb9b2dc5e1ddce
Parkjju <rudwns3927@gmail.com>
2024-05-30 17:15:57 +0900

    feat: Common 뷰모델 loginWithApple 함수 호출 파라미터 수정

--- posepicker/Presentation/Common/ViewModel/CommonViewModel.swift
+++ posepicker/Presentation/Common/ViewModel/CommonViewModel.swift
@@ -66,8 +66,7 @@ final class CommonViewModel {
             .subscribe(onNext: { [weak self] in
                 switch $0 {
                 case .apple:
-                    guard let idToken = try? KeychainManager.shared.retrieveItem(ofClass: .password, key: K.Parameters.idToken) else { return }
-                    self?.commonUseCase.loginWithApple(idToken: idToken)
+                    self?.commonUseCase.loginWithApple()
                 case .kakao:
                     self?.commonUseCase.loginWithKakao()
                 case .none:
@@ -87,8 +86,7 @@ final class CommonViewModel {
             .subscribe(onNext: { [weak self] in
                 switch $0 {
                 case .apple:
-                    guard let idToken = try? KeychainManager.shared.retrieveItem(ofClass: .password, key: K.Parameters.idToken) else { return }
-                    self?.commonUseCase.loginWithApple(idToken: idToken)
+                    self?.commonUseCase.loginWithApple()
                 case .kakao:
                     self?.commonUseCase.loginWithKakao()
                 default:
