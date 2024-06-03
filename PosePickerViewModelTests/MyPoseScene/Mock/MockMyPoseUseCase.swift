72be5fee473606b9e0e809378c68a33174c67084
Parkjju <rudwns3927@gmail.com>
2024-06-03 16:32:09 +0900

    feat: 포즈 업로드 유스케이스 목업 함수 추가

--- PosePickerViewModelTests/MyPoseScene/Mock/MockMyPoseUseCase.swift
+++ PosePickerViewModelTests/MyPoseScene/Mock/MockMyPoseUseCase.swift
@@ -19,15 +19,45 @@ final class MockMyPoseUseCase: MyPoseUseCase {
     var bookmarkTaskCompleted = PublishSubject<Bool>()
     
     func fetchFeedContents(pageNumber: Int, pageSize: Int) {
+        contentLoaded.onNext(())
+        isLastPage.accept(false)
+        let value = generateMockupData()
         
+        if pageNumber == 0 {
+            self.uploadedContents.accept(value)
+            
+            self.contentSizes.accept(
+                .init(
+                    repeating: CGSize(width: 10, height: 10),
+                    count: 5
+                )
+            )
+        }
+        else {
+            self.contentSizes.accept(
+                self.contentSizes.value + .init(
+                repeating: CGSize(width: 10, height: 10),
+                count: 5
+            ))
+            
+            var contents = self.uploadedContents.value
+            contents += value
+            self.uploadedContents.accept(contents)
+        }
     }
     
     func removeAllContents() {
-        
+        self.uploadedContents.accept([])
+        self.contentSizes.accept([])
     }
     
     func bookmarkContent(poseId: Int, currentChecked: Bool) {
-        
+        if let item = uploadedContents.value.first(where: { $0.poseId.value == poseId }) {
+            item.bookmarkCheck.accept(!currentChecked)
+            bookmarkTaskCompleted.onNext(true)
+        } else {
+            bookmarkTaskCompleted.onNext(false)
+        }
     }
     
     var uploadedPoseCount = PublishSubject<String>()
@@ -39,5 +69,13 @@ final class MockMyPoseUseCase: MyPoseUseCase {
         savedPoseCount.onNext("저장 10")
     }
     
-    
+    private func generateMockupData() -> [BookmarkFeedCellViewModel] {
+        return [
+            .init(image: ImageLiteral.imgInfo24, poseId: 0, bookmarkCheck: false),
+            .init(image: ImageLiteral.imgInfo24, poseId: 1, bookmarkCheck: false),
+            .init(image: ImageLiteral.imgInfo24, poseId: 2, bookmarkCheck: false),
+            .init(image: ImageLiteral.imgInfo24, poseId: 3, bookmarkCheck: false),
+            .init(image: ImageLiteral.imgInfo24, poseId: 4, bookmarkCheck: true),
+        ]
+    }
 }
