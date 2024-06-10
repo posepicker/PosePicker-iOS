8295bfbeae41d9195f492857363427582f7abe2f
Parkjju <rudwns3927@gmail.com>
2024-06-10 13:07:56 +0900

    feat: Common 뷰모델 테스트코드 - 페이지뷰 코디네이터 삽입 / 로그인 분기처리 후 마이포즈 접근 테스트코드 작성

--- PosePickerViewModelTests/Common/CommonViewModelTest.swift
+++ PosePickerViewModelTests/Common/CommonViewModelTest.swift
@@ -24,7 +24,11 @@ final class CommonViewTest: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        self.mockPageViewCoordinator = MockPageViewCoordinator(UINavigationController(rootViewController: UIViewController()))
+        self.mockPageViewCoordinator = MockPageViewCoordinator(
+            UINavigationController(
+                rootViewController: UIViewController()
+            )
+        )
         self.mockPageViewCoordinator.start()
         self.commonUseCase = MockCommonUseCase()
         self.viewModel = CommonViewModel(
@@ -41,19 +45,32 @@ final class CommonViewTest: XCTestCase {
         // MARK: - 뷰 컨트롤러의 페이지뷰 데이터소스에 대한 접근 권한을 테스트코드에서 얻어올 수 없어서 작성 한계
          let pageviewTransitionDelegateEvent = self.scheduler.createHotObservable([
              .next(1, ()),
-             .next(2, ()),
+             .next(2, ())
          ])
-         let mypageButtonTapped: TestableObservable<Void> = self.scheduler.createHotObservable([])
+        
+         let mypageButtonTapped: TestableObservable<Void> = self.scheduler.createColdObservable([
+            .next(0, ())
+         ])
+        
          let currentPage = self.scheduler.createHotObservable([
              .next(1, 1)
          ])
+        
+        let bookmarkButtonTapObservable = self.scheduler.createColdObservable([
+            .next(2, ()),
+            .next(3, ()),
+            .next(4, ())
+        ])
+        
         
          self.input = CommonViewModel.Input(
              pageviewTransitionDelegateEvent: pageviewTransitionDelegateEvent.asObservable(),
              myPageButtonTapEvent: mypageButtonTapped.asObservable(),
              currentPage: currentPage.asObservable(),
-             bookmarkButtonTapEvent: .empty(),
-             removeMyPoseContentsEvent: .empty()
+             bookmarkButtonTapEvent: bookmarkButtonTapObservable.asObservable(),
+             removeMyPoseContentsEvent: self.scheduler.createColdObservable([
+                .next(0, ())
+             ]).asObservable()
          )
         
          self.output = self.viewModel.transform(
@@ -73,6 +90,77 @@ final class CommonViewTest: XCTestCase {
              .next(1, 0),
              .next(2, 1)
          ])
+    }
+    
+    func test_로그인_되지_않은_상태에서_마이포즈_접근_로직_테스트() {
+        // MARK: - Observers
+        let pageTransitionObservers = self.scheduler.createObserver(Int.self)
+        
+        // MARK: - Test Config
+        self.scheduler.createColdObservable([
+            .next(0, ())
+        ])
+        .subscribe(onNext: {
+            UserDefaults.standard.setValue(false, forKey: K.SocialLogin.isLoggedIn)
+        })
+        .disposed(by: self.disposeBag)
+        
+        self.scheduler.createColdObservable([
+            .next(4, ())
+        ])
+        .subscribe(onNext: {
+            UserDefaults.standard.setValue(true, forKey: K.SocialLogin.isLoggedIn)
+        })
+        .disposed(by: self.disposeBag)
+        
+        /// 로그인 완료 이후 세팅되어 있는 페이지값 출력
+        self.scheduler.createColdObservable([
+            .next(6, ())
+        ])
+        .subscribe(onNext: { [weak self] in
+            guard let self = self,
+                  let coordinator = self.viewModel.coordinator else { return }
+            pageTransitionObservers.onNext(coordinator.currentPage().pageOrderNumber())
+        })
+        .disposed(by: self.disposeBag)
+        
+        // MARK: - Observables
+        let currentPage = self.scheduler.createHotObservable([
+            .next(1, 3),     // 로그인 하지 않은 채로 마이포즈 접근 시도
+            .next(1, 2),
+            .next(2, 3),     // 애플로그인, 카카오 로그인, none switch-case 검증
+            .next(3, 3),     // 로그인 완료 이후 시나리오 테스트
+            .next(5, 3)
+        ])
+        
+        // MARK: - Input & Output
+        self.input = CommonViewModel.Input(
+            pageviewTransitionDelegateEvent: .empty(),
+            myPageButtonTapEvent: .empty(),
+            currentPage: currentPage.asObservable(),
+            bookmarkButtonTapEvent: .empty(),
+            removeMyPoseContentsEvent: .empty()
+        )
+       
+        self.output = self.viewModel.transform(from: self.input, disposeBag: self.disposeBag)
+        
+        self.output
+            .pageTransitionEvent
+            .subscribe(pageTransitionObservers)
+            .disposed(by: self.disposeBag)
+        
+        _ = self.viewModel.viewControllerAfter()
+        _ = self.viewModel.viewControllerBefore()
+        
+        self.scheduler.start()
+        
+        XCTAssertEqual(pageTransitionObservers.events, [
+            .next(1, 0),
+            .next(2, 0),
+            .next(3, 0),
+            .next(6, 3)
+        ])
+        
     }
 
     override func tearDown() {
