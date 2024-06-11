db3b551921912b0926be610e5c8ef81653eab036
Parkjju <rudwns3927@gmail.com>
2024-06-11 10:35:47 +0900

    test: 이미지 출처 뷰모델 테스트 코드 - 에러 테스트 포함

--- PosePickerViewModelTests/PoseUploadScene/PageViews/PoseUploadImageSourceViewModelTests.swift
+++ PosePickerViewModelTests/PoseUploadScene/PageViews/PoseUploadImageSourceViewModelTests.swift
@@ -6,30 +6,80 @@
 //
 
 import XCTest
+import RxTest
+import RxSwift
+
+@testable import posepicker
 
 final class PoseUploadImageSourceViewModelTests: XCTestCase {
+    
+    private var viewModel: PoseUploadImageSourceViewModel!
+    private var disposeBag: DisposeBag!
+    private var scheduler: TestScheduler!
+    private var poseuploadUsecase: PoseUploadUseCase!
+    private var poseUploadCoordinator: PoseUploadCoordinator!
+    private var input: PoseUploadImageSourceViewModel.Input!
+    private var output: PoseUploadImageSourceViewModel.Output!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        self.disposeBag = DisposeBag()
+        self.scheduler = .init(initialClock: 0)
+        self.poseUploadCoordinator = MockPoseUploadCoordinator(
+            UINavigationController(
+                rootViewController: UIViewController()
+            )
+        )
+        self.poseuploadUsecase = MockPoseUploadUseCase()
+        self.viewModel = PoseUploadImageSourceViewModel(
+            coordinator: self.poseUploadCoordinator,
+            poseUploadUseCase: self.poseuploadUsecase
+        )
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func test_포즈업로드_테스트() {
+        let loadingObservable = self.scheduler.createObserver(Bool.self)
+        let poseObservable = self.scheduler.createObserver(Pose.self)
+        
+        self.input = PoseUploadImageSourceViewModel.Input(
+            sourceURL: self.scheduler.createColdObservable([
+                .next(0, "https://인스타URL.com")
+            ]).asObservable(),
+            submitButtonTapEvent: self.scheduler.createColdObservable([
+                .next(1, ()),
+                .error(2, APIError.http(status: 500))
+            ]).asObservable()
+        )
+        
+        self.output = self.viewModel.transform(input: self.input, disposeBag: self.disposeBag)
+        
+        self.output
+            .isLoading
+            .subscribe(loadingObservable)
+            .disposed(by: self.disposeBag)
+        
+        self.poseuploadUsecase
+            .uploadCompletedEvent
+            .subscribe(poseObservable)
+            .disposed(by: self.disposeBag)
+        
+        self.scheduler.start()
+        
+        XCTAssertEqual(loadingObservable.events, [
+            .next(0, false),
+            .next(1, true),
+            .next(1, false),
+            .next(2, false)
+        ])
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    override func tearDown() {
+        super.tearDown()
+        self.disposeBag = nil
+        self.scheduler = nil
+        self.input = nil
+        self.output = nil
+        self.poseUploadCoordinator = nil
+        self.viewModel = nil
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
