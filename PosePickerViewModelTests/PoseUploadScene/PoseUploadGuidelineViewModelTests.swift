9fee312ade82126e8452b95ecc336b27e032a498
Parkjju <rudwns3927@gmail.com>
2024-06-11 10:37:01 +0900

    test: 포즈 가이드라인 뷰모델 코디네이터 화면 전환 테스트

--- PosePickerViewModelTests/PoseUploadScene/PoseUploadGuidelineViewModelTests.swift
+++ PosePickerViewModelTests/PoseUploadScene/PoseUploadGuidelineViewModelTests.swift
@@ -6,30 +6,53 @@
 //
 
 import XCTest
+import RxTest
+import RxSwift
+
+@testable import posepicker
 
 final class PoseUploadGuidelineViewModelTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    private var viewModel: MyPoseGuidelineViewModel!
+    private var disposeBag: DisposeBag!
+    private var scheduler: TestScheduler!
+    private var poseUploadCoordinator: PoseUploadCoordinator!
+    private var input: MyPoseGuidelineViewModel.Input!
+    private var output: MyPoseGuidelineViewModel.Output!
+    
+    override func setUp() {
+        super.setUp()
+        self.disposeBag = DisposeBag()
+        self.scheduler = .init(initialClock: 0)
+        self.poseUploadCoordinator = MockPoseUploadCoordinator(
+            UINavigationController(
+                rootViewController: UIViewController()
+            )
+        )
+        self.viewModel = MyPoseGuidelineViewModel(
+            coordinator: self.poseUploadCoordinator
+        )
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func test_화면전환_테스트() {
+        self.input = MyPoseGuidelineViewModel.Input(
+            guidelineCheckButtonTapEvent: self.scheduler.createColdObservable([
+                .next(0, ())
+            ]).asObservable(),
+            imageLoadCompletedEvent: self.scheduler.createColdObservable([
+                .next(0, nil)
+            ]).asObservable(),
+            imageLoadFailedEvent: self.scheduler.createColdObservable([
+                .next(0, ())
+            ]).asObservable()
+        )
+        
+        self.output = self.viewModel.transform(input: self.input, disposeBag: self.disposeBag)
+        
+        self.scheduler.start()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    override func tearDown() {
+        super.tearDown()
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
