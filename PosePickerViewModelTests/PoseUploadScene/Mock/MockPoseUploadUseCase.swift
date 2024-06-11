3fc0a2b4a56be4d0c679c89e4a9843ef720b6644
Parkjju <rudwns3927@gmail.com>
2024-06-11 10:34:53 +0900

    feat: 목업 포즈 업로드 유스케이스 클래스 추가

---
+++ PosePickerViewModelTests/PoseUploadScene/Mock/MockPoseUploadUseCase.swift
@@ -0,0 +1,36 @@
+//
+//  MockPoseUploadUseCase.swift
+//  PosePickerViewModelTests
+//
+//  Created by 박경준 on 6/11/24.
+//
+
+import UIKit
+import RxSwift
+
+@testable import posepicker
+
+final class MockPoseUploadUseCase: PoseUploadUseCase {
+    var uploadCompletedEvent = PublishSubject<Pose>()
+    
+    func savePose(image: UIImage?, frameCount: String, peopleCount: String, source: String, sourceUrl: String, tag: String) {
+        uploadCompletedEvent.onNext(
+            .init(
+                poseInfo:
+                    .init(
+                        createdAt: Date.now.toString(),
+                        frameCount: 4,
+                        imageKey: "https://imageURL.com",
+                        peopleCount: 1,
+                        poseId: 1,
+                        source: source,
+                        sourceUrl: sourceUrl,
+                        tagAttributes: "친구,재미",
+                        updatedAt: Date.now.toString(),
+                        bookmarkCheck: true,
+                        poseUploadUser: nil
+                    )
+            )
+        )
+    }
+}
